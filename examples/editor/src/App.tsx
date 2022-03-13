400cde77be718a089898cb37e8230088e36e966a
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2022-03-13 16:50:59 +0200

    Added type to window

--- examples/editor/src/App.tsx
+++ examples/editor/src/App.tsx
@@ -1,14 +1,18 @@
 // import logo from './logo.svg'
 import styles from "./App.module.css";
 import { EditorContent, useEditor } from "@blocknote/core";
+import { Editor } from "@tiptap/core";
 import React from "react";
 // import "@blocknote/core/style.css";
+
+type WindowWithProseMirror = Window &
+  typeof globalThis & { ProseMirror: Editor };
 
 function App() {
   const editor = useEditor({
     onUpdate: ({ editor }) => {
       console.log(editor.getJSON());
-      (window as any).ProseMirror = editor; // Give tests a way to get editor instance
+      (window as WindowWithProseMirror).ProseMirror = editor; // Give tests a way to get editor instance
     },
     editorProps: {
       attributes: {
