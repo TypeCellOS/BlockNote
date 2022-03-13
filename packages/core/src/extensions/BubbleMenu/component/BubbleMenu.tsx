7050f6d2c03230d91b7f045316125ea98f8c10fe
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2022-03-13 12:34:37 +0200

    Added basic dropdown
    - Also installed @atlaskit/dropdown-menu

--- packages/core/src/extensions/BubbleMenu/component/BubbleMenu.tsx
+++ packages/core/src/extensions/BubbleMenu/component/BubbleMenu.tsx
@@ -4,10 +4,12 @@ import {
   RiBold,
   RiH1,
   RiH2,
+  RiH3,
   RiItalic,
   RiLink,
   RiStrikethrough,
   RiUnderline,
+  RiText,
 } from "react-icons/ri";
 import browser from "../../../lib/atlaskit/browser";
 import { SimpleToolbarButton } from "../../../shared/components/toolbar/SimpleToolbarButton";
@@ -15,6 +17,8 @@ import { Toolbar } from "../../../shared/components/toolbar/Toolbar";
 import { useEditorForceUpdate } from "../../../shared/hooks/useEditorForceUpdate";
 import { findBlock } from "../../Blocks/helpers/findBlock";
 import LinkToolbarButton from "./LinkToolbarButton";
+import DropdownMenu, { DropdownItemGroup } from "@atlaskit/dropdown-menu";
+import DropdownBlockItem from "./DropdownBlockItem";
 
 function formatKeyboardShortcut(shortcut: string) {
   if (browser.ios || browser.mac) {
@@ -35,8 +39,60 @@ export const BubbleMenu = (props: { editor: Editor }) => {
   const isHeading2Active =
     findBlock(props.editor.state.selection)?.node.attrs.headingType === 2;
 
+  const currentBlockHeading = findBlock(props.editor.state.selection)?.node
+    .attrs.headingType;
+
+  const currentBlockName = (() => {
+    switch (currentBlockHeading) {
+      case 1:
+        return "Heading 1";
+      case 2:
+        return "Heading 2";
+      case 3:
+        return "Heading 3";
+      default:
+        return "Paragraph";
+    }
+  })();
+
   return (
     <Toolbar>
+      <DropdownMenu trigger={currentBlockName}>
+        <DropdownItemGroup>
+          <DropdownBlockItem
+            title="Heading 1"
+            icon={RiH1}
+            isSelected={currentBlockHeading === 1}
+            onClick={() =>
+              props.editor.chain().focus().setBlockHeading({ level: 1 }).run()
+            }
+          />
+          <DropdownBlockItem
+            title="Heading 2"
+            icon={RiH2}
+            isSelected={currentBlockHeading === 2}
+            onClick={() =>
+              props.editor.chain().focus().setBlockHeading({ level: 2 }).run()
+            }
+          />
+          <DropdownBlockItem
+            title="Heading 3"
+            icon={RiH3}
+            isSelected={currentBlockHeading === 3}
+            onClick={() =>
+              props.editor.chain().focus().setBlockHeading({ level: 3 }).run()
+            }
+          />
+          <DropdownBlockItem
+            title="Paragraph"
+            icon={RiText}
+            isSelected={currentBlockHeading === undefined}
+            onClick={() =>
+              props.editor.chain().focus().unsetBlockHeading().run()
+            }
+          />
+        </DropdownItemGroup>
+      </DropdownMenu>
       <SimpleToolbarButton
         onClick={() =>
           isHeadingActive
