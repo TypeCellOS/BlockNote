3c638029e7aac5c111f6d02ececaa0481a5cb9ce
Matthew <matthewlipski@gmail.com>
2022-11-01 13:29:49 +0100

    Test

---
+++ packages/core/src/BlockNoteTheme.ts
@@ -0,0 +1,79 @@
+import { MantineThemeOverride } from "@mantine/core";
+
+export const BlockNoteTheme: MantineThemeOverride = {
+  activeStyles: {
+    // Removes button press effect.
+    transform: "none",
+  },
+  colorScheme: "light",
+  colors: {
+    brandFinal: [
+      "#F6F6F8",
+      "#ECEDF0",
+      "#DFE1E6",
+      "#C2C7D0",
+      "#A6ADBA",
+      "#8993A4",
+      "#6D798F",
+      "#505F79",
+      "#344563",
+      "#172B4D",
+    ],
+  },
+  components: {
+    MyComponent: {
+      styles: (theme) => ({
+        root: {
+          backgroundColor: "red",
+          border: `1px solid red`,
+          borderRadius: "6px",
+          width: "fit-content",
+        },
+      }),
+    },
+    Menu: {
+      styles: (theme) => ({
+        dropdown: {
+          border: "",
+          boxShadow: "",
+        },
+        item: {
+          color: theme.colors.brandFinal,
+        },
+        // Adds some space between the item text and selection tick
+        itemRightSection: {
+          // paddingLeft: "10px",
+        },
+      }),
+    },
+    // Box: {
+    //   styles: {
+    //     root: {
+    //       // backgroundColor: "white",
+    //       boxShadow:
+    //         "0px 4px 8px rgba(9, 30, 66, 0.15), 0px 0px 1px rgba(9, 30, 66, 0.21)",
+    //       // border: `1px solid ${theme.colors.brandFinal[1]}`,
+    //       borderRadius: "6px",
+    //       width: "fit-content",
+    //     },
+    //   },
+    // },
+    Group: {
+      styles: {},
+    },
+    Button: {
+      styles: {},
+    },
+    ActionIcon: {
+      styles: {},
+    },
+  },
+  fontFamily: "Inter",
+  primaryColor: "brandFinal",
+  primaryShade: 9,
+  other: {
+    defaultBoxShadow:
+      "0px 4px 8px rgba(9, 30, 66, 0.15), 0px 0px 1px rgba(9, 30, 66, 0.21)",
+    // border: `1px solid ${theme.colors.brandFinal[1]}`,
+  },
+};
--- packages/core/src/shared/components/toolbar/Toolbar.tsx
+++ packages/core/src/shared/components/toolbar/Toolbar.tsx
@@ -1,57 +1,58 @@
-import { Box, Group, MantineProvider } from "@mantine/core";
+import {
+  Box,
+  Button,
+  createStyles,
+  DefaultProps,
+  Group,
+  MantineProvider,
+  Menu,
+  Selectors,
+} from "@mantine/core";
+import { BlockNoteTheme } from "../../../BlockNoteTheme";
+import { HiChevronDown } from "react-icons/hi";
+import { SimpleToolbarDropdownItem } from "./SimpleToolbarDropdownItem";
+import { ReactElement } from "react";
 
-export const Toolbar = (props: { children: any }) => {
+type ToolbarStylesNames = Selectors<typeof useStyles>;
+
+interface ToolbarProps extends DefaultProps<ToolbarStylesNames, {}> {
+  children: ReactElement;
+}
+
+export const useStyles = createStyles((_theme, _params: {}) => ({
+  // add all styles as usual
+  root: {},
+  items: {},
+}));
+
+export const Toolbar = (props: ToolbarProps) => {
+  const { classes, cx } = useStyles(
+    // First argument of useStyles is styles params
+    {},
+    // Second argument is responsible for styles api integration
+    {
+      name: "MyComponent",
+      classNames: props.classNames,
+      styles: props.styles,
+      unstyled: props.unstyled,
+    }
+  );
+
+  const test = BlockNoteTheme;
+  console.log(classes);
+  // debugger;
+
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        activeStyles: {
-          // Removes button press effect.
-          transform: "none",
-        },
-        colorScheme: "light",
-        colors: {
-          brandFinal: [
-            "#F6F6F8",
-            "#ECEDF0",
-            "#DFE1E6",
-            "#C2C7D0",
-            "#A6ADBA",
-            "#8993A4",
-            "#6D798F",
-            "#505F79",
-            "#344563",
-            "#172B4D",
-          ],
-        },
-        components: {
-          Menu: {
-            styles: (theme) => ({
-              item: {
-                fontSize: "12px",
-                color: theme.colors.brandFinal,
-              },
-              // Adds some space between the item text and selection tick
-              itemRightSection: {
-                paddingLeft: "10px",
-              },
-            }),
-          },
-        },
-        fontFamily: "Inter",
-        primaryColor: "brandFinal",
-        primaryShade: 9,
-      }}>
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={BlockNoteTheme}>
       <Box
-        sx={(theme) => ({
-          backgroundColor: "white",
-          boxShadow:
-            "0px 4px 8px rgba(9, 30, 66, 0.15), 0px 0px 1px rgba(9, 30, 66, 0.21)",
-          border: `1px solid ${theme.colors.brandFinal[1]}`,
-          borderRadius: "6px",
-          width: "fit-content",
-        })}>
+        className={cx(classes.root, props.className)}
+        // sx={(theme) => ({
+        //   backgroundColor: "white",
+        //   border: `1px solid ${theme.colors.brandFinal[1]}`,
+        //   borderRadius: "6px",
+        //   width: "fit-content",
+        // })}
+      >
         <Group p={2} noWrap grow={false} spacing={2}>
           {props.children}
         </Group>
