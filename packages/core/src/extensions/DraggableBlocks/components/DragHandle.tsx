a10632ddb019589ce4032a003d96c806f44ebfdf
Matthew <matthewlipski@gmail.com>
2022-11-25 18:52:25 +0100

    Minor fix

--- packages/core/src/extensions/DraggableBlocks/components/DragHandle.tsx
+++ packages/core/src/extensions/DraggableBlocks/components/DragHandle.tsx
@@ -32,7 +32,7 @@ export const DragHandle = (props: {
     if (!pos) return;
 
     // Creates a new block if current one is not empty.
-    props.editor.commands.BNCreateBlockOrSetContentType(pos.pos, "textBlock");
+    props.editor.commands.BNCreateBlockOrSetContentType(pos.pos, "textContent");
 
     // Focuses and activates the slash menu.
     props.editor.view.focus();
