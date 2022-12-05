a01ad22982900f701418907429ac6e18172fe2f0
Matthew Lipski <matthewlipski@gmail.com>
2022-12-05 01:15:24 +0100

    Added functions to allow testing block IDs

--- tests/end-to-end/keyboardhandlers/keyboardhandlers.test.ts
+++ tests/end-to-end/keyboardhandlers/keyboardhandlers.test.ts
@@ -4,17 +4,13 @@ import {
   H_ONE_BLOCK_SELECTOR,
   H_TWO_BLOCK_SELECTOR,
 } from "../../utils/const";
-import { compareDocToSnapshot, focusOnEditor } from "../../utils/editor";
 import {
-  copyPasteAll,
-  insertHeading,
-  startList,
-  insertListItems,
-  insertNestedListItems,
-  insertParagraph,
-} from "../../utils/copypaste";
-import { moveMouseOverElement } from "../../utils/mouse";
-import { showMouseCursor } from "../../utils/debug";
+  compareDocWithIDsToSnapshot,
+  focusOnEditor,
+  getDoc,
+  getMockIDMap,
+} from "../../utils/editor";
+import { insertHeading } from "../../utils/copypaste";
 
 test.beforeEach(async ({ page }) => {
   await page.goto(BASE_URL);
@@ -22,33 +18,31 @@ test.beforeEach(async ({ page }) => {
 
 test.describe.configure({ mode: "serial" });
 test.describe("Check Keyboard Handlers' Behaviour", () => {
-  test("Check Enter when selection is not empty", async ({
-    page,
-    browserName,
-  }) => {
+  test("Check Enter when selection is not empty", async ({ page }) => {
     await focusOnEditor(page);
     await insertHeading(page, 1);
     await insertHeading(page, 2);
-    await showMouseCursor(page);
+
+    const mockIDMap = getMockIDMap(await getDoc(page));
 
     const startElement = await page.locator(H_ONE_BLOCK_SELECTOR);
     let boundingBox = await startElement.boundingBox();
     let { x, y, height } = boundingBox;
     await page.mouse.move(x + 35, y + height / 2, { steps: 5 });
     await page.mouse.down();
-    // await page.pause();
 
     const endElement = await page.locator(H_TWO_BLOCK_SELECTOR);
     boundingBox = await endElement.boundingBox();
     ({ x, y, height } = boundingBox);
     await page.mouse.move(x + 105, y + height / 2, { steps: 5 });
     await page.mouse.up();
-    // await page.pause();
-    // await page.waitForTimeout(200);
+
     await page.keyboard.press("Enter");
 
-    // await page.pause();
-
-    await compareDocToSnapshot(page, "enterSelectionNotEmpty.json");
+    await compareDocWithIDsToSnapshot(
+      page,
+      mockIDMap,
+      "enterSelectionNotEmpty.json"
+    );
   });
 });
