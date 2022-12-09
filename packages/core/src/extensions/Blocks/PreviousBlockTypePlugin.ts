66027cf674e4b168ec20ffb243ade6e0ab6e6063
Matthew Lipski <matthewlipski@gmail.com>
2022-12-09 15:54:42 +0100

    Rewrite to fix remaining issues

--- packages/core/src/extensions/Blocks/PreviousBlockTypePlugin.ts
+++ packages/core/src/extensions/Blocks/PreviousBlockTypePlugin.ts
@@ -1,8 +1,4 @@
-import {
-  combineTransactionSteps,
-  findChildren,
-  getChangedRanges,
-} from "@tiptap/core";
+import { findChildren } from "@tiptap/core";
 import { Plugin, PluginKey } from "prosemirror-state";
 import { Decoration, DecorationSet } from "prosemirror-view";
 
@@ -48,6 +44,7 @@ export const PreviousBlockTypePlugin = () => {
         return {
           prevBlockAttrs: {} as any,
           updatedBlocks: new Set<string>(),
+          prevTransactionOldAttrs: {} as any,
         };
       },
 
@@ -58,127 +55,85 @@ export const PreviousBlockTypePlugin = () => {
           return prev;
         }
 
-        const transform = combineTransactionSteps(oldState.doc, [transaction]);
-        // const { mapping } = transform;
-        const changes = getChangedRanges(transform);
+        const prevTransactionOldAttrs = {} as any;
 
-        // TODO: instead of iterating through the entire document, only check nodes affected by the transactions
-        // We didn't get this to work yet:
-        // changes.forEach(({ oldRange, newRange }) => {
-        // const oldNodes = findChildrenInRange(
-        //   oldState.doc,
-        //   oldRange,
-        //   (node) => node.attrs.id
-        // );
+        const oldNodes = findChildren(oldState.doc, (node) => node.attrs.id);
+        const oldNodesById = new Map(
+          oldNodes.map((node) => [node.node.attrs.id, node])
+        );
+        const newNodes = findChildren(newState.doc, (node) => node.attrs.id);
 
-        // const newNodes = findChildrenInRange(
-        //   newState.doc,
-        //   newRange,
-        //   (node) => node.attrs.id
-        // );
+        for (let node of newNodes) {
+          const oldNode = oldNodesById.get(node.node.attrs.id);
 
-        changes.forEach(() => {
-          const oldNodes = findChildren(oldState.doc, (node) => node.attrs.id);
-          const oldNodesById = new Map(
-            oldNodes.map((node) => [node.node.attrs.id, node])
-          );
-          const newNodes = findChildren(newState.doc, (node) => node.attrs.id);
+          const oldContentNode = oldNode?.node.firstChild;
+          const newContentNode = node.node.firstChild;
 
-          for (let node of newNodes) {
-            const oldNode = oldNodesById.get(node.node.attrs.id);
+          if (oldNode && oldContentNode && newContentNode) {
+            const newAttrs = {
+              listItemType: newContentNode.attrs.listItemType,
+              listItemIndex: newContentNode.attrs.listItemIndex,
+              headingLevel: newContentNode.attrs.headingLevel,
+              type: newContentNode.type.name,
+              depth: newState.doc.resolve(node.pos).depth,
+            };
 
-            const oldContentNode = oldNode?.node.firstChild;
-            const newContentNode = node.node.firstChild;
+            let oldAttrs = {
+              listItemType: oldContentNode.attrs.listItemType,
+              listItemIndex: oldContentNode.attrs.listItemIndex,
+              headingLevel: oldContentNode.attrs.headingLevel,
+              type: oldContentNode.type.name,
+              depth: oldState.doc.resolve(oldNode.pos).depth,
+            };
 
-            if (oldNode && oldContentNode && newContentNode) {
-              let newAttrs = {
-                listItemType: newContentNode.attrs.listItemType,
-                listItemIndex: newContentNode.attrs.listItemIndex,
-                headingLevel: newContentNode.attrs.headingLevel,
-                type: newContentNode.type.name,
-                depth: newState.doc.resolve(node.pos).depth,
-              };
+            prevTransactionOldAttrs[node.node.attrs.id] = oldAttrs;
 
-              let oldAttrs = {
-                listItemType: oldContentNode.attrs.listItemType,
-                listItemIndex: oldContentNode.attrs.listItemIndex,
-                headingLevel: oldContentNode.attrs.headingLevel,
-                type: oldContentNode.type.name,
-                depth: oldState.doc.resolve(oldNode.pos).depth,
-              };
+            let shouldUpdate =
+              JSON.stringify(oldAttrs) !== JSON.stringify(newAttrs); // TODO: faster deep equal?
 
-              let ou = false;
-
-              // Handles all transactions which modify ordered list items.
-              if (
-                oldAttrs.listItemType === "ordered" &&
-                newAttrs.listItemType === "ordered"
-              ) {
-                const depthChanged =
-                  oldAttrs.listItemIndex !== null &&
-                  newAttrs.listItemIndex !== null &&
-                  oldAttrs.depth !== newAttrs.depth;
-                const indexInitialized =
-                  oldAttrs.listItemIndex === null &&
-                  newAttrs.listItemIndex !== null;
-                const indexUpdated =
-                  oldAttrs.listItemIndex !== newAttrs.listItemIndex &&
-                  oldAttrs.listItemIndex !== null &&
-                  newAttrs.listItemIndex !== null;
-
-                // Checks if the transaction has moved the block to a different nesting level. Keep in mind if this also
-                // changes the enclosed list item's index (it usually does), the OrderedListItemIndexPlugin will
-                // immediately dispatch a transaction to update the index, which immediately overrides the changes made
-                // by this one. Therefore, this is only relevant for the case in which the nesting level changes but the
-                // index doesn't.
-                if (depthChanged) {
-                  ou = true;
-                }
-
-                // Checks if the transaction has either set the ordered list item's index for the first time or updated
-                // it. These transactions are always generated by the OrderedListItemIndexPlugin.
-                if (indexInitialized || indexUpdated) {
-                  // Checks if the block containing the ordered list item already exists. Since this transaction was
-                  // generated by the OrderedListItemIndexPlugin, the only attribute which has changed between oldAttrs
-                  // and newAttrs is listItemIndex. However, if the block already exists and the index needs updating,
-                  // it must mean that the block content type was just changed, or the block has just changed positions
-                  // (including nesting levels). In either case, it means we need to get the remaining attributes from
-                  // before the previous transaction.
-                  if (prev.prevBlockAttrs[node.node.attrs.id]) {
-                    oldAttrs = prev.prevBlockAttrs[node.node.attrs.id];
-                    ou = true;
-                  }
-
-                  // Animating the index changing isn't necessary since we only care about the "slide out" animation, so
-                  // this line disables that.
-                  oldAttrs.listItemIndex = newAttrs.listItemIndex;
-                }
+            // Whenever a transaction is appended by the OrderedListItemIndexPlugin, it's given the metadata:
+            // { "orderedListIndexing": true }
+            // These appended transactions happen immediately after any transaction which causes ordered list item
+            // indices to require updating, including those which trigger animations. Therefore, these animations are
+            // immediately overridden when the PreviousBlockTypePlugin processes the appended transaction, despite only
+            // the listItemIndex attribute changing. To solve this, oldAttrs must be edited for transactions with the
+            // "orderedListIndexing" metadata, so the correct animation can be re-triggered.
+            if (transaction.getMeta("orderedListIndexing")) {
+              // If the block existed before the transaction, gets the attributes from before the previous transaction
+              // (i.e. the transaction that caused list item indices to need updating).
+              if (node.node.attrs.id in prev.prevTransactionOldAttrs) {
+                oldAttrs = prev.prevTransactionOldAttrs[node.node.attrs.id];
               }
 
-              prev.prevBlockAttrs[node.node.attrs.id] = oldAttrs;
-
-              if (
-                (newAttrs.listItemType === "ordered" ? ou : true) &&
-                JSON.stringify(oldAttrs) !== JSON.stringify(newAttrs) // TODO: faster deep equal?
-              ) {
-                (oldAttrs as any)["depth-change"] =
-                  oldAttrs.depth - newAttrs.depth;
-
-                // for debugging:
-                // console.log(
-                //   "id:",
-                //   node.node.attrs.id,
-                //   "previousBlockTypePlugin changes detected, oldAttrs",
-                //   oldAttrs,
-                //   "new",
-                //   newAttrs
-                // );
-
-                prev.updatedBlocks.add(node.node.attrs.id);
+              // Stops list item indices themselves being animated (looks smoother), unless the block's content type is
+              // changing from an ordered list item to something else.
+              if (newAttrs.listItemIndex) {
+                oldAttrs.listItemIndex = newAttrs.listItemIndex;
               }
             }
+
+            prev.prevBlockAttrs[node.node.attrs.id] = oldAttrs;
+
+            if (shouldUpdate) {
+              (oldAttrs as any)["depth-change"] =
+                oldAttrs.depth - newAttrs.depth;
+
+              // for debugging:
+              console.log(
+                "id:",
+                node.node.attrs.id,
+                "previousBlockTypePlugin changes detected, oldAttrs",
+                oldAttrs,
+                "new",
+                newAttrs
+              );
+
+              prev.updatedBlocks.add(node.node.attrs.id);
+            }
           }
-        });
+        }
+
+        prev.prevTransactionOldAttrs = prevTransactionOldAttrs;
 
         return prev;
       },
