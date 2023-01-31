c13cf0a6b89a28c9851f2a71ce6fdb1780c8e8e0
Matthew Lipski <matthewlipski@gmail.com>
2023-01-31 16:55:17 +0100

    Fixed list item keyboard shortcuts not working

--- packages/core/src/extensions/Blocks/nodes/BlockContent/ListItemBlockContent/ListItemKeyboardShortcuts.ts
+++ packages/core/src/extensions/Blocks/nodes/BlockContent/ListItemBlockContent/ListItemKeyboardShortcuts.ts
@@ -10,13 +10,13 @@ export const handleEnter = (editor: Editor) => {
   const selectionEmpty =
     editor.state.selection.anchor === editor.state.selection.head;
 
-  if (contentType.name !== "listItem" || !selectionEmpty) {
+  if (!contentType.name.endsWith("ListItem") || !selectionEmpty) {
     return false;
   }
 
   return editor.commands.first(({ state, chain, commands }) => [
     () =>
-      // Changes list item block to a text block if both the content is empty.
+      // Changes list item block to a text block if the content is empty.
       commands.command(() => {
         if (node.textContent.length === 0) {
           return commands.BNUpdateBlock(state.selection.from, {
