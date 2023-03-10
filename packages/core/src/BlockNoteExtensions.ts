9510530c5b627dcb9404c38cf318594b85e85dd6
Yousef <yousefdardiry@gmail.com>
2023-03-10 16:48:44 +0100

    don't use default imports, hopefully fixes sandpack (#115)
    * don't use default imports, hopefully fixes sandpack
    
    * fix build

--- packages/core/src/BlockNoteExtensions.ts
+++ packages/core/src/BlockNoteExtensions.ts
@@ -1,16 +1,16 @@
 import { Extensions, extensions } from "@tiptap/core";
 
-import Bold from "@tiptap/extension-bold";
-import Code from "@tiptap/extension-code";
-import DropCursor from "@tiptap/extension-dropcursor";
-import GapCursor from "@tiptap/extension-gapcursor";
-import HardBreak from "@tiptap/extension-hard-break";
+import { Bold } from "@tiptap/extension-bold";
+import { Code } from "@tiptap/extension-code";
+import { Dropcursor } from "@tiptap/extension-dropcursor";
+import { Gapcursor } from "@tiptap/extension-gapcursor";
+import { HardBreak } from "@tiptap/extension-hard-break";
 import { History } from "@tiptap/extension-history";
-import Italic from "@tiptap/extension-italic";
+import { Italic } from "@tiptap/extension-italic";
 import { Link } from "@tiptap/extension-link";
-import Strike from "@tiptap/extension-strike";
-import Text from "@tiptap/extension-text";
-import Underline from "@tiptap/extension-underline";
+import { Strike } from "@tiptap/extension-strike";
+import { Text } from "@tiptap/extension-text";
+import { Underline } from "@tiptap/extension-underline";
 import { BackgroundColorExtension } from "./extensions/BackgroundColor/BackgroundColorExtension";
 import { BackgroundColorMark } from "./extensions/BackgroundColor/BackgroundColorMark";
 import { blocks } from "./extensions/Blocks";
@@ -53,7 +53,7 @@ export const getBlockNoteExtensions = (opts: {
     extensions.Tabindex,
 
     // DevTools,
-    GapCursor,
+    Gapcursor,
 
     // DropCursor,
     Placeholder.configure({
@@ -87,7 +87,7 @@ export const getBlockNoteExtensions = (opts: {
     // custom blocks:
     ...blocks,
 
-    DropCursor.configure({ width: 5, color: "#ddeeff" }),
+    Dropcursor.configure({ width: 5, color: "#ddeeff" }),
     History,
     // This needs to be at the bottom of this list, because Key events (such as enter, when selecting a /command),
     // should be handled before Enter handlers in other components like splitListItem
