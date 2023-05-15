a4c5836340b8c09a5c36c752bb1be3960ebbf976
Sangwan Jeon <commits@swjeon.kr>
2023-05-15 14:32:12 +0000

    chore: update dependencies

--- packages/react/src/BlockNoteView.tsx
+++ packages/react/src/BlockNoteView.tsx
@@ -1,5 +1,5 @@
 import { BlockNoteEditor } from "@blocknote/core";
-import { EditorContent } from "@tiptap/react";
+import { Editor, EditorContent } from "@tiptap/react";
 // import { BlockNoteTheme } from "./BlockNoteTheme";
 // import { MantineProvider } from "@mantine/core";
 
@@ -7,7 +7,15 @@ export function BlockNoteView(props: { editor: BlockNoteEditor | null }) {
   return (
     // TODO: Should we wrap editor in MantineProvider? Otherwise we have to duplicate color hex values.
     // <MantineProvider theme={BlockNoteTheme}>
-    <EditorContent editor={props.editor?._tiptapEditor || null} />
+    // FIXME: Type error occurs here
+    // error TS2322: Type '(Editor & { contentComponent: any; }) | null' is not assignable to type 'Editor | null'.
+    // Type 'Editor & { contentComponent: any; }' is not assignable to type 'Editor | null'.
+    //   Type 'Editor & { contentComponent: any; }' is not assignable to type 'Editor'.
+    //     Types have separate declarations of a private property 'commandManager'.
+
+    <EditorContent
+      editor={(props.editor?._tiptapEditor as unknown as Editor) || null}
+    />
     // </MantineProvider>
   );
 }
