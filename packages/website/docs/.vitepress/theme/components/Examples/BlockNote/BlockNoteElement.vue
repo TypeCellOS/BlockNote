2882c3f5245b2f01d1ade365910f39550646840c
Matthew Lipski <matthewlipski@gmail.com>
2023-06-14 23:39:02 +0200

    Fixed theme switching in home page

--- packages/website/docs/.vitepress/theme/components/Examples/BlockNote/BlockNoteElement.vue
+++ packages/website/docs/.vitepress/theme/components/Examples/BlockNote/BlockNoteElement.vue
@@ -1,21 +1,22 @@
 <template>
-  <BasicPure></BasicPure>
+  <BasicPure :theme="theme"></BasicPure>
 </template>
 <script>
 import { applyPureReactInVue } from "veaury";
 // This is a React component
 
 import { ReactBlockNote } from "./ReactBlockNote";
-import { ref } from "vue";
+import { useData } from "vitepress";
 
 export default {
   components: {
     BasicPure: applyPureReactInVue(ReactBlockNote),
   },
-  setup() {
+  data() {
     return {
-      // foo: ref("Hello!"),
+      theme: useData().isDark,
     };
   },
+  props: ["theme"],
 };
 </script>
