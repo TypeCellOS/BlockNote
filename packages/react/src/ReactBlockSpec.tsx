a9baab24e40003f43ee6243fc8206a606193d5c6
Matthew Lipski <matthewlipski@gmail.com>
2023-06-23 18:31:57 +0200

    Revert "Cleaned up serializer implementation - no longer uses function override"
    This reverts commit b4f3fb6433c6a9e00b90a4f3a6f7d1ff87bc1758.

--- packages/react/src/ReactBlockSpec.tsx
+++ packages/react/src/ReactBlockSpec.tsx
@@ -154,6 +154,8 @@ export function createReactBlockSpec<
     node: node,
     propSchema: blockConfig.propSchema,
     serialize: (block, editor) => {
+      // TODO: This doesn't seem like a great way of doing things - any better
+      //  method for converting a component to HTML?
       const blockContentWrapper = document.createElement("div");
       const BlockContent = reactRenderWithBlockStructure<
         BType,
