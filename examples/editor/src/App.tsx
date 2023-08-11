9ab70959b70bf7659523be5a00d3b88167f0d078
Matthew Lipski <matthewlipski@gmail.com>
2023-08-11 16:09:46 +0200

    Reverted `App.tsx`

--- examples/editor/src/App.tsx
+++ examples/editor/src/App.tsx
@@ -1,149 +1,27 @@
 // import logo from './logo.svg'
 import "@blocknote/core/style.css";
-import {
-  BlockNoteView,
-  getDefaultReactSlashMenuItems,
-  useBlockNote,
-  Theme,
-  lightDefaultTheme,
-  darkDefaultTheme,
-} from "@blocknote/react";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import styles from "./App.module.css";
-import { BlockSchema, defaultBlockSchema } from "@blocknote/core";
-import { Image, insertImage } from "../../../tests/utils/customblocks/Image";
-import {
-  insertReactImage,
-  ReactImage,
-} from "../../../tests/utils/customblocks/ReactImage";
 
 type WindowWithProseMirror = Window & typeof globalThis & { ProseMirror: any };
 
-const lightRedTheme = {
-  type: "light",
-  colors: {
-    editor: {
-      text: "#222222",
-      background: "#ffffff",
-    },
-    menu: {
-      text: "#ffffff",
-      background: "#9b0000",
-    },
-    tooltip: {
-      text: "#ffffff",
-      background: "#b00000",
-    },
-    hovered: {
-      text: "#ffffff",
-      background: "#b00000",
-    },
-    selected: {
-      text: "#ffffff",
-      background: "#c50000",
-    },
-    disabled: {
-      text: "#9b0000",
-      background: "#7d0000",
-    },
-    shadow: "#640000",
-    border: "#870000",
-    sideMenu: "#bababa",
-    // TODO: Update
-    highlightColors: lightDefaultTheme.colors.highlightColors,
-  },
-  borderRadius: 4,
-  fontFamily: "Helvetica Neue, sans-serif",
-} satisfies Partial<Theme>;
-
-const darkRedTheme = {
-  ...lightRedTheme,
-  type: "dark",
-  colors: {
-    ...lightRedTheme.colors,
-    editor: {
-      text: "#ffffff",
-      background: "#9b0000",
-    },
-    sideMenu: "#ffffff",
-    // TODO: Update
-    highlightColors: darkDefaultTheme.colors.highlightColors,
-  },
-} satisfies Partial<Theme>;
-
-const schema = {
-  ...defaultBlockSchema,
-  image: Image,
-  reactImage: ReactImage,
-} satisfies BlockSchema;
-
 function App() {
-  const editor = useBlockNote(
-    {
-      onEditorContentChange: (editor) => {
-        console.log(editor.topLevelBlocks);
-      },
-      blockSchema: schema,
-      slashMenuItems: [
-        ...getDefaultReactSlashMenuItems(schema),
-        insertImage,
-        insertReactImage,
-      ],
-      domAttributes: {
-        editor: {
-          class: styles.editor + " customEditorClass",
-          "data-custom-editor-attr": "editor",
-        },
-        blockContainer: {
-          class: "customBlockContainerClass",
-          "data-custom-block-container-attr": "blockContainer",
-        },
-        blockGroup: {
-          class: "customBlockGroupClass",
-          "data-custom-block-group-attr": "blockGroup",
-        },
-        blockContent: {
-          class: "customBlockContentClass",
-          "data-custom-block-content-attr": "blockContent",
-        },
-        inlineContent: {
-          class: "customInlineContentClass",
-          "data-custom-inline-content-attr": "inlineContent",
-        },
+  const editor = useBlockNote({
+    onEditorContentChange: (editor) => {
+      console.log(editor.topLevelBlocks);
+    },
+    domAttributes: {
+      editor: {
+        class: styles.editor,
+        "data-test": "editor",
       },
     },
-    []
-  );
+  });
 
   // Give tests a way to get prosemirror instance
   (window as WindowWithProseMirror).ProseMirror = editor?._tiptapEditor;
 
-  return (
-    <BlockNoteView
-      editor={editor}
-      theme={{
-        light: lightRedTheme,
-        dark: darkRedTheme,
-      }}
-      componentStyles={(theme) => ({
-        Editor: {
-          backgroundColor: theme.colors.editor.background,
-          borderRadius: theme.borderRadius,
-          border:
-            theme.type === "dark"
-              ? `1px solid ${theme.colors.border}`
-              : `1px solid ${theme.colors.sideMenu}`,
-          boxShadow:
-            theme.type === "dark"
-              ? `0 4px 12px ${theme.colors.shadow}`
-              : `0 4px 12px ${theme.colors.sideMenu}`,
-          height: "100vw",
-          ".ProseMirror": {
-            fontFamily: "Comic Sans MS",
-          },
-        },
-      })}
-    />
-  );
+  return <BlockNoteView editor={editor} />;
 }
 
 export default App;
