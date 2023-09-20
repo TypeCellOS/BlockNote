41247d0f5d26fbc14f6c87e791e9cb7e3f971b09
Marwan Zaarab <marwan.zaarab@gmail.com>
2023-09-20 08:14:30 -0500

    Fix code block formatting issue with backticks in parentheses

---
+++ packages/core/src/extensions/CustomCode/CustomCodeExtension.ts
@@ -0,0 +1,119 @@
+import {
+  Mark,
+  markInputRule,
+  markPasteRule,
+  mergeAttributes,
+} from "@tiptap/core";
+
+export interface CodeOptions {
+  HTMLAttributes: Record<string, any>;
+}
+
+declare module "@tiptap/core" {
+  interface Commands<ReturnType> {
+    code: {
+      /**
+       * Set a code mark
+       */
+      setCode: () => ReturnType;
+      /**
+       * Toggle inline code
+       */
+      toggleCode: () => ReturnType;
+      /**
+       * Unset a code mark
+       */
+      unsetCode: () => ReturnType;
+    };
+  }
+}
+
+/**
+ * Regular expressions to match inline code blocks enclosed in backticks.
+ * Original one from @tiptap/extension-code was:
+ *
+ *  inputRegex = /(?:^|\s)((?:`)((?:[^`]+))(?:`))$/
+ *  pasteRegex = /(?:^|\s)((?:`)((?:[^`]+))(?:`))/g
+ *
+ *  The edited regex below fixes this issue: https://github.com/TypeCellOS/BlockNote/issues/338
+ *  It matches:
+ *     - An opening backtick, followed by
+ *     - Any text that doesn't include a backtick (captured for marking), followed by
+ *     - A closing backtick.
+ *  This ensures that any text between backticks is formatted as code,
+ *  regardless of the surrounding characters.
+ */
+export const inputRegex = /`([^`]+)`/;
+export const pasteRegex = /`([^`]+)`/g;
+
+export const CustomCodeExtension = Mark.create<CodeOptions>({
+  name: "code",
+
+  addOptions() {
+    return {
+      HTMLAttributes: {},
+    };
+  },
+
+  excludes: "_",
+
+  code: true,
+
+  exitable: true,
+
+  parseHTML() {
+    return [{ tag: "code" }];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return [
+      "code",
+      mergeAttributes(this.options.HTMLAttributes, HTMLAttributes),
+      0,
+    ];
+  },
+
+  addCommands() {
+    return {
+      setCode:
+        () =>
+        ({ commands }) => {
+          return commands.setMark(this.name);
+        },
+      toggleCode:
+        () =>
+        ({ commands }) => {
+          return commands.toggleMark(this.name);
+        },
+      unsetCode:
+        () =>
+        ({ commands }) => {
+          return commands.unsetMark(this.name);
+        },
+    };
+  },
+
+  addKeyboardShortcuts() {
+    return {
+      "Mod-e": () => this.editor.commands.toggleCode(),
+    };
+  },
+
+  addInputRules() {
+    return [
+      markInputRule({
+        find: inputRegex,
+        type: this.type,
+      }),
+    ];
+  },
+
+  addPasteRules() {
+    return [
+      markPasteRule({
+        find: pasteRegex,
+        type: this.type,
+      }),
+    ];
+  },
+});
