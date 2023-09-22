9a10c688815dc3549859b85334a6bc78726b99c8
Matthew Lipski <50169049+matthewlipski@users.noreply.github.com>
2023-09-22 19:13:09 +0200

    fix: Menu overflows (#340)
    * Fixed overflow for slash menu, color picker, and formatting toolbar dropdown.
    
    * Updated screenshots
    
    * Added comment for `setTimeout`

--- packages/react/src/SharedComponents/Toolbar/components/ToolbarDropdown.tsx
+++ packages/react/src/SharedComponents/Toolbar/components/ToolbarDropdown.tsx
@@ -4,6 +4,7 @@ import {
   ToolbarDropdownItemProps,
 } from "./ToolbarDropdownItem";
 import { ToolbarDropdownTarget } from "./ToolbarDropdownTarget";
+import { usePreventMenuOverflow } from "../../../hooks/usePreventMenuOverflow";
 
 export type ToolbarDropdownProps = {
   items: ToolbarDropdownItemProps[];
@@ -13,12 +14,17 @@ export type ToolbarDropdownProps = {
 export function ToolbarDropdown(props: ToolbarDropdownProps) {
   const selectedItem = props.items.filter((p) => p.isSelected)[0];
 
+  const { ref, updateMaxHeight } = usePreventMenuOverflow();
+
   if (!selectedItem) {
     return null;
   }
 
   return (
-    <Menu exitTransitionDuration={0} disabled={props.isDisabled}>
+    <Menu
+      exitTransitionDuration={0}
+      disabled={props.isDisabled}
+      onOpen={updateMaxHeight}>
       <Menu.Target>
         <ToolbarDropdownTarget
           text={selectedItem.text}
@@ -26,11 +32,13 @@ export function ToolbarDropdown(props: ToolbarDropdownProps) {
           isDisabled={selectedItem.isDisabled}
         />
       </Menu.Target>
-      <Menu.Dropdown>
-        {props.items.map((item) => (
-          <ToolbarDropdownItem key={item.text} {...item} />
-        ))}
-      </Menu.Dropdown>
+      <div ref={ref}>
+        <Menu.Dropdown>
+          {props.items.map((item) => (
+            <ToolbarDropdownItem key={item.text} {...item} />
+          ))}
+        </Menu.Dropdown>
+      </div>
     </Menu>
   );
 }
