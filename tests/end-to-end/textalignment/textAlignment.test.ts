82cd68f3f9d5f4cc0433f143ee64c156b3e9df94
Matthew Lipski <matthewlipski@gmail.com>
2023-10-03 00:00:18 +0200

    Minor updates to tests & screenshots

--- tests/end-to-end/textalignment/textAlignment.test.ts
+++ tests/end-to-end/textalignment/textAlignment.test.ts
@@ -23,15 +23,15 @@ test.describe("Check Text Alignment Functionality", () => {
     await insertHeading(page, 2);
     await insertHeading(page, 3);
 
-    const element = await page.locator(H_TWO_BLOCK_SELECTOR);
-    const boundingBox = await element.boundingBox();
-    const { x, y, width, height } = boundingBox;
-
-    await page.mouse.click(x + 20, y + height / 2, { clickCount: 2 });
+    await page.keyboard.press("ArrowUp");
+    await page.keyboard.press("ArrowUp");
+    await page.keyboard.press("Control+ArrowLeft");
+    await page.keyboard.press("Control+Shift+ArrowRight");
 
     await page.locator(ALIGN_TEXT_RIGHT_BUTTON_SELECTOR).click();
     await page.waitForTimeout(350);
-    await page.mouse.click(x + width - 20, y + height / 2);
+
+    await page.keyboard.press("ArrowLeft");
 
     // Waits for formatting toolbar animation to finish.
     await page.waitForTimeout(500);
@@ -47,28 +47,15 @@ test.describe("Check Text Alignment Functionality", () => {
     await insertHeading(page, 2);
     await insertHeading(page, 3);
 
-    const firstElement = await page.locator(H_TWO_BLOCK_SELECTOR);
-    const firstElementBoundingBox = await firstElement.boundingBox();
-    await page.mouse.click(
-      firstElementBoundingBox.x + 20,
-      firstElementBoundingBox.y + firstElementBoundingBox.height / 2
-    );
-
-    const secondElement = await page.locator(H_ONE_BLOCK_SELECTOR);
-    const secondElementBoundingBox = await secondElement.boundingBox();
-    await page.keyboard.down("Shift");
-    await page.mouse.click(
-      secondElementBoundingBox.x + 20,
-      secondElementBoundingBox.y + secondElementBoundingBox.height / 2
-    );
-    await page.keyboard.up("Shift");
+    await page.keyboard.press("ArrowUp");
+    await page.keyboard.press("ArrowUp");
+    await page.keyboard.press("Control+ArrowLeft");
+    await page.keyboard.press("Control+Shift+ArrowUp");
 
     await page.locator(ALIGN_TEXT_RIGHT_BUTTON_SELECTOR).click();
     await page.waitForTimeout(350);
-    await page.mouse.click(
-      secondElementBoundingBox.x + secondElementBoundingBox.width - 20,
-      secondElementBoundingBox.y + secondElementBoundingBox.height / 2
-    );
+
+    await page.keyboard.press("ArrowLeft");
 
     // Waits for formatting toolbar animation to finish.
     await page.waitForTimeout(500);
