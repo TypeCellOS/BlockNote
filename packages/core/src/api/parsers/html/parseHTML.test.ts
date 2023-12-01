b76ce3a8a7f48a9a19a86900d0c429b507aa0933
Matthew Lipski <matthewlipski@gmail.com>
2023-12-01 17:17:31 +0100

    Fixed parse test indentation and added test for parsing non `li` elements

--- packages/core/src/api/parsers/html/parseHTML.test.ts
+++ packages/core/src/api/parsers/html/parseHTML.test.ts
@@ -61,99 +61,100 @@ async function parseHTMLAndCompareSnapshots(
 describe("Parse HTML", () => {
   it("Parse basic block types", async () => {
     const html = `<h1>Heading 1</h1>
-  <h2>Heading 2</h2>
-  <h3>Heading 3</h3>
-  <p>Paragraph</p>
-  <figure><img src="exampleURL" /><figcaption>Image Caption</figcaption></figure>
-  <p>None <strong>Bold </strong><em>Italic </em><u>Underline </u><s>Strikethrough </s><strong><em><s><u>All</u></s></em></strong></p>`;
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<p>Paragraph</p>
+<figure><img src="exampleURL" /><figcaption>Image Caption</figcaption></figure>
+<p>None <strong>Bold </strong><em>Italic </em><u>Underline </u><s>Strikethrough </s><strong><em><s><u>All</u></s></em></strong></p>`;
 
     await parseHTMLAndCompareSnapshots(html, "parse-basic-block-types");
   });
 
   it("list test", async () => {
     const html = `<ul>
-   <li>First</li>
-   <li>Second</li>
-   <li>Third</li>
-   <li>Five Parent
-   <ul>
-   <li>Child 1</li>
-   <li>Child 2</li>
-   </ul>
-   </li>
-   </ul>`;
+  <li>First</li>
+  <li>Second</li>
+  <li>Third</li>
+  <li>Five Parent
+  <ul>
+  <li>Child 1</li>
+  <li>Child 2</li>
+  </ul>
+  </li>
+</ul>`;
     await parseHTMLAndCompareSnapshots(html, "list-test");
   });
 
   it("Parse nested lists", async () => {
     const html = `<ul>
-    <li>Bullet List Item</li>
-      <li>Bullet List Item</li>
-        <ul>
-           <li>
-              Nested Bullet List Item
-           </li>
-           <li>
-              Nested Bullet List Item
-           </li>
-        </ul>
-     <li>
-        Bullet List Item
-     </li>
-  </ul>
-  <ol>
-     <li>
-        Numbered List Item
-        <ol>
-           <li>
-              Nested Numbered List Item
-           </li>
-           <li>
-              Nested Numbered List Item
-           </li>
-        </ol>
-     </li>
-     <li>
-        Numbered List Item
-     </li>
-  </ol>`;
+  <li>
+    Bullet List Item
+    <ul>
+      <li>
+        Nested Bullet List Item
+      </li>
+      <li>
+        Nested Bullet List Item
+      </li>
+    </ul>
+  </li>
+  <li>
+    Bullet List Item
+  </li>
+</ul>
+<ol>
+  <li>
+    Numbered List Item
+    <ol>
+      <li>
+        Nested Numbered List Item
+      </li>
+      <li>
+        Nested Numbered List Item
+      </li>
+    </ol>
+  </li>
+  <li>
+    Numbered List Item
+  </li>
+</ol>`;
 
     await parseHTMLAndCompareSnapshots(html, "parse-nested-lists");
   });
 
   it("Parse nested lists with paragraphs", async () => {
     const html = `<ul>
-     <li>
-        <p>Bullet List Item</p>
-        <ul>
-           <li>
-              <p>Nested Bullet List Item</p>
-           </li>
-           <li>
-              <p>Nested Bullet List Item</p>
-           </li>
-        </ul>
-     </li>
-     <li>
-        <p>Bullet List Item</p>
-     </li>
-  </ul>
-  <ol>
-     <li>
-        <p>Numbered List Item</p>
-        <ol>
-           <li>
-              <p>Nested Numbered List Item</p>
-           </li>
-           <li>
-              <p>Nested Numbered List Item</p>
-           </li>
-        </ol>
-     </li>
-     <li>
-        <p>Numbered List Item</p>
-     </li>
-  </ol>`;
+  <li>
+    <p>Bullet List Item</p>
+    <ul>
+      <li>
+        <p>Nested Bullet List Item</p>
+      </li>
+      <li>
+        <p>Nested Bullet List Item</p>
+      </li>
+    </ul>
+  </li>
+  <li>
+    <p>Bullet List Item</p>
+  </li>
+</ul>
+<ol>
+  <li>
+    <p>Numbered List Item</p>
+    <ol>
+      <li>
+        <p>Nested Numbered List Item</p>
+      </li>
+      <li>
+        <p>Nested Numbered List Item</p>
+      </li>
+    </ol>
+  </li>
+  <li>
+    <p>Numbered List Item</p>
+  </li>
+</ol>`;
 
     await parseHTMLAndCompareSnapshots(
       html,
@@ -163,53 +164,53 @@ describe("Parse HTML", () => {
 
   it("Parse mixed nested lists", async () => {
     const html = `<ul>
-     <li>
-        Bullet List Item
-        <ol>
-           <li>
-              Nested Numbered List Item
-           </li>
-           <li>
-              Nested Numbered List Item
-           </li>
-        </ol>
-     </li>
-     <li>
-        Bullet List Item
-     </li>
-  </ul>
-  <ol>
-     <li>
-        Numbered List Item
-        <ul>
-           <li>
-              <p>Nested Bullet List Item</p>
-           </li>
-           <li>
-              <p>Nested Bullet List Item</p>
-           </li>
-        </ul>
-     </li>
-     <li>
-        Numbered List Item
-     </li>
-  </ol>`;
+  <li>
+    Bullet List Item
+    <ol>
+      <li>
+        Nested Numbered List Item
+      </li>
+      <li>
+        Nested Numbered List Item
+      </li>
+    </ol>
+  </li>
+  <li>
+    Bullet List Item
+  </li>
+</ul>
+<ol>
+  <li>
+    Numbered List Item
+    <ul>
+      <li>
+        <p>Nested Bullet List Item</p>
+      </li>
+      <li>
+        <p>Nested Bullet List Item</p>
+      </li>
+    </ul>
+  </li>
+  <li>
+    Numbered List Item
+  </li>
+</ol>`;
 
     await parseHTMLAndCompareSnapshots(html, "parse-mixed-nested-lists");
   });
 
   it("Parse divs", async () => {
     const html = `<div>Single Div</div>
-  <div>
-    Div
-    <div>Nested Div</div>
-    <div>Nested Div</div>
-  </div>
-  <div>Single Div 2</div>
-  <div>
-    <div>Nested Div</div>
-    <div>Nested Div</div>
-  </div>`;
+<div>
+  Div
+  <div>Nested Div</div>
+  <div>Nested Div</div>
+</div>
+<div>Single Div 2</div>
+<div>
+  <div>Nested Div</div>
+  <div>Nested Div</div>
+</div>`;
 
     await parseHTMLAndCompareSnapshots(html, "parse-divs");
   });
@@ -222,9 +223,9 @@ describe("Parse HTML", () => {
 
   it("Parse fake image caption", async () => {
     const html = `<div>
-    <img src="exampleURL">
-    <p>Image Caption</p>
-  </div>`;
+  <img src="exampleURL">
+  <p>Image Caption</p>
+</div>`;
 
     await parseHTMLAndCompareSnapshots(html, "parse-fake-image-caption");
   });
@@ -232,38 +233,60 @@ describe("Parse HTML", () => {
   // TODO: this one fails
   it.skip("Parse deep nested content", async () => {
     const html = `<div>
-      Outer 1 Div Before
+  Outer 1 Div Before
+  <div>
+    Outer 2 Div Before
     <div>
-      Outer 2 Div Before
+      Outer 3 Div Before
       <div>
-        Outer 3 Div Before
-        <div>
-          Outer 4 Div Before
-          <h1>Heading 1</h1>
-          <h2>Heading 2</h2>
-          <h3>Heading 3</h3>
-          <p>Paragraph</p>
-          <figure><img src="exampleURL"><figcaption>Image Caption</figcaption></figure>
-          <p><strong>Bold</strong> <em>Italic</em> <u>Underline</u> <s>Strikethrough</s> <strong><em><s><u>All</u></s></em></strong></p>
-          Outer 4 Div After
-        </div>
-        Outer 3 Div After
+        Outer 4 Div Before
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <p>Paragraph</p>
+        <figure><img src="exampleURL"><figcaption>Image Caption</figcaption></figure>
+        <p><strong>Bold</strong> <em>Italic</em> <u>Underline</u> <s>Strikethrough</s> <strong><em><s><u>All</u></s></em></strong></p>
+        Outer 4 Div After
       </div>
-      Outer 2 Div After
+      Outer 3 Div After
     </div>
-    Outer 1 Div After
-  </div>`;
+    Outer 2 Div After
+  </div>
+  Outer 1 Div After
+</div>`;
 
     await parseHTMLAndCompareSnapshots(html, "parse-deep-nested-content");
   });
 
   it("Parse div with inline content and nested blocks", async () => {
     const html = `<div>
-    None <strong>Bold </strong><em>Italic </em><u>Underline </u><s>Strikethrough </s><strong><em><s><u>All</u></s></em></strong>
-    <div>Nested Div</div>
-    <p>Nested Paragraph</p>
-  </div>`;
+  None <strong>Bold </strong><em>Italic </em><u>Underline </u><s>Strikethrough </s><strong><em><s><u>All</u></s></em></strong>
+  <div>Nested Div</div>
+  <p>Nested Paragraph</p>
+</div>`;
 
     await parseHTMLAndCompareSnapshots(html, "parse-div-with-inline-content");
+  });
+
+  it("Parse nested lists with other elements", async () => {
+    const html = `<ul>
+  <li>
+    <p>Paragraph</p>
+    <h1>Heading 1</h1>
+    <div>Div</div>
+    <ul>
+      <li>
+        <p>Paragraph</p>
+        <h1>Heading 1</h1>
+        <div>Div</div>
+      </li>
+    </ul>
+  </li>
+</ul>`;
+
+    await parseHTMLAndCompareSnapshots(
+      html,
+      "parse-nested-lists-with-other-elements"
+    );
   });
 });
