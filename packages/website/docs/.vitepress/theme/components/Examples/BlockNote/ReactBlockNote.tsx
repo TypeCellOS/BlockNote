8cd4a3402acd1432b917dd238f101e358123158e
yousefed <yousefdardiry@gmail.com>
2023-12-03 21:38:39 +0100

    change room

--- packages/website/docs/.vitepress/theme/components/Examples/BlockNote/ReactBlockNote.tsx
+++ packages/website/docs/.vitepress/theme/components/Examples/BlockNote/ReactBlockNote.tsx
@@ -45,8 +45,8 @@ export function ReactBlockNote(props: { theme: "light" | "dark" }) {
     const doc = new Y.Doc();
     const provider = new YPartyKitProvider(
       "blocknote.yousefed.partykit.dev",
-      // "127.0.0.1:1999",
-      "homepage-1",
+      // "127.0.0.1:1999", // (dev server)
+      "homepage-2",
       doc
     );
     return [doc, provider];
