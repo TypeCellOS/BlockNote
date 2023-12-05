f6ab9299a232d53e94db65ac66c0b300947fc9e1
yousefed <yousefdardiry@gmail.com>
2023-12-05 14:31:59 +0100

    remove veaury

--- packages/website/docs/.vitepress/theme/components/Examples/BlockNote/BlockNoteElement.vue
+++ packages/website/docs/.vitepress/theme/components/Examples/BlockNote/BlockNoteElement.vue
@@ -1,21 +1,48 @@
 <template>
-  <BasicPure :theme="isDark ? 'dark' : 'light'"></BasicPure>
+  <div ref="el"></div>
 </template>
-<script>
-import { applyPureReactInVue } from "veaury";
-// This is a React component
 
-import { ReactBlockNote } from "./ReactBlockNote";
+<script setup>
+import { createElement } from "react";
+import { createRoot } from "react-dom/client";
+import { ref, onMounted, onUnmounted, watchEffect } from "vue";
+import { ReactBlockNote } from "./ReactBlockNote"; // Replace with your actual React component
 import { useData } from "vitepress";
 
-export default {
-  components: {
-    BasicPure: applyPureReactInVue(ReactBlockNote),
-  },
-  data() {
-    return {
-      isDark: useData().isDark,
-    };
-  },
-};
+const el = ref(null);
+const isDark = useData().isDark;
+let root = null;
+
+onMounted(() => {
+  if (el.value) {
+    root = createRoot(el.value);
+    renderReactComponent();
+  }
+});
+
+onUnmounted(() => {
+  if (root) {
+    console.log("unmount");
+    root.unmount();
+  }
+});
+
+// Reactively update the React component when Vue data changes
+watchEffect(() => {
+  console.log("effect", isDark.value);
+  if (root) {
+    renderReactComponent();
+  }
+});
+
+function renderReactComponent() {
+  // console.log("render", isDark.value);
+  root.render(
+    createElement(
+      ReactBlockNote,
+      { theme: isDark.value ? "dark" : "light" },
+      null
+    )
+  );
+}
 </script>
