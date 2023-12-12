1106945bc2ea9e51c0df656c0f3152c060403ea0
yousefed <yousefdardiry@gmail.com>
2023-12-12 13:55:56 +0100

    misc fixes

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import * as path from "path";
 import { defineConfig } from "vite";
 // import eslintPlugin from "vite-plugin-eslint";
 // https://vitejs.dev/config/
@@ -17,14 +18,11 @@ export default defineConfig((conf) => ({
         : {
             // Comment out the lines below to load a built version of blocknote
             // or, keep as is to load live from sources with live reload working
-            // "@blocknote/core": path.resolve(
-            //   __dirname,
-            //   "../../packages/core/src/"
-            // ),
-            // "@blocknote/react": path.resolve(
-            //   __dirname,
-            //   "../../packages/react/src/"
-            // ),
+            "@blocknote/core": path.resolve(__dirname, "../packages/core/src/"),
+            "@blocknote/react": path.resolve(
+              __dirname,
+              "../packages/react/src/"
+            ),
           },
   },
 }));
