924ab96c377b09cb7b145958be293d6279c86094
yousefed <yousefdardiry@gmail.com>
2024-01-08 10:35:50 +0100

    fix tests

--- packages/react/vitestSetup.ts
+++ packages/react/vitestSetup.ts
@@ -1,4 +1,4 @@
-import { beforeEach, afterEach } from "vitest";
+import { afterEach, beforeEach } from "vitest";
 
 beforeEach(() => {
   (window as Window & { __TEST_OPTIONS?: any }).__TEST_OPTIONS = {};
@@ -7,3 +7,29 @@ beforeEach(() => {
 afterEach(() => {
   delete (window as Window & { __TEST_OPTIONS?: any }).__TEST_OPTIONS;
 });
+
+// Mock ClipboardEvent
+class ClipboardEventMock extends Event {
+  public clipboardData = {
+    getData: () => {
+      //
+    },
+    setData: () => {
+      //
+    },
+  };
+}
+(global as any).ClipboardEvent = ClipboardEventMock;
+
+// Mock DragEvent
+class DragEventMock extends Event {
+  public dataTransfer = {
+    getData: () => {
+      //
+    },
+    setData: () => {
+      //
+    },
+  };
+}
+(global as any).DragEvent = DragEventMock;
