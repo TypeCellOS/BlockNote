c025d0534759ccfe318c15cafdc0dff8d9bcd9dd
yousefed <yousefdardiry@gmail.com>
2024-01-24 12:57:15 +0100

    check emptyness with css

--- packages/core/src/extensions/Placeholder/PlaceholderExtension.ts
+++ packages/core/src/extensions/Placeholder/PlaceholderExtension.ts
@@ -1,10 +1,9 @@
 import { Extension } from "@tiptap/core";
 import { Plugin, PluginKey } from "prosemirror-state";
 import { Decoration, DecorationSet } from "prosemirror-view";
-import { slashMenuPluginKey } from "../SlashMenu/SlashMenuPlugin";
-import { nodeToBlock } from "../../api/nodeConversions/nodeConversions";
 import type { BlockNoteEditor } from "../../editor/BlockNoteEditor";
 import { Block } from "../../schema";
+import { slashMenuPluginKey } from "../SlashMenu/SlashMenuPlugin";
 
 const PLUGIN_KEY = new PluginKey(`blocknote-placeholder`);
 
@@ -60,78 +59,37 @@ export const Placeholder = Extension.create<PlaceholderOptions>({
       new Plugin({
         key: PLUGIN_KEY,
         props: {
+          // TODO: maybe also add placeholder for empty document ("e.g.: start writing..")
           decorations: (state) => {
             const { doc, selection } = state;
-            // Get state of slash menu
+
+            // TODO: fix slash menu ("type to filter")
             const menuState = slashMenuPluginKey.getState(state);
+
             const active = this.editor.isEditable;
-            const { anchor } = selection;
-            const decorations: Decoration[] = [];
 
             if (!active) {
               return;
             }
 
-            doc.descendants((node, pos) => {
-              if (
-                node.type.spec.group !== "blockContent" ||
-                node.type.spec.content === ""
-              ) {
-                return true;
-              }
+            if (!selection.empty) {
+              return;
+            }
 
-              const isEmpty = !node.isLeaf && !node.childCount;
+            const $pos = selection.$anchor;
+            const node = $pos.parent;
 
-              if (isEmpty) {
-                const blockContainer = state.doc.resolve(pos).node();
-                const block = nodeToBlock(
-                  blockContainer,
-                  this.options.editor!.blockSchema,
-                  this.options.editor!.inlineContentSchema,
-                  this.options.editor!.styleSchema,
-                  this.options.editor!.blockCache
-                );
+            if (node.content.size > 0) {
+              return null;
+            }
 
-                const containsCursor =
-                  anchor >= pos && anchor <= pos + node.nodeSize;
-                const isFilter =
-                  menuState?.triggerCharacter === "" && menuState?.active;
+            const before = $pos.before();
 
-                const placeholder = this.options.placeholder(
-                  block,
-                  containsCursor,
-                  isFilter
-                );
-
-                if (placeholder !== undefined) {
-                  // Because we cannot set a decoration on the inline content
-                  // element itself (it's only counted as a distinct node in the
-                  // DOM, not in the PM schema), this is a hack to get the
-                  // placeholder value inside CSS.
-                  const decoration = Decoration.node(pos, pos + node.nodeSize, {
-                    style: `--placeholder: '${placeholder}'`,
-                    "data-placeholder": "",
-                  });
-                  decorations.push(decoration);
-                }
-
-                // using widget, didn't work (caret position bug)
-                // const decoration = Decoration.widget(
-                //   pos + 1,
-                //   () => {
-                //     const el = document.createElement("span");
-                //     el.innerText = "hello";
-                //     return el;
-                //   },
-                //   { side: 0 }
-
-                return false;
-              }
-
-              return true;
+            const dec = Decoration.node(before, before + node.nodeSize, {
+              "data-is-empty-and-focused": "true",
             });
 
-            return DecorationSet.create(doc, decorations);
+            return DecorationSet.create(doc, [dec]);
           },
         },
       }),
