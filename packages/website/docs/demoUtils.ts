8f803c1c3a807175b93a8d026ffced16e4a16253
Matthew Lipski <matthewlipski@gmail.com>
2024-01-26 18:12:42 +0100

    Fixed styles that were overwritten by Mantine globals

--- packages/website/docs/demoUtils.ts
+++ packages/website/docs/demoUtils.ts
@@ -14,5 +14,16 @@ export const getStyles = (isDark: Ref<boolean>): string => `body {
   -webkit-tap-highlight-color: transparent;
   -webkit-touch-callout: none;
   
-  background-color: ${isDark ? "#151515" : "white"};
+  background-color: ${isDark ? "#151515" : "white"} !important;
 }`;
+
+export const index = `import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);`;
