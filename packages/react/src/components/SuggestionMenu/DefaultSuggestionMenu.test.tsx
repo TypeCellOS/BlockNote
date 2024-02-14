ce78cc61ee505ef3079d69cff3ab7fc661b715f1
yousefed <yousefdardiry@gmail.com>
2024-02-14 22:11:35 +0100

    test

---
+++ packages/react/src/components/SuggestionMenu/DefaultSuggestionMenu.test.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { it } from "vitest";
+import { DefaultPositionedSuggestionMenu } from "./DefaultSuggestionMenu";
+
+it("has good typing", () => {
+  let menu = (
+    // @ts-expect-error
+    <DefaultPositionedSuggestionMenu
+      getItems={async () => [{ name: "hello" }]}
+      editor={undefined as any}
+    />
+  );
+
+  // valid, because getItems returns DefaultSuggestionItem so suggestionMenuComponent is optional
+  menu = (
+    <DefaultPositionedSuggestionMenu
+      getItems={async () => [{ title: "hello" }]}
+      editor={undefined as any}
+    />
+  );
+
+  // validate type of onItemClick
+  menu = (
+    <DefaultPositionedSuggestionMenu
+      suggestionMenuComponent={undefined as any}
+      getItems={async () => [{ hello: "hello" }]}
+      editor={undefined as any}
+      onItemClick={(item) => {
+        console.log(item.hello);
+      }}
+    />
+  );
+});
