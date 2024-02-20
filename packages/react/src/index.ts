43a5c1963605e097648d5deaa1295c19dbfcdefa
Matthew Lipski <matthewlipski@gmail.com>
2024-02-20 18:45:14 +0100

    Small fix

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -5,36 +5,36 @@ export * from "./editor/BlockNoteTheme";
 export * from "./editor/BlockNoteView";
 export * from "./editor/defaultThemes";
 
-export * from "./components/FormattingToolbar/DefaultButtons/ColorStyleButton";
-export * from "./components/FormattingToolbar/DefaultButtons/CreateLinkButton";
-export * from "./components/FormattingToolbar/DefaultButtons/ImageCaptionButton";
-export * from "./components/FormattingToolbar/DefaultButtons/NestBlockButtons";
-export * from "./components/FormattingToolbar/DefaultButtons/ReplaceImageButton";
-export * from "./components/FormattingToolbar/DefaultButtons/TextAlignButton";
-export * from "./components/FormattingToolbar/DefaultButtons/BasicTextStyleButton";
-export * from "./components/FormattingToolbar/DefaultDropdowns/BlockTypeDropdown";
-export * from "./components/FormattingToolbar/FormattingToolbar";
+export * from "./components/FormattingToolbar/mantine/DefaultButtons/ColorStyleButton";
+export * from "./components/FormattingToolbar/mantine/DefaultButtons/CreateLinkButton";
+export * from "./components/FormattingToolbar/mantine/DefaultButtons/ImageCaptionButton";
+export * from "./components/FormattingToolbar/mantine/DefaultButtons/NestBlockButtons";
+export * from "./components/FormattingToolbar/mantine/DefaultButtons/ReplaceImageButton";
+export * from "./components/FormattingToolbar/mantine/DefaultButtons/TextAlignButton";
+export * from "./components/FormattingToolbar/mantine/DefaultButtons/BasicTextStyleButton";
+export * from "./components/FormattingToolbar/mantine/DefaultDropdowns/BlockTypeDropdown";
+export * from "./components/FormattingToolbar/mantine/FormattingToolbar";
 export * from "./components/FormattingToolbar/FormattingToolbarProps";
 export * from "./components/FormattingToolbar/FormattingToolbarController";
 
-export * from "./components/HyperlinkToolbar/HyperlinkToolbar";
+export * from "./components/HyperlinkToolbar/mantine/HyperlinkToolbar";
 export * from "./components/HyperlinkToolbar/HyperlinkToolbarProps";
 export * from "./components/HyperlinkToolbar/HyperlinkToolbarController";
 
-export * from "./components/SideMenu/DefaultButtons/AddBlockButton";
-export * from "./components/SideMenu/DefaultButtons/DragHandle";
-export * from "./components/SideMenu/SideMenu";
+export * from "./components/SideMenu/mantine/DefaultButtons/AddBlockButton";
+export * from "./components/SideMenu/mantine/DefaultButtons/DragHandle";
+export * from "./components/SideMenu/mantine/SideMenu";
 export * from "./components/SideMenu/SideMenuProps";
 export * from "./components/SideMenu/SideMenuController";
-export * from "./components/SideMenu/SideMenuWrapper";
-export * from "./components/SideMenu/SideMenuButton";
+export * from "./components/SideMenu/mantine/SideMenuWrapper";
+export * from "./components/SideMenu/mantine/SideMenuButton";
 
-export * from "./components/SideMenu/DragHandleMenu/DefaultButtons/BlockColorsButton";
-export * from "./components/SideMenu/DragHandleMenu/DefaultButtons/RemoveBlockButton";
-export * from "./components/SideMenu/DragHandleMenu/DragHandleMenu";
+export * from "./components/SideMenu/DragHandleMenu/mantine/DefaultButtons/BlockColorsButton";
+export * from "./components/SideMenu/DragHandleMenu/mantine/DefaultButtons/RemoveBlockButton";
+export * from "./components/SideMenu/DragHandleMenu/mantine/DragHandleMenu";
 export * from "./components/SideMenu/DragHandleMenu/DragHandleMenuProps";
-export * from "./components/SideMenu/DragHandleMenu/DragHandleMenuWrapper";
-export * from "./components/SideMenu/DragHandleMenu/DragHandleMenuItem";
+export * from "./components/SideMenu/DragHandleMenu/mantine/DragHandleMenuWrapper";
+export * from "./components/SideMenu/DragHandleMenu/mantine/DragHandleMenuItem";
 
 export * from "./components/SuggestionMenu/getDefaultReactSlashMenuItems";
 export * from "./components/SuggestionMenu/hooks/useCloseSuggestionMenuNoItems";
@@ -46,11 +46,11 @@ export * from "./components/SuggestionMenu/mantine/SuggestionMenu";
 export * from "./components/SuggestionMenu/mantine/SuggestionMenuItem";
 export * from "./components/SuggestionMenu/types";
 
-export * from "./components/ImageToolbar/ImageToolbar";
+export * from "./components/ImageToolbar/mantine/ImageToolbar";
 export * from "./components/ImageToolbar/ImageToolbarProps";
 export * from "./components/ImageToolbar/ImageToolbarController";
 
-export * from "./components/TableHandles/TableHandle";
+export * from "./components/TableHandles/mantine/TableHandle";
 export * from "./components/TableHandles/TableHandleProps";
 export * from "./components/TableHandles/TableHandlesController";
 export * from "./components/TableHandles/hooks/useTableHandlesPositioning";
