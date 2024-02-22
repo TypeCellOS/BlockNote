940700ee05c9a6f0d61153f247e8ac95881762a2
yousefed <yousefdardiry@gmail.com>
2024-02-22 10:30:44 +0100

    remove files

--- examples/06-custom-schema/react-custom-styles/js/main.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-// AUTO-GENERATED FILE, DO NOT EDIT DIRECTLY
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-const root = createRoot(document.getElementById("root"));
-root.render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
