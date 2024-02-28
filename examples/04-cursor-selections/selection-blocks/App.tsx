cdd9677ea253b30ba701a689fb02a37f3b03e257
Matthew Lipski <matthewlipski@gmail.com>
2024-02-28 16:10:09 +0100

    Fixed demo input/output TODOs and removed hide last line hack

--- examples/04-cursor-selections/selection-blocks/App.tsx
+++ examples/04-cursor-selections/selection-blocks/App.tsx
@@ -3,6 +3,8 @@ import { BlockNoteView, useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/react/style.css";
 import { useState } from "react";
 
+import "./styles.css";
+
 export default function App() {
   // Stores the selected blocks as an array of Block objects.
   const [blocks, setBlocks] = useState<Block[]>([]);
@@ -11,24 +13,31 @@ export default function App() {
 
   // Renders the editor instance.
   return (
-    <div>
-      <BlockNoteView
-        editor={editor}
-        onSelectionChange={() => {
-          const selection = editor.getSelection();
+    <div className={"wrapper"}>
+      <div>BlockNote Editor:</div>
+      <div className={"item"}>
+        <BlockNoteView
+          editor={editor}
+          onSelectionChange={() => {
+            const selection = editor.getSelection();
 
-          // Get the blocks in the current selection and store on the state. If
-          // the selection is empty, store the block containing the text cursor
-          // instead.
-          if (selection !== undefined) {
-            setBlocks(selection.blocks);
-          } else {
-            setBlocks([editor.getTextCursorPosition().block]);
-          }
-        }}
-      />
-      <p>Selection JSON:</p>
-      <pre>{JSON.stringify(blocks, null, 2)}</pre>
+            // Get the blocks in the current selection and store on the state. If
+            // the selection is empty, store the block containing the text cursor
+            // instead.
+            if (selection !== undefined) {
+              setBlocks(selection.blocks);
+            } else {
+              setBlocks([editor.getTextCursorPosition().block]);
+            }
+          }}
+        />
+      </div>
+      <div>Selection JSON:</div>
+      <div className={"item bordered"}>
+        <pre>
+          <code>{JSON.stringify(blocks, null, 2)}</code>
+        </pre>
+      </div>
     </div>
   );
 }
