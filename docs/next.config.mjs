a5cd7f0eaf8cf655d67c69c80e52fa7db1939eb9
Matthew Lipski <matthewlipski@gmail.com>
2024-02-29 14:42:26 +0100

    Added docs pages redirects and misc changes

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -37,6 +37,88 @@ const nextConfig = withAnalyzer(
         },
       ],
     },
+    redirects: async () => [
+      {
+        source: "/docs/introduction",
+        destination: "/docs",
+        permanent: true,
+      },
+      {
+        source: "/docs/editor",
+        destination: "/docs/editor-basics/setup",
+        permanent: true,
+      },
+      {
+        source: "/docs/theming",
+        destination: "/docs/styling-theming",
+        permanent: true,
+      },
+      {
+        source: "/docs/formatting-toolbar",
+        destination: "/docs/ui-components/formatting-toolbar",
+        permanent: true,
+      },
+      {
+        source: "/docs/slash-menu",
+        destination: "/docs/ui-components/suggestion-menus",
+        permanent: true,
+      },
+      {
+        source: "/docs/side-menu",
+        destination: "/docs/ui-components/side-menu",
+        permanent: true,
+      },
+      {
+        source: "/docs/ui-elements",
+        destination: "/docs/ui-components",
+        permanent: true,
+      },
+      {
+        source: "/docs/blocks",
+        destination: "/docs/editor-basics/document-structure",
+        permanent: true,
+      },
+      {
+        source: "/docs/block-types",
+        destination: "/docs/editor-basics/default-schema",
+        permanent: true,
+      },
+      {
+        source: "/docs/manipulating-blocks",
+        destination: "/docs/editor-api/manipulating-blocks",
+        permanent: true,
+      },
+      {
+        source: "/docs/inline-content",
+        destination: "/docs/editor-api/manipulating-inline-content",
+        permanent: true,
+      },
+      {
+        source: "/docs/cursor-selections",
+        destination: "/docs/editor-api/cursor-selections",
+        permanent: true,
+      },
+      {
+        source: "/docs/converting-blocks",
+        destination: "/docs/editor-api/converting-blocks",
+        permanent: true,
+      },
+      {
+        source: "/docs/real-time-collaboration",
+        destination: "/docs/advanced/real-time-collaboration",
+        permanent: true,
+      },
+      {
+        source: "/docs/nextjs",
+        destination: "/docs/advanced/nextjs",
+        permanent: true,
+      },
+      {
+        source: "/docs/vanilla-js",
+        destination: "/docs/advanced/vanilla-js",
+        permanent: true,
+      },
+    ],
     experimental: {
       externalDir: true,
     },
--- examples/04-cursor-selections/selection-blocks/README.md
+++ examples/04-cursor-selections/selection-blocks/README.md
@@ -1,3 +1,7 @@
-# Highlighting Blocks in Selection
+# Displaying Selection/Text Cursor Position Blocks
 
-TODO: same as text-cursor block. Perhaps replace both by 1 single example
+In this example, below the editor, we display the blocks spanned by the user selection as they're represented in code - i.e. as a JSON array of `Block` objects. If no selection is active, the block containing the text cursor is displayed instead.
+
+**Relevant Docs:**
+
+- TODO
