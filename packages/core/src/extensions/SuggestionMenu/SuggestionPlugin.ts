16351c0b5b7b2c835c38cc3b336ffe406cc0337d
Matthew Lipski <50169049+matthewlipski@users.noreply.github.com>
2024-04-03 11:50:27 +0200

    Removed `keydown` handler from suggestion plugin (#682)

--- packages/core/src/extensions/SuggestionMenu/SuggestionPlugin.ts
+++ packages/core/src/extensions/SuggestionMenu/SuggestionPlugin.ts
@@ -245,30 +245,6 @@ export class SuggestionMenuProseMirrorPlugin<
       },
 
       props: {
-        handleKeyDown(view, event) {
-          const suggestionPluginState: SuggestionPluginState = (
-            this as Plugin
-          ).getState(view.state);
-
-          if (
-            triggerCharacters.includes(event.key) &&
-            suggestionPluginState === undefined
-          ) {
-            event.preventDefault();
-
-            view.dispatch(
-              view.state.tr
-                .insertText(event.key)
-                .scrollIntoView()
-                .setMeta(suggestionMenuPluginKey, {
-                  triggerCharacter: event.key,
-                })
-            );
-
-            return true;
-          }
-          return false;
-        },
         handleTextInput(view, _from, _to, text) {
           const suggestionPluginState: SuggestionPluginState = (
             this as Plugin
