14ad402fe512482e7d21d3db111a941aeec860a6
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-05-06 11:49:38 +0200

    feat: add french locales to dictionary

---
+++ packages/core/src/i18n/locales/fr.ts
@@ -0,0 +1,196 @@
+export const fr = {
+  slash_menu: {
+    heading: {
+      title: "Titre 1",
+      subtext: "Utilisé pour un titre de premier niveau",
+      aliases: ["h", "titre1", "h1"],
+      group: "Titres",
+    },
+    heading_2: {
+      title: "Titre 2",
+      subtext: "Utilisé pour les sections clés",
+      aliases: ["h2", "titre2", "sous-titre"],
+      group: "Titres",
+    },
+    heading_3: {
+      title: "Titre 3",
+      subtext: "Utilisé pour les sous-sections et les titres de groupe",
+      aliases: ["h3", "titre3", "sous-titre"],
+      group: "Titres",
+    },
+    numbered_list: {
+      title: "Liste Numérotée",
+      subtext: "Utilisé pour afficher une liste numérotée",
+      aliases: ["ol", "li", "liste", "listenumérotée", "liste numérotée"],
+      group: "Blocs de base",
+    },
+    bullet_list: {
+      title: "Liste à Puces",
+      subtext: "Utilisé pour afficher une liste non ordonnée",
+      aliases: ["ul", "li", "liste", "listeàpuces", "liste à puces"],
+      group: "Blocs de base",
+    },
+    paragraph: {
+      title: "Paragraphe",
+      subtext: "Utilisé pour le corps de votre document",
+      aliases: ["p", "paragraphe"],
+      group: "Blocs de base",
+    },
+    table: {
+      title: "Tableau",
+      subtext: "Utilisé pour les tableaux",
+      aliases: ["tableau"],
+      group: "Avancé",
+    },
+    image: {
+      title: "Image",
+      subtext: "Insérer une image",
+      aliases: [
+        "image",
+        "uploadImage",
+        "télécharger",
+        "img",
+        "photo",
+        "média",
+        "url",
+        "drive",
+        "dropbox",
+      ],
+      group: "Médias",
+    },
+  },
+  placeholders: {
+    default: "Entrez du texte ou tapez '/' pour les commandes",
+    heading: "Titre",
+    bulletListItem: "Liste",
+    numberedListItem: "Liste",
+  },
+  image: {
+    add_button: "Ajouter une Image",
+  },
+  // from react package:
+  side_menu: {
+    add_block_label: "Ajouter un bloc",
+    drag_handle_label: "Ouvrir le menu du bloc",
+  },
+  drag_handle: {
+    delete_menuitem: "Supprimer",
+    colors_menuitem: "Couleurs",
+  },
+  table_handle: {
+    delete_column_menuitem: "Supprimer la colonne",
+    delete_row_menuitem: "Supprimer la ligne",
+    add_left_menuitem: "Ajouter une colonne à gauche",
+    add_right_menuitem: "Ajouter une colonne à droite",
+    add_above_menuitem: "Ajouter une ligne au-dessus",
+    add_below_menuitem: "Ajouter une ligne en dessous",
+  },
+  suggestion_menu: {
+    no_items_title: "Aucun élément trouvé",
+    loading: "Chargement…",
+  },
+  color_picker: {
+    text_title: "Texte",
+    background_title: "Fond",
+    colors: {
+      default: "Défaut",
+      gray: "Gris",
+      brown: "Marron",
+      red: "Rouge",
+      orange: "Orange",
+      yellow: "Jaune",
+      green: "Vert",
+      blue: "Bleu",
+      purple: "Violet",
+      pink: "Rose",
+    },
+  },
+
+  formatting_toolbar: {
+    bold: {
+      tooltip: "Gras",
+      secondary_tooltip: "Mod+B",
+    },
+    italic: {
+      tooltip: "Italique",
+      secondary_tooltip: "Mod+I",
+    },
+    underline: {
+      tooltip: "Souligner",
+      secondary_tooltip: "Mod+U",
+    },
+    strike: {
+      tooltip: "Barré",
+      secondary_tooltip: "Mod+Shift+X",
+    },
+    code: {
+      tooltip: "Code",
+      secondary_tooltip: "",
+    },
+    colors: {
+      tooltip: "Couleurs",
+    },
+    link: {
+      tooltip: "Créer un lien",
+      secondary_tooltip: "Mod+K",
+    },
+    image_caption: {
+      tooltip: "Modifier la légende",
+      input_placeholder: "Modifier la légende",
+    },
+    image_replace: {
+      tooltip: "Remplacer l'image",
+    },
+    nest: {
+      tooltip: "Emboîter le bloc",
+      secondary_tooltip: "Tab",
+    },
+    unnest: {
+      tooltip: "Démboîter le bloc",
+      secondary_tooltip: "Shift+Tab",
+    },
+    align_left: {
+      tooltip: "Aligner le texte à gauche",
+    },
+    align_center: {
+      tooltip: "Aligner le texte au centre",
+    },
+    align_right: {
+      tooltip: "Aligner le texte à droite",
+    },
+    align_justify: {
+      tooltip: "Justifier le texte",
+    },
+  },
+  image_panel: {
+    upload: {
+      title: "Télécharger",
+      file_placeholder: "Télécharger l'image",
+      upload_error: "Erreur : Échec du téléchargement",
+    },
+    embed: {
+      title: "Intégrer",
+      embed_button: "Intégrer l'image",
+      url_placeholder: "Entrez l'URL",
+    },
+  },
+  link_toolbar: {
+    delete: {
+      tooltip: "Supprimer le lien",
+    },
+    edit: {
+      text: "Modifier le lien",
+      tooltip: "Modifier",
+    },
+    open: {
+      tooltip: "Ouvrir dans un nouvel onglet",
+    },
+    form: {
+      title_placeholder: "Modifier le titre",
+      url_placeholder: "Modifier l'URL",
+    },
+  },
+  generic: {
+    ctrl_shortcut: "Ctrl",
+  },
+};
