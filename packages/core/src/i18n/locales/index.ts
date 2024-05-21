f3327838ca5444ba8deaee325299704a9cfdd918
Dominik Łasiński <76247450+Dmkk01@users.noreply.github.com>
2024-05-21 12:40:10 +0200

    feat: add i18n support for pl-PL and is-IS (#752)
    * chore: add i18n support to pl and is
    
    * chore: use dictionary type
    
    ---------
    
    Co-authored-by: Matthew Lipski <50169049+matthewlipski@users.noreply.github.com>

--- packages/core/src/i18n/locales/index.ts
+++ packages/core/src/i18n/locales/index.ts
@@ -2,5 +2,7 @@ export * from "./en";
 export * from "./nl";
 export * from "./zh";
 export * from "./fr";
+export * from "./pl";
+export * from "./is";
 export * from "./vi";
 export * from "./pt-BR";
