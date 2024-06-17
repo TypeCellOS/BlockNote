9f122b490f8ab6228b9967321a7ff480244f5b19
hunxjunedo <junaidhunain6@gmail.com>
2024-06-18 02:19:57 +0500

    add more reasonable color to emojiMenu

--- packages/react/src/components/SuggestionMenu/emojisMenu.jsx
+++ packages/react/src/components/SuggestionMenu/emojisMenu.jsx
@@ -3,10 +3,10 @@ import { useEffect, useState } from 'react'
 
 export default function EmojiMenu({items, emojiInsert, selectedIndex}){
 
-    return (<div style={{display: 'grid', justifyItems: 'center', background: 'black', borderRadius: 20, gap: 7, padding: 20, maxHeight: '30vh', minWidth: '30vh', overflowY: 'scroll', gridTemplateColumns: 'repeat(10, 1fr)'}}>
+    return (<div style={{display: 'grid', justifyItems: 'center', background: '#1d1d1d', borderRadius: 20, gap: 7, padding: 20, maxHeight: '30vh', minWidth: '30vh', overflowY: 'scroll', gridTemplateColumns: 'repeat(10, 1fr)'}}>
 {
   items.map((item, index)=>(
-    <p key={index} style={{margin: 0, cursor: 'pointer', fontSize: 'large', background: selectedIndex === index ? 'cyan' : 'transparent'}} onClick={()=>(emojiInsert(item))}>{item}</p>
+    <p key={index} style={{margin: 0, padding: 4, borderRadius: 10, cursor: 'pointer', fontSize: 'large', background: selectedIndex === index ? 'rgb(70, 70, 70)' : 'transparent'}} onClick={()=>(emojiInsert(item))}>{item}</p>
   ))
 }
     </div>
