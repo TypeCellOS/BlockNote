92ada58bff9741beae332a56ec73c3063bd0084f
Matthew Lipski <matthewlipski@gmail.com>
2024-06-24 20:23:20 +0200

    Added comment

--- packages/shadcn/src/lib/utils.ts
+++ packages/shadcn/src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { extendTailwindMerge } from "tailwind-merge";
 
+// Ensures that `bn-` prefixed Tailwind classes are recognized as Tailwind
+// classes, so they can be merged correctly.
 const twMerge = extendTailwindMerge({
   prefix: "bn-",
 });
