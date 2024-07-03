d9562a2adfab261fc130ec50447619b79dd22114
Matthew Lipski <50169049+matthewlipski@users.noreply.github.com>
2024-07-03 17:55:49 +0200

    fix: Drag preview issues (#905)
    * Changed drag preview position from `left: -100000px;` to `z-index: -1;`
    
    * Fixed `appendChild` being called instead of `removeChild`

--- packages/core/src/extensions/SideMenu/SideMenuPlugin.ts
+++ packages/core/src/extensions/SideMenu/SideMenuPlugin.ts
@@ -158,7 +158,7 @@ function unsetDragImage(rootEl: Document | ShadowRoot) {
     if (rootEl instanceof ShadowRoot) {
       rootEl.removeChild(dragImageElement);
     } else {
-      rootEl.body.appendChild(dragImageElement);
+      rootEl.body.removeChild(dragImageElement);
     }
 
     dragImageElement = undefined;
