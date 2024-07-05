a4fdc9904a65072727aa44d0d7c74b7ade6f3c26
yousefed <yousefdardiry@gmail.com>
2024-07-05 12:02:22 +0200

    update comments

--- examples/07-server-side-processing/01-rendering-static-documents/README.md
+++ examples/07-server-side-processing/01-rendering-static-documents/README.md
@@ -1,6 +1,6 @@
 # Rendering static documents
 
-TODO
+This example shows how you can use HTML exported using the `blocksToFullHTML` and render it as a static document (a view-only document, without the editor). You can use this for example if you use BlockNote to edit blog posts in a CMS, but want to display non-editable static, published pages to end-users.
 
 **Relevant Docs:**
 
--- packages/react/src/editor/ElementRenderer.tsx
+++ packages/react/src/editor/ElementRenderer.tsx
@@ -1,6 +1,11 @@
 import { forwardRef, useImperativeHandle, useState } from "react";
 import { createPortal, flushSync } from "react-dom";
 
+/**
+ * A helper component to render a single element to a container so we can subsequently read the DOM / HTML contents
+ *
+ * This is useful so we can render arbitrary React elements (blocks) in the correct context (used by `ReactRenderUtil`)
+ */
 export const ElementRenderer = forwardRef<
   (node: React.ReactNode, container: HTMLElement) => void
 >((_props, ref) => {
