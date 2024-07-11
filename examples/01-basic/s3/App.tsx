abffa534887b44f0ae254e62e994a3a141d54627
matthewlipski <matthewlipski@gmail.com>
2024-07-11 10:59:08 +0200

    Minor comment formatting

--- examples/01-basic/s3/App.tsx
+++ examples/01-basic/s3/App.tsx
@@ -14,13 +14,14 @@ import "@blocknote/mantine/style.css";
 import { useCreateBlockNote } from "@blocknote/react";
 
 /**
- * SERVER Code. Normally, this part would be implemented on your server so you can
- * hide your AWS credentials and control access to your S3 bucket.
+ * SERVER Code. Normally, this part would be implemented on your server, so you
+ * can hide your AWS credentials and control access to your S3 bucket.
  *
- * (in our demo, we are using a public S3 bucket so everything can be done in the client)
+ * In our demo, we are using a public S3 bucket so everything can be done in
+ * the client.
  */
 
-// Set up the AWS SDK
+// Set up the AWS SDK.
 const client = new S3Client({
   region: "us-east-1",
   credentials: {
@@ -30,14 +31,14 @@ const client = new S3Client({
 });
 
 /**
- * The method on the server that generates a presigned URL for a PUT request
+ * The method on the server that generates a pre-signed URL for a PUT request.
  */
 const SERVER_createPresignedUrlPUT = (opts: {
   bucket: string;
   key: string;
 }) => {
-  // This function would normally be implemented on your server
-  // Of course, make sure the calling user is authenticated, etc.
+  // This function would normally be implemented on your server. Of course, you
+  // should make sure the calling user is authenticated, etc.
   const { bucket, key } = opts;
   const command = new PutObjectCommand({
     Bucket: bucket,
@@ -47,14 +48,14 @@ const SERVER_createPresignedUrlPUT = (opts: {
 };
 
 /**
- * The method on the server that generates a presigned URL for a GET request
+ * The method on the server that generates a pre-signed URL for a GET request.
  */
 const SERVER_createPresignedUrlGET = (opts: {
   bucket: string;
   key: string;
 }) => {
-  // This function would normally be implemented on your server
-  // Of course, make sure the calling user is authenticated, etc.
+  // This function would normally be implemented on your server. Of course, you
+  // should make sure the calling user is authenticated, etc.
   const { bucket, key } = opts;
   const command = new GetObjectCommand({
     Bucket: bucket,
@@ -70,14 +71,14 @@ export default function App() {
   const editor = useCreateBlockNote({
     uploadFile: async (file) => {
       /**
-       * This function is called by BlockNote whenever it wants to upload a file
-       * In this implementation, we are uploading the file to an S3 bucket by first requesting a
-       * upload URL from the server
+       * This function is called by BlockNote whenever it wants to upload a
+       * file. In this implementation, we are uploading the file to an S3 bucket
+       * by first requesting an upload URL from the server.
        */
       const bucket = "blocknote-demo";
       const key = file.name;
 
-      // Get a URL to upload to from the server
+      // Get a URL to upload to from the server.
       const signedUrl = await SERVER_createPresignedUrlPUT({
         bucket,
         key,
@@ -85,11 +86,11 @@ export default function App() {
 
       const headers: any = {};
       if (file?.type) {
-        // S3 requires setting the correct content type
+        // S3 requires setting the correct content type.
         headers["Content-Type"] = file!.type || "application/octet-stream";
       }
 
-      // Actually upload the file
+      // Actually upload the file.
       const uploaded = await fetch(signedUrl, {
         method: "PUT",
         body: file,
@@ -101,15 +102,17 @@ export default function App() {
       }
 
       // We store the URL in a custom format, in this case s3://bucket/key.
-      // We'll subsequently parse this URL in the resolveFileUrl function
+      // We'll subsequently parse this URL in the resolveFileUrl function.
       return `s3://${bucket}/${key}`;
     },
     resolveFileUrl: async (url) => {
       /**
-       * This function is called by BlockNote whenever it needs to use URL from a file block,
-       * for example, when displaying an image or downloading a file.
+       * This function is called by BlockNote whenever it needs to use URL from
+       * a file block. For example, when displaying an image or downloading a
+       * file.
        *
-       * In this implementation, we are parsing our custom format and return a signed URL from our backend.
+       * In this implementation, we are parsing our custom format and return a
+       * signed URL from our backend.
        */
       if (url.startsWith("s3:")) {
         // it's our custom format, request a signed url from the backend
@@ -129,6 +132,6 @@ export default function App() {
   return <BlockNoteView editor={editor} />;
 }
 
-// This is a hack to make sure the S3RequestPresigner is not used (our demo bucket is configured for anonymous access)
-// Remove this in your own code
+// This is a hack to make sure the S3RequestPresigner is not used (our demo
+// bucket is configured for anonymous access). Remove this in your own code.
 S3RequestPresigner.prototype.presign = (request: any) => request;
