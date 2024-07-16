a0b7a2b15f349c0c4b50561d12b1b3b4a4422f4c
yousefed <yousefdardiry@gmail.com>
2024-07-16 12:43:43 +0200

    improve emoji loading

--- packages/core/src/extensions/SuggestionMenu/getDefaultEmojiPickerItems.ts
+++ packages/core/src/extensions/SuggestionMenu/getDefaultEmojiPickerItems.ts
@@ -1,13 +1,16 @@
-import data, { Emoji, EmojiMartData } from "@emoji-mart/data";
-import { init, SearchIndex } from "emoji-mart";
+import type { Emoji, EmojiMartData } from "@emoji-mart/data";
+import { SearchIndex, init } from "emoji-mart";
 
-import { BlockSchema, InlineContentSchema, StyleSchema } from "../../schema";
-import { BlockNoteEditor } from "../../editor/BlockNoteEditor";
 import { checkDefaultInlineContentTypeInSchema } from "../../blocks/defaultBlockTypeGuards";
+import { BlockNoteEditor } from "../../editor/BlockNoteEditor";
+import { BlockSchema, InlineContentSchema, StyleSchema } from "../../schema";
 import { DefaultGridSuggestionItem } from "./DefaultGridSuggestionItem";
 
-let dataInitialized = false;
-const emojiMartData = data as EmojiMartData;
+let data:
+  | Promise<{
+      default: EmojiMartData;
+    }>
+  | undefined;
 
 export async function getDefaultEmojiPickerItems<
   BSchema extends BlockSchema,
@@ -21,17 +24,22 @@ export async function getDefaultEmojiPickerItems<
     return [];
   }
 
-  if (!dataInitialized) {
-    dataInitialized = true;
-    await init({ emojiMartData });
+  if (!data) {
+    // use a dynamic import to encourage bundle-splitting
+    // and a smaller initial client bundle size
+    data = import("@emoji-mart/data", { assert: { type: "json" } }) as any;
+    const emojiMartData = (await data)!.default;
+    await init({ data: emojiMartData });
   }
+
+  const emojiMartData = (await data)!.default;
 
   const emojisToShow =
     query.trim() === ""
       ? Object.values(emojiMartData.emojis)
       : ((await SearchIndex.search(query)) as Emoji[]);
 
-  return emojisToShow.map((emoji: Emoji) => ({
+  return emojisToShow.map((emoji) => ({
     id: emoji.skins[0].native,
     onItemClick: () => editor.insertInlineContent(emoji.skins[0].native + " "),
   }));
