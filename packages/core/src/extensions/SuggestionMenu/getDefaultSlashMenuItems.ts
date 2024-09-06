4e87349979ad77cf9109ab2d042e2396dc53d0bd
Ethan Chen <38372737+jkcs@users.noreply.github.com>
2024-09-06 19:56:41 +0800

    fix: Emoji suggestion slash menu does not display (#984)
    * fix #975
    
    * fix: Unexpected show GridSuggestionMenu
    
    * feat: add payload state in SuggestionPlugin.ts
    
    * feat: rename SuggestionPlugin pluginState

--- packages/core/src/extensions/SuggestionMenu/getDefaultSlashMenuItems.ts
+++ packages/core/src/extensions/SuggestionMenu/getDefaultSlashMenuItems.ts
@@ -272,7 +272,12 @@ export function getDefaultSlashMenuItems<
   }
 
   items.push({
-    onItemClick: () => editor.openSuggestionMenu(":"),
+    onItemClick: () => {
+      editor.openSuggestionMenu(":", {
+        deleteTriggerCharacter: true,
+        ignoreQueryLength: true,
+      });
+    },
     key: "emoji",
     ...editor.dictionary.slash_menu.emoji,
   });
