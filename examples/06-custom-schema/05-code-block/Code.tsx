5ef365714dddfbe3b444f0026576b3ed24802fa2
Jaeung Jang <wodndckato@gmail.com>
2024-10-04 00:36:13 +0000

    feat: creating custom dropdown search for selecting language

--- examples/06-custom-schema/05-code-block/Code.tsx
+++ examples/06-custom-schema/05-code-block/Code.tsx
@@ -1,16 +1,125 @@
 "use client";
 
-import { defaultProps } from "@blocknote/core";
+import { useState } from "react";
+import { ReactCustomBlockRenderProps } from "@blocknote/react";
+import {
+  defaultProps,
+  PropSchema,
+  BlockConfig,
+  CustomBlockConfig,
+} from "@blocknote/core";
 import { createReactBlockSpec } from "@blocknote/react";
 import CodeMirror from "@uiw/react-codemirror";
-import { NativeSelect } from "@mantine/core";
 import {
   langs,
   langNames,
   LanguageName,
 } from "@uiw/codemirror-extensions-langs";
 import { material } from "@uiw/codemirror-theme-material";
+import {
+  CheckIcon,
+  Group,
+  Combobox,
+  Input,
+  InputBase,
+  useCombobox,
+} from "@mantine/core";
 import "./styles.css";
+
+// // Custom block configuration
+type CustomCodeBlockConfig = {
+  type: string;
+  readonly propSchema: {
+    language: {
+      default: string;
+    };
+    data: {
+      default: string;
+    };
+  };
+  content: "none";
+  isSelectable?: boolean;
+  isFileBlock: false;
+};
+
+// Dropdown search component for selecting language
+// export const SelectDropdownSearch = (props: any
+export const SelectDropdownSearch = (
+  props: Omit<
+    ReactCustomBlockRenderProps<CustomCodeBlockConfig, any, any>,
+    "contentRef"
+  >
+) => {
+  const [search, setSearch] = useState("");
+  const [value, setValue] = useState<string>(props.block.props.language);
+
+  const combobox = useCombobox({
+    onDropdownClose: () => {
+      combobox.resetSelectedOption();
+      combobox.focusTarget();
+      setSearch("");
+    },
+
+    onDropdownOpen: () => {
+      combobox.focusSearchInput();
+    },
+  });
+
+  // options for the dropdown from CodeMirror language names
+  const options = langNames
+    .filter((item) => item.toLowerCase().includes(search.toLowerCase().trim()))
+    .map((item) => (
+      <Combobox.Option value={item} key={item} active={item === value}>
+        <Group gap="xs">
+          {item === value && <CheckIcon size={12} />}
+          <span>{item}</span>
+        </Group>
+      </Combobox.Option>
+    ));
+
+  return (
+    <Combobox
+      store={combobox}
+      withinPortal={false}
+      onOptionSubmit={(val) => {
+        setValue(val);
+        props.editor.updateBlock(props.block, {
+          props: {
+            ...props.block.props,
+            language: val,
+          },
+        });
+        combobox.closeDropdown();
+      }}>
+      <Combobox.Target>
+        <InputBase
+          component="button"
+          type="button"
+          pointer
+          rightSection={<Combobox.Chevron />}
+          onClick={() => combobox.toggleDropdown()}
+          rightSectionPointerEvents="none">
+          {value || <Input.Placeholder>Pick language</Input.Placeholder>}
+        </InputBase>
+      </Combobox.Target>
+
+      <Combobox.Dropdown>
+        <Combobox.Search
+          value={search}
+          onChange={(event) => setSearch(event.currentTarget.value)}
+          placeholder="Search language..."
+        />
+        <Combobox.Options mah={200} style={{ overflowY: "auto" }}>
+          {options.length > 0 ? (
+            options
+          ) : (
+            <Combobox.Empty>No language found.</Combobox.Empty>
+          )}
+        </Combobox.Options>
+      </Combobox.Dropdown>
+    </Combobox>
+  );
+};
 
 export const Code = createReactBlockSpec(
   {
@@ -31,16 +140,9 @@ export const Code = createReactBlockSpec(
     render: (props) => {
       const { data } = props.block.props;
       const { language } = props.block.props;
-      const onInputChange = (val: string) => {
-        props.editor.updateBlock(props.block, {
-          props: {
-            ...props.block.props,
-            data: val,
-          },
-        });
-      };
 
-      const onLanguageChange = (val: string) => {
+      // event handler for input change
+      const onInputChange = (val: string) => {
         props.editor.updateBlock(props.block, {
           props: {
             ...props.block.props,
@@ -50,14 +152,17 @@ export const Code = createReactBlockSpec(
       };
 
       return (
+        // wrapper for Code block
         <div className={"code-block"}>
+          {/* section to display currently selected language */}
           <div className={"code-block-language-wrapper"}>
-            <NativeSelect
-              data={langNames}
-              defaultValue={props.block.props.language}
-              onChange={(e) => onLanguageChange(e.target.value)}
+            <SelectDropdownSearch
+              block={props.block}
+              editor={props.editor as any}
             />
           </div>
+
+          {/* component to display code using Codemirror */}
           <CodeMirror
             id={props.block.id}
             placeholder={"Write your code here..."}
