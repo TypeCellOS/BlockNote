85521dffcad3baea55eff2e8abe644ae7fa06dd0
yousefed <yousefdardiry@gmail.com>
2024-11-19 15:01:31 +0100

    revert minimal

--- examples/01-basic/01-minimal/App.tsx
+++ examples/01-basic/01-minimal/App.tsx
@@ -1,35 +1,12 @@
 import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
-import "@liveblocks/react-ui/../styles.css";
-
-import "@liveblocks/react-tiptap/../styles.css";
-
-import { Editor } from "./Editor.jsx";
-import { Room } from "./Room.jsx";
-import "./globals.css";
+import { useCreateBlockNote } from "@blocknote/react";
 
 export default function App() {
-  // Renders the editor instance using a React component.
-  return (
-    <Room>
-      <Editor />
-    </Room>
-  );
-}
+  // Creates a new editor instance.
+  const editor = useCreateBlockNote();
 
-/**
- *
- * TODO:
- * - blocking: DOM updates
- * - fix mount issue
- * - - (domelement)
- * - - make with with queueMicrotask
- * - versioning
- * - automatic comment button?
- * - Even simpler API?
- * - History
- * - mentions:
- * -- paste handler
- * -- "change" API to trigger code from document changes
- * -- hook up blocknote mentions, create default mention API that adds inline content + menu
- */
+  // Renders the editor instance using a React component.
+  return <BlockNoteView editor={editor} />;
+}
