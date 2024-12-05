ffb07e54be1bde882d5a1a67ff8ce735045d1c32
matthewlipski <matthewlipski@gmail.com>
2024-12-05 11:54:19 +0100

    Fixed lint

--- packages/core/src/util/isAllowedUri.ts
+++ packages/core/src/util/isAllowedUri.ts
@@ -2,6 +2,7 @@ import { LinkOptions } from "@tiptap/extension-link";
 
 // From https://github.com/ueberdosis/tiptap/blob/a0d2f2803652851bbe2f06f124a70bc01cfb0dab/packages/extension-link/src/link.ts#L161
 const ATTR_WHITESPACE =
+  // eslint-disable-next-line no-control-regex
   /[\u0000-\u0020\u00A0\u1680\u180E\u2000-\u2029\u205F\u3000]/g;
 
 export function isAllowedUri(
@@ -32,18 +33,16 @@ export function isAllowedUri(
     });
   }
 
-  // eslint-disable-next-line no-useless-escape
   return (
     !uri ||
-    uri
-      .replace(ATTR_WHITESPACE, "")
-      .match(
-        new RegExp(
-          `^(?:(?:${allowedProtocols.join(
-            "|"
-          )}):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))`,
-          "i"
-        )
+    uri.replace(ATTR_WHITESPACE, "").match(
+      new RegExp(
+        `^(?:(?:${allowedProtocols.join(
+          "|"
+          // eslint-disable-next-line no-useless-escape
+        )}):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))`,
+        "i"
       )
+    )
   );
 }
