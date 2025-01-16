9d35f72b7d0214b781b985ecad5f2ec2f24e12e3
yousefed <yousefdardiry@gmail.com>
2025-01-16 15:02:08 +0100

    document recommended auth rules

--- packages/core/src/extensions/Comments/store/ThreadStore.ts
+++ packages/core/src/extensions/Comments/store/ThreadStore.ts
@@ -1,6 +1,24 @@
 import { CommentBody, CommentData, ThreadData } from "../types.js";
 
+/**
+ * ThreadStore is an abstract class that defines the interface
+ * to read / add / update / delete threads and comments.
+ *
+ * The methods are annotated with the recommended auth pattern
+ * (but of course this could be different in your app):
+ * - View-only users should not be able to see any comments
+ * - Comment-only users and editors can:
+ * - - create new comments / replies / reactions
+ * - - edit / delete their own comments / reactions
+ * - - resolve / unresolve threads
+ * - Editors can also delete any comment or thread
+ */
 export abstract class ThreadStore {
+  /**
+   * Creates a new thread with an initial comment.
+   *
+   * Auth: should be possible by anyone with comment access
+   */
   abstract createThread(options: {
     initialComment: {
       body: CommentBody;
@@ -9,6 +27,11 @@ export abstract class ThreadStore {
     metadata?: any;
   }): Promise<ThreadData>;
 
+  /**
+   * Adds a comment to a thread.
+   *
+   * Auth: should be possible by anyone with comment access
+   */
   abstract addComment(options: {
     comment: {
       body: CommentBody;
@@ -17,6 +40,11 @@ export abstract class ThreadStore {
     threadId: string;
   }): Promise<CommentData>;
 
+  /**
+   * Updates a comment in a thread.
+   *
+   * Auth: should only be possible by the comment author
+   */
   abstract updateComment(options: {
     comment: {
       body: CommentBody;
@@ -26,23 +54,53 @@ export abstract class ThreadStore {
     commentId: string;
   }): Promise<void>;
 
+  /**
+   * Deletes a comment from a thread.
+   *
+   * Auth: should be possible by the comment author OR an editor of the document
+   */
   abstract deleteComment(options: {
     threadId: string;
     commentId: string;
   }): Promise<void>;
 
+  /**
+   * Deletes a thread.
+   *
+   * Auth: should only be possible by an editor of the document
+   */
   abstract deleteThread(options: { threadId: string }): Promise<void>;
 
+  /**
+   * Marks a thread as resolved.
+   *
+   * Auth: should be possible by anyone with comment access
+   */
   abstract resolveThread(options: { threadId: string }): Promise<void>;
 
+  /**
+   * Marks a thread as unresolved.
+   *
+   * Auth: should be possible by anyone with comment access
+   */
   abstract unresolveThread(options: { threadId: string }): Promise<void>;
 
+  /**
+   * Adds a reaction to a comment.
+   *
+   * Auth: should be possible by anyone with comment access
+   */
   abstract addReaction(options: {
     threadId: string;
     commentId: string;
     // reaction: string; TODO
   }): Promise<void>;
 
+  /**
+   * Deletes a reaction from a comment.
+   *
+   * Auth: should be possible by the reaction author
+   */
   abstract deleteReaction(options: {
     threadId: string;
     commentId: string;
