2d6c856de63edfc2c4b680ef3e6634a6052ce49a
yousefed <yousefdardiry@gmail.com>
2025-02-17 11:52:51 +0100

    additional cleanup

--- packages/core/src/editor/BlockNoteTipTapEditor.ts
+++ packages/core/src/editor/BlockNoteTipTapEditor.ts
@@ -193,12 +193,3 @@ export class BlockNoteTipTapEditor extends TiptapEditor {
     }
   };
 }
-
-(BlockNoteTipTapEditor.prototype as any).createView = function () {
-  // no-op
-  // Disable default call to `createView` in the Editor constructor.
-  // We should call `createView` manually only when a DOM element is available
-
-  // additional fix because onPaste and onDrop depend on installing plugins in constructor which we don't support
-  this.options.onPaste = this.options.onDrop = undefined;
-};
