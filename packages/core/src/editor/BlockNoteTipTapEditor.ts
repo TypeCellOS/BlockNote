10d15a0651af270ca4cef6798373f30b86940bbc
yousefed <yousefdardiry@gmail.com>
2025-02-17 11:54:17 +0100

    fix

--- packages/core/src/editor/BlockNoteTipTapEditor.ts
+++ packages/core/src/editor/BlockNoteTipTapEditor.ts
@@ -193,3 +193,9 @@ export class BlockNoteTipTapEditor extends TiptapEditor {
     }
   };
 }
+
+(BlockNoteTipTapEditor.prototype as any).createView = function () {
+  // no-op
+  // Disable default call to `createView` in the Editor constructor.
+  // We should call `createView` manually only when a DOM element is available
+};
