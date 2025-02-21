c6af87e1a0a07125a05d92b559c45c557fe2cd8c
Nick the Sick <nick@blocknotejs.org>
2025-02-21 14:32:27 +0100

    fix: minor bug fix

--- packages/react/src/components/TableHandles/TableHandleMenu/DefaultButtons/ColorPicker.tsx
+++ packages/react/src/components/TableHandles/TableHandleMenu/DefaultButtons/ColorPicker.tsx
@@ -73,7 +73,7 @@ export const ColorPickerButton = <
     editor.setTextCursorPosition(props.block);
   };
 
-  if (!currentCells || !tableHandles) {
+  if (!currentCells || !currentCells[0] || !tableHandles) {
     return null;
   }
 
