31408ea6bc5930a5260c961273742fab3035d940
Nick the Sick <nick@blocknotejs.org>
2025-02-27 14:03:11 +0100

    feat: allow toggling the header row/col via the table row/col handle menu

--- packages/react/src/components/TableHandles/TableHandleMenu/TableHandleMenu.tsx
+++ packages/react/src/components/TableHandles/TableHandleMenu/TableHandleMenu.tsx
@@ -11,6 +11,8 @@ import { AddButton } from "./DefaultButtons/AddButton.js";
 import { DeleteButton } from "./DefaultButtons/DeleteButton.js";
 import { TableHandleMenuProps } from "./TableHandleMenuProps.js";
 import { ColorPickerButton } from "./DefaultButtons/ColorPicker.js";
+import { TableHeaderColumnButton } from "./DefaultButtons/TableHeaderButton.js";
+import { TableHeaderRowButton } from "./DefaultButtons/TableHeaderButton.js";
 
 export const TableHandleMenu = <
   I extends InlineContentSchema = DefaultInlineContentSchema,
@@ -40,6 +42,16 @@ export const TableHandleMenu = <
             block={props.block}
             index={props.index}
             side={props.orientation === "row" ? "below" : ("right" as any)}
+          />
+          <TableHeaderRowButton
+            orientation={props.orientation}
+            block={props.block}
+            index={props.index}
+          />
+          <TableHeaderColumnButton
+            orientation={props.orientation}
+            block={props.block}
+            index={props.index}
           />
           <ColorPickerButton
             orientation={props.orientation}
