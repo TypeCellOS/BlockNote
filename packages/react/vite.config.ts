3ada6423c374ab2a50473048359799dc0d1353a2
Yousef <yousefdardiry@gmail.com>
2025-03-04 13:35:42 +0100

    fix: externalize react-dom/client (#1483)

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -38,6 +38,7 @@ export default defineConfig((conf) => ({
           ...pkg.peerDependencies,
           ...pkg.devDependencies,
         }),
+        "react-dom/client",
         "react/jsx-runtime",
       ],
       output: {
