7f61926f9e7fd4fb8c2fd261e9ed4bd0a0633937
yousefed <yousefdardiry@gmail.com>
2025-03-04 23:01:44 +0100

    fix deploy

--- packages/core/src/api/clipboard/fromClipboard/pasteExtension.ts
+++ packages/core/src/api/clipboard/fromClipboard/pasteExtension.ts
@@ -8,42 +8,10 @@ import {
   StyleSchema,
 } from "../../../schema/index.js";
 import { nestedListsToBlockNoteStructure } from "../../parsers/html/util/nestedLists.js";
+import { markdownToHTML } from "../../parsers/markdown/parseMarkdown.js";
 import { acceptedMIMETypes } from "./acceptedMIMETypes.js";
 import { handleFileInsertion } from "./handleFileInsertion.js";
 import { handleVSCodePaste } from "./handleVSCodePaste.js";
-import {
-  markdownToBlocks,
-  markdownToHTML,
-} from "../../parsers/markdown/parseMarkdown.js";
-import { Block } from "../../../blocks/defaultBlocks.js";
-import { PartialBlock } from "../../../blocks/defaultBlocks.js";
-
-function insertOrUpdateBlock<
-  BSchema extends BlockSchema,
-  I extends InlineContentSchema,
-  S extends StyleSchema
->(
-  editor: BlockNoteEditor<BSchema, I, S>,
-  referenceBlock: Block<BSchema, I, S>,
-  newBlock: PartialBlock<BSchema, I, S>
-) {
-  let insertedBlockId: string | undefined;
-
-  if (
-    Array.isArray(referenceBlock.content) &&
-    referenceBlock.content.length === 0
-  ) {
-    insertedBlockId = editor.updateBlock(referenceBlock, newBlock).id;
-  } else {
-    insertedBlockId = editor.insertBlocks(
-      [newBlock],
-      referenceBlock,
-      "after"
-    )[0].id;
-  }
-
-  return insertedBlockId;
-}
 
 export const createPasteFromClipboardExtension = <
   BSchema extends BlockSchema,
