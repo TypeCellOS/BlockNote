6e50d3a6c32ae7a630f76585194bae4b31ffd1ea
Nick the Sick <nick@blocknotejs.org>
2025-03-05 12:05:49 +0100

    feat: add sentry to website for error detection

---
+++ docs/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://227b9bda1101ccf4be54a40f8fd89996@o4508924767240192.ingest.de.sentry.io/4508924767633488",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
