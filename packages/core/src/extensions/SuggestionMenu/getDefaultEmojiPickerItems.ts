addd923bf382b094ac096425fe2d2dac7b3789d5
Nick the Sick <nick@blocknotejs.org>
2025-03-05 14:45:45 +0100

    fix: load emoji data dynamically and initialize emoji mart with that data

--- packages/core/src/extensions/SuggestionMenu/getDefaultEmojiPickerItems.ts
+++ packages/core/src/extensions/SuggestionMenu/getDefaultEmojiPickerItems.ts
@@ -9,13 +9,32 @@ import {
 } from "../../schema/index.js";
 import { DefaultGridSuggestionItem } from "./DefaultGridSuggestionItem.js";
 
-let data:
-  | Promise<{
-      default: EmojiMartData;
-    }>
-  | undefined;
-
+// Temporary fix for https://github.com/missive/emoji-mart/pull/929
 let emojiMart: typeof import("emoji-mart") | undefined;
+let emojiData: EmojiMartData | undefined;
+
+async function loadEmojiMart() {
+  if (!emojiMart || !emojiData) {
+    // load dynamically because emoji-mart doesn't specify type: module and breaks in nodejs
+    const [emojiMartModule, emojiDataModule] = await Promise.all([
+      import("emoji-mart"),
+      // use a dynamic import to encourage bundle-splitting
+      // and a smaller initial client bundle size
+      import("@emoji-mart/data"),
+    ]);
+
+    emojiMart =
+      "default" in emojiMartModule ? emojiMartModule.default : emojiMartModule;
+    emojiData =
+      "default" in emojiDataModule
+        ? (emojiDataModule.default as EmojiMartData)
+        : (emojiDataModule as EmojiMartData);
+
+    emojiMart.init({ data: emojiData });
+  }
+
+  return { emojiMart, emojiData };
+}
 
 export async function getDefaultEmojiPickerItems<
   BSchema extends BlockSchema,
@@ -29,23 +48,11 @@ export async function getDefaultEmojiPickerItems<
     return [];
   }
 
-  if (!data) {
-    // use a dynamic import to encourage bundle-splitting
-    // and a smaller initial client bundle size
-
-    data = import("@emoji-mart/data") as any;
-
-    // load dynamically because emoji-mart doesn't specify type: module and breaks in nodejs
-    emojiMart = await import("emoji-mart");
-    const emojiMartData = (await data)!.default;
-    await emojiMart.init({ data: emojiMartData });
-  }
-
-  const emojiMartData = (await data)!.default;
+  const { emojiData } = await loadEmojiMart();
 
   const emojisToShow =
     query.trim() === ""
-      ? Object.values(emojiMartData.emojis)
+      ? Object.values(emojiData.emojis)
       : ((await emojiMart!.SearchIndex.search(query)) as Emoji[]);
 
   return emojisToShow.map((emoji) => ({
--- packages/react/src/components/Comments/EmojiMartPicker.tsx
+++ packages/react/src/components/Comments/EmojiMartPicker.tsx
@@ -1,8 +1,33 @@
 // From https://github.com/missive/emoji-mart/blob/main/packages/emoji-mart-react/react.tsx
+import type { EmojiMartData } from "@emoji-mart/data";
 import React, { useEffect, useRef } from "react";
 
 // Temporary fix for https://github.com/missive/emoji-mart/pull/929
 let emojiMart: typeof import("emoji-mart") | undefined;
+let emojiData: EmojiMartData | undefined;
+
+async function loadEmojiMart() {
+  if (!emojiMart || !emojiData) {
+    // load dynamically because emoji-mart doesn't specify type: module and breaks in nodejs
+    const [emojiMartModule, emojiDataModule] = await Promise.all([
+      import("emoji-mart"),
+      // use a dynamic import to encourage bundle-splitting
+      // and a smaller initial client bundle size
+      import("@emoji-mart/data"),
+    ]);
+
+    emojiMart =
+      "default" in emojiMartModule ? emojiMartModule.default : emojiMartModule;
+    emojiData =
+      "default" in emojiDataModule
+        ? (emojiDataModule.default as EmojiMartData)
+        : (emojiDataModule as EmojiMartData);
+
+    emojiMart.init({ data: emojiData });
+  }
+
+  return { emojiMart, emojiData };
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function EmojiPicker(props: any) {
@@ -15,10 +40,7 @@ export default function EmojiPicker(props: any) {
 
   useEffect(() => {
     (async () => {
-      if (!emojiMart) {
-        // load dynamically because emoji-mart doesn't specify type: module and breaks in nodejs
-        emojiMart = await import("emoji-mart");
-      }
+      const { emojiMart } = await loadEmojiMart();
 
       instance.current = new emojiMart.Picker({ ...props, ref });
     })();
