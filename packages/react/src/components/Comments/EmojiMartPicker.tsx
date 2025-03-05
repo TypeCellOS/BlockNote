eb52684479688ee1110a61c8b618890cc49b2326
Nick Perez <nick@blocknotejs.org>
2025-03-05 16:54:40 +0100

    fix: import emoji-mart dynamically like (#950) (#1491)

--- packages/react/src/components/Comments/EmojiMartPicker.tsx
+++ packages/react/src/components/Comments/EmojiMartPicker.tsx
@@ -1,6 +1,43 @@
 // From https://github.com/missive/emoji-mart/blob/main/packages/emoji-mart-react/react.tsx
+import type { EmojiMartData } from "@emoji-mart/data";
 import React, { useEffect, useRef } from "react";
-import { Picker } from "emoji-mart";
+
+// Temporary fix for https://github.com/missive/emoji-mart/pull/929
+let emojiLoadingPromise:
+  | Promise<{
+      emojiMart: typeof import("emoji-mart");
+      emojiData: EmojiMartData;
+    }>
+  | undefined;
+
+async function loadEmojiMart() {
+  if (emojiLoadingPromise) {
+    return emojiLoadingPromise;
+  }
+
+  emojiLoadingPromise = (async () => {
+    // load dynamically because emoji-mart doesn't specify type: module and breaks in nodejs
+    const [emojiMartModule, emojiDataModule] = await Promise.all([
+      import("emoji-mart"),
+      // use a dynamic import to encourage bundle-splitting
+      // and a smaller initial client bundle size
+      import("@emoji-mart/data"),
+    ]);
+
+    const emojiMart =
+      "default" in emojiMartModule ? emojiMartModule.default : emojiMartModule;
+    const emojiData =
+      "default" in emojiDataModule
+        ? (emojiDataModule.default as EmojiMartData)
+        : (emojiDataModule as EmojiMartData);
+
+    await emojiMart.init({ data: emojiData });
+
+    return { emojiMart, emojiData };
+  })();
+
+  return emojiLoadingPromise;
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function EmojiPicker(props: any) {
@@ -12,7 +49,11 @@ export default function EmojiPicker(props: any) {
   }
 
   useEffect(() => {
-    instance.current = new Picker({ ...props, ref });
+    (async () => {
+      const { emojiMart } = await loadEmojiMart();
+
+      instance.current = new emojiMart.Picker({ ...props, ref });
+    })();
 
     return () => {
       instance.current = null;
