a7308cb0d1996685c1b4ec3efa988d185d222f5e
Nick the Sick <nick@blocknotejs.org>
2025-03-24 22:20:12 +0100

    chore: fix import

--- docs/components/pages/landing/hero/DemoEditor.tsx
+++ docs/components/pages/landing/hero/DemoEditor.tsx
@@ -2,9 +2,9 @@ import {
   BlockNoteSchema,
   combineByGroup,
   filterSuggestionItems,
-  locales,
   uploadToTmpFilesDotOrg_DEV_ONLY,
 } from "@blocknote/core";
+import { locales } from "@blocknote/core/locales";
 import "@blocknote/core/fonts/inter.css";
 import {
   getDefaultReactSlashMenuItems,
