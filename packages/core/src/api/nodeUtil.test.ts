8948cf542f690d696b28c4d76d762972faeff97f
Nick the Sick <nick@blocknotejs.org>
2025-04-07 11:17:29 +0200

    test: add even more tests

--- packages/core/src/api/nodeUtil.test.ts
+++ packages/core/src/api/nodeUtil.test.ts
@@ -4,6 +4,7 @@ import { setupTestEnv } from "./blockManipulation/setupTestEnv.js";
 import { getBlocksChangedByTransaction } from "./nodeUtil.js";
 import { Transaction } from "prosemirror-state";
 import { BlockNoteEditor } from "../editor/BlockNoteEditor.js";
+import { Step } from "prosemirror-transform";
 
 const getEditor = setupTestEnv();
 
@@ -13,10 +14,18 @@ describe("Test getBlocksChangedByTransaction", () => {
   let originalDispatch: typeof editor.dispatch;
 
   beforeEach(() => {
+    transaction = undefined as unknown as Transaction;
     editor = getEditor();
     originalDispatch = editor.dispatch;
     const mockDispatch = vi.fn((tr) => {
-      transaction = tr;
+      editor._tiptapEditor.dispatch(tr);
+      if (transaction) {
+        tr.steps.forEach((step: Step) => {
+          transaction.step(step);
+        });
+      } else {
+        transaction = tr;
+      }
     });
     editor.dispatch = mockDispatch;
   });
@@ -134,6 +143,218 @@ describe("Test getBlocksChangedByTransaction", () => {
         },
         source: { type: "local" },
         type: "update",
+      },
+    ]);
+  });
+
+  it("should only return a single block, if multiple updates change a single block in a transaction", () => {
+    editor.updateBlock("paragraph-0", {
+      props: {
+        backgroundColor: "red",
+      },
+    });
+    editor.updateBlock("paragraph-0", {
+      props: {
+        backgroundColor: "blue",
+      },
+    });
+
+    const blocksChanged = getBlocksChangedByTransaction(transaction!, editor);
+
+    expect(blocksChanged).toEqual([
+      {
+        block: {
+          children: [],
+          id: "paragraph-0",
+          props: {
+            backgroundColor: "blue",
+            textAlignment: "left",
+            textColor: "default",
+          },
+          type: "paragraph",
+          content: [
+            {
+              styles: {},
+              text: "Paragraph 0",
+              type: "text",
+            },
+          ],
+        },
+        prevBlock: {
+          children: [],
+          id: "paragraph-0",
+          props: {
+            backgroundColor: "default",
+            textAlignment: "left",
+            textColor: "default",
+          },
+          type: "paragraph",
+          content: [
+            {
+              styles: {},
+              text: "Paragraph 0",
+              type: "text",
+            },
+          ],
+        },
+        source: { type: "local" },
+        type: "update",
+      },
+    ]);
+  });
+
+  it("should return multiple blocks, if multiple updates change multiple blocks in a transaction", () => {
+    editor.updateBlock("paragraph-0", {
+      props: {
+        backgroundColor: "red",
+      },
+    });
+    editor.updateBlock("paragraph-1", {
+      props: {
+        backgroundColor: "blue",
+      },
+    });
+
+    const blocksChanged = getBlocksChangedByTransaction(transaction!, editor);
+
+    expect(blocksChanged).toEqual([
+      {
+        block: {
+          children: [],
+          id: "paragraph-0",
+          props: {
+            backgroundColor: "red",
+            textAlignment: "left",
+            textColor: "default",
+          },
+          type: "paragraph",
+          content: [
+            {
+              styles: {},
+              text: "Paragraph 0",
+              type: "text",
+            },
+          ],
+        },
+        prevBlock: {
+          children: [],
+          id: "paragraph-0",
+          props: {
+            backgroundColor: "default",
+            textAlignment: "left",
+            textColor: "default",
+          },
+          type: "paragraph",
+          content: [
+            {
+              styles: {},
+              text: "Paragraph 0",
+              type: "text",
+            },
+          ],
+        },
+        source: { type: "local" },
+        type: "update",
+      },
+      {
+        block: {
+          children: [],
+          id: "paragraph-1",
+          props: {
+            backgroundColor: "blue",
+            textAlignment: "left",
+            textColor: "default",
+          },
+          type: "paragraph",
+          content: [
+            {
+              styles: {},
+              text: "Paragraph 1",
+              type: "text",
+            },
+          ],
+        },
+        prevBlock: {
+          children: [],
+          id: "paragraph-1",
+          props: {
+            backgroundColor: "default",
+            textAlignment: "left",
+            textColor: "default",
+          },
+          type: "paragraph",
+          content: [
+            {
+              styles: {},
+              text: "Paragraph 1",
+              type: "text",
+            },
+          ],
+        },
+        source: { type: "local" },
+        type: "update",
+      },
+    ]);
+  });
+
+  it("should return multiple blocks, if multiple inserts add new blocks in a transaction", () => {
+    editor.insertBlocks(
+      [{ type: "paragraph", content: "ABC" }],
+      "paragraph-0",
+      "after"
+    );
+    editor.insertBlocks(
+      [{ type: "paragraph", content: "DEF" }],
+      "paragraph-1",
+      "after"
+    );
+
+    const blocksChanged = getBlocksChangedByTransaction(transaction!, editor);
+
+    expect(blocksChanged).toEqual([
+      {
+        block: {
+          children: [],
+          content: [
+            {
+              styles: {},
+              text: "ABC",
+              type: "text",
+            },
+          ],
+          id: "0",
+          props: {
+            backgroundColor: "default",
+            textAlignment: "left",
+            textColor: "default",
+          },
+          type: "paragraph",
+        },
+        prevBlock: undefined,
+        source: { type: "local" },
+        type: "insert",
+      },
+      {
+        block: {
+          children: [],
+          content: [
+            {
+              styles: {},
+              text: "DEF",
+              type: "text",
+            },
+          ],
+          id: "1",
+          props: {
+            backgroundColor: "default",
+            textAlignment: "left",
+            textColor: "default",
+          },
+          type: "paragraph",
+        },
+        prevBlock: undefined,
+        source: { type: "local" },
+        type: "insert",
       },
     ]);
   });
