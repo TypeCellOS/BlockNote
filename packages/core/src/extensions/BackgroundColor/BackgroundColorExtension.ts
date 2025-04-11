175ad1386ab6388b4d14544c7778d3e531c85a90
Matthew Lipski <matthewlipski@gmail.com>
2025-04-12 01:12:43 +0200

    Fixed parsing of numbered list start index, background color mark, and text color mark

--- packages/core/src/extensions/BackgroundColor/BackgroundColorExtension.ts
+++ packages/core/src/extensions/BackgroundColor/BackgroundColorExtension.ts
@@ -1,5 +1,5 @@
 import { Extension } from "@tiptap/core";
-import { defaultProps } from "../../blocks/defaultProps.js";
+import { getBackgroundColorAttribute } from "../../blocks/defaultProps.js";
 
 export const BackgroundColorExtension = Extension.create({
   name: "blockBackgroundColor",
@@ -9,24 +9,7 @@ export const BackgroundColorExtension = Extension.create({
       {
         types: ["blockContainer", "tableCell", "tableHeader"],
         attributes: {
-          backgroundColor: {
-            default: defaultProps.backgroundColor.default,
-            parseHTML: (element) =>
-              element.hasAttribute("data-background-color")
-                ? element.getAttribute("data-background-color")
-                : defaultProps.backgroundColor.default,
-            renderHTML: (attributes) => {
-              if (
-                attributes.backgroundColor ===
-                defaultProps.backgroundColor.default
-              ) {
-                return {};
-              }
-              return {
-                "data-background-color": attributes.backgroundColor,
-              };
-            },
-          },
+          backgroundColor: getBackgroundColorAttribute(),
         },
       },
     ];
