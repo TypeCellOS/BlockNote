0cf25c0fb11fd58182a75edf8f0776cf7ab57844
Drew Johnson <drewjohnsongt@gmail.com>
2025-04-23 13:47:46 -0400

    Test H6 notion snapshot

--- tests/src/unit/core/formatConversion/parse/parseTestInstances.ts
+++ tests/src/unit/core/formatConversion/parse/parseTestInstances.ts
@@ -360,6 +360,7 @@ export const parseTestInstancesHTML: TestInstance<
 <h3>Heading 3</h3>
 <h4>Heading 4</h4>
 <h5>Heading 5</h5>
+<h6>Heading 6</h6>
 <p>Paragraph 1</p>
 <p>Nested Paragraph 1</p>
 <p>Nested Paragraph 2</p>
