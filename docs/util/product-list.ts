e7b465c0346006aa7d5a12230a94c50470b4fd06
Nick the Sick <nick@blocknotejs.org>
2025-05-01 18:11:42 +0200

    fix: update the price-id

--- docs/util/product-list.ts
+++ docs/util/product-list.ts
@@ -1,11 +1,17 @@
 export const PRODUCTS = {
   business: {
-    id: "8049f66f-fd0a-4690-a0aa-442ac5b03040",
+    id:
+      process.env.NODE_ENV === "production"
+        ? "c7faaa4c-7805-4722-88d2-5a68f068d546"
+        : "8049f66f-fd0a-4690-a0aa-442ac5b03040",
     name: "Business",
     slug: "business",
   } as const,
   starter: {
-    id: "ab70fea5-172c-4aac-b3fc-0824f2a5b670",
+    id:
+      process.env.NODE_ENV === "production"
+        ? "ef89c65b-9b18-4091-8de6-264554aa027e"
+        : "8049f66f-fd0a-4690-a0aa-442ac5b03040",
     name: "Starter",
     slug: "starter",
   } as const,
