b20ac11e5241010c85b07695759fa2d4305a9abf
Nick the Sick <nick@blocknotejs.org>
2025-05-02 13:01:49 +0200

    feat: implementation of something which rejects by transaction

--- examples/01-basic/01-minimal/App.tsx
+++ examples/01-basic/01-minimal/App.tsx
@@ -2,10 +2,30 @@ import "@blocknote/core/fonts/inter.css";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 import { useCreateBlockNote } from "@blocknote/react";
+import { getBlocksChangedByTransaction } from "@blocknote/core";
+import { Plugin, PluginKey } from "prosemirror-state";
+
+const MyFilterExtension = {
+  plugin: new Plugin({
+    filterTransaction: (transaction) => {
+      const blocksChanged = getBlocksChangedByTransaction(transaction);
+      console.log(blocksChanged);
+      if (JSON.stringify(blocksChanged).includes("NOPE")) {
+        return false;
+      }
+      return true;
+    },
+    key: new PluginKey("my-extension"),
+  }),
+};
 
 export default function App() {
   // Creates a new editor instance.
-  const editor = useCreateBlockNote();
+  const editor = useCreateBlockNote({
+    _extensions: {
+      filterExtension: MyFilterExtension,
+    },
+  });
 
   // Renders the editor instance using a React component.
   return <BlockNoteView editor={editor} />;
