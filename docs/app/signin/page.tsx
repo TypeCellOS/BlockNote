2bc646c5b0f65efb8c8d6c9f619cff7489026f8f
Nick the Sick <nick@blocknotejs.org>
2025-05-06 17:41:31 +0200

    build: put back dynamic

--- docs/app/signin/page.tsx
+++ docs/app/signin/page.tsx
@@ -1,10 +1,20 @@
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
-import AuthenticationPage from "../../components/AuthenticationPage";
 export const metadata: Metadata = {
   title: "Login",
 };
 
+// dynamic import because we use search params in the client component
+const AuthenticationPage = dynamic(
+  () => import("../../components/AuthenticationPage"),
+);
+
 export default function Register() {
-  return <AuthenticationPage variant="email" />;
+  return (
+    <Suspense>
+      <AuthenticationPage variant="email" />
+    </Suspense>
+  );
 }
--- docs/app/signup/page.tsx
+++ docs/app/signup/page.tsx
@@ -1,11 +1,20 @@
 import { Metadata } from "next";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
 
-import AuthenticationPage from "../../components/AuthenticationPage";
+// dynamic import because we use search params in the client component
+const AuthenticationPage = dynamic(
+  () => import("../../components/AuthenticationPage"),
+);
 
 export const metadata: Metadata = {
   title: "Sign-up",
 };
 
 export default function Register() {
-  return <AuthenticationPage variant="register" />;
+  return (
+    <Suspense>
+      <AuthenticationPage variant="register" />
+    </Suspense>
+  );
 }
