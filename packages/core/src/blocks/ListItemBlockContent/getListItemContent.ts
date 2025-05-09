0e3f503152da0cf2155c4cc7f8db2ab4c7221cd9
Nick the Sick <nick@blocknotejs.org>
2025-05-09 09:11:51 +0200

    docs: add some more context around the problem

--- packages/core/src/blocks/ListItemBlockContent/getListItemContent.ts
+++ packages/core/src/blocks/ListItemBlockContent/getListItemContent.ts
@@ -1,16 +1,38 @@
 import { DOMParser, Fragment, Schema } from "prosemirror-model";
 
+/**
+ * This function is used to parse the content of a list item external HTML node.
+ *
+ * Due to a change in how prosemirror-model handles parsing elements, we have additional flexibility in how we can "fit" content into a list item.
+ *
+ * We've decided to take an approach that is similar to Notion. The core rules of the algorithm are:
+ *
+ *  - If the first child of an `li` has ONLY text content, take the text content, and flatten it into the list item. Subsequent siblings are carried over as is, as children of the list item.
+ *    - e.g. `<li><h1>Hello</h1><p>World</p></li> -> <li>Hello<blockGroup><blockContainer><p>World</p></blockContainer></blockGroup></li>`
+ *  - Else, take the content and insert it as children instead.
+ *    - e.g. `<li><img src="url" /></li> -> <li><p></p><blockGroup><blockContainer><img src="url" /></blockContainer></blockGroup></li>`
+ *
+ * This ensures that a list item's content is always valid ProseMirror content. Smoothing over differences between how external HTML may be rendered, and how ProseMirror expects content to be structured.
+ */
 export function getListItemContent(
+  /**
+   * The `li` element to parse.
+   */
   node: Node,
+  /**
+   * The schema to use for parsing.
+   */
   schema: Schema,
+  /**
+   * The name of the list item node.
+   */
   name: string
 ): Fragment {
   const parser = DOMParser.fromSchema(schema);
 
   if (!(node instanceof HTMLElement)) {
-    return parser.parse(node, {
-      topNode: schema.nodes[name].create(),
-    }).content;
+    // TODO: This will be unnecessary in the future: https://github.com/ProseMirror/prosemirror-model/commit/166188d4f9db96eb86fb7de62e72049c86c9dd79
+    throw new Error("Node is not an HTMLElement");
   }
 
   // TODO: Super hacky workaround so check list items are parsed correctly.
