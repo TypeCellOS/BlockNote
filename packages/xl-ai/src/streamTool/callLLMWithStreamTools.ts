1a8fd2d8f1eabe538461864a45aead7d7389abe3
yousefed <yousefdardiry@gmail.com>
2025-05-20 12:47:56 +0200

    add comment

--- packages/xl-ai/src/streamTool/callLLMWithStreamTools.ts
+++ packages/xl-ai/src/streamTool/callLLMWithStreamTools.ts
@@ -47,8 +47,21 @@ export type OperationsResult<T extends StreamTool<any>[]> = {
    * Calling this consumes the underlying streams
    */
   operationsSource: AsyncIterableStream<{
+    /**
+     * The operation the LLM wants to execute
+     */
     operation: StreamToolCall<T>;
+    /**
+     * Whether {@link operation} is an update to the previous operation in the stream.
+     *
+     * For non-streaming mode, this will always be `false`
+     */
     isUpdateToPreviousOperation: boolean;
+    /**
+     * Whether the {@link operation} is possibly partial (i.e. the LLM is still streaming data about this operation)
+     *
+     * For non-streaming mode, this will always be `false`
+     */
     isPossiblyPartial: boolean;
   }>;
   /**
