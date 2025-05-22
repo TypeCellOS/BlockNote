245298edf60dc4fff7f0d7e7fa9d905b8dca7a74
Yousef <yousefdardiry@gmail.com>
2025-05-22 14:32:39 +0200

    docs: add attention to new AI features (#1709)

--- docs/components/Navigation.tsx
+++ docs/components/Navigation.tsx
@@ -3,9 +3,18 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment -- Lots of Nextra magic. */
 
 import { Navbar } from "nextra-theme-docs";
+import "./navigation.css";
 
 export function Navigation(props: any) {
   // items last to override the default
   // return <div>hello</div>;
-  return <Navbar {...props} />;
+  return (
+    <>
+      <div className="top-banner">
+        🚀 BlockNote AI is here!{" "}
+        <a href="/docs/ai">Access the early preview.</a>
+      </div>
+      <Navbar {...props} />
+    </>
+  );
 }
