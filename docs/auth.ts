accb2a607090e8d636acd9c0134ce1712c029d4c
Nick Perez <nick@blocknotejs.org>
2025-06-09 15:45:00 +0200

    fix(website): log in bug fixes (#1742)

--- docs/auth.ts
+++ docs/auth.ts
@@ -223,25 +223,34 @@ export const auth = betterAuth({
         // webhooks have to be publicly accessible
         // ngrok http http://localhost:3000
         secret: process.env.POLAR_WEBHOOK_SECRET as string,
-        onSubscriptionUpdated: async (payload) => {
-          const authContext = await auth.$context;
-          const userId = payload.data.customer.externalId;
-          if (!userId) {
-            return;
-          }
-          if (payload.data.status === "active") {
-            const productId = payload.data.product.id;
-            const planType = Object.values(PRODUCTS).find(
-              (p) => p.id === productId,
-            )?.slug;
-            await authContext.internalAdapter.updateUser(userId, {
-              planType,
-            });
-          } else {
-            // No active subscription, so we need to remove the plan type
-            await authContext.internalAdapter.updateUser(userId, {
-              planType: null,
-            });
+        async onPayload(payload) {
+          switch (payload.type) {
+            case "subscription.active":
+            case "subscription.canceled":
+            case "subscription.updated":
+            case "subscription.revoked":
+            case "subscription.created":
+            case "subscription.uncanceled": {
+              const authContext = await auth.$context;
+              const userId = payload.data.customer.externalId;
+              if (!userId) {
+                return;
+              }
+              if (payload.data.status === "active") {
+                const productId = payload.data.product.id;
+                const planType = Object.values(PRODUCTS).find(
+                  (p) => p.id === productId,
+                )?.slug;
+                await authContext.internalAdapter.updateUser(userId, {
+                  planType,
+                });
+              } else {
+                // No active subscription, so we need to remove the plan type
+                await authContext.internalAdapter.updateUser(userId, {
+                  planType: null,
+                });
+              }
+            }
           }
         },
       },
@@ -254,11 +263,7 @@ export const auth = betterAuth({
   },
   hooks: {
     after: createAuthMiddleware(async (ctx) => {
-      if (
-        ctx.path === "/magic-link/verify" ||
-        ctx.path === "/verify-email" ||
-        ctx.path === "/sign-in/social"
-      ) {
+      if (ctx.path === "/verify-email" || ctx.path === "/sign-in/social") {
         // After verifying email, send them a welcome email
         const newSession = ctx.context.newSession;
         if (newSession) {
