9378ef072354a366f00fc07b704e62e7f64b5263
Matthew Lipski <matthewlipski@gmail.com>
2025-06-11 12:54:55 +0200

    Changed heading block to again use `createStronglyTypedTiptapNode` instead of `createBlockSpec`

--- packages/core/src/blocks/HeadingBlockContent/HeadingBlockContent.ts
+++ packages/core/src/blocks/HeadingBlockContent/HeadingBlockContent.ts
@@ -1,9 +1,15 @@
+import { InputRule } from "@tiptap/core";
+
+import { updateBlockCommand } from "../../api/blockManipulation/commands/updateBlock/updateBlock.js";
+import { getBlockInfoFromSelection } from "../../api/getBlockInfoFromPos.js";
 import {
-  CustomBlockConfig,
-  CustomBlockImplementation,
   PropSchema,
-  createBlockSpec,
+  createBlockSpecFromStronglyTypedTiptapNode,
+  createStronglyTypedTiptapNode,
+  getBlockFromPos,
+  propsToAttributes,
 } from "../../schema/index.js";
+import { createDefaultBlockDOMOutputSpec } from "../defaultBlockHelpers.js";
 import { defaultProps } from "../defaultProps.js";
 import { createToggleWrapper } from "../ToggleWrapper/createToggleWrapper.js";
 
@@ -13,189 +19,180 @@ export const headingPropSchema = {
   isTogglable: { default: false },
 } as const satisfies PropSchema;
 
-export const headingConfig = {
-  type: "heading",
-  propSchema: headingPropSchema,
-  content: "inline",
-} as const satisfies CustomBlockConfig;
+const HeadingBlockContent = createStronglyTypedTiptapNode({
+  name: "heading",
+  content: "inline*",
+  group: "blockContent",
 
-export const headingRender: CustomBlockImplementation<
-  typeof headingConfig,
-  any,
-  any
->["render"] = (block, editor) => {
-  const dom = document.createElement(`h${block.props.level}`);
+  addAttributes() {
+    return propsToAttributes(headingPropSchema);
+  },
 
-  return createToggleWrapper(block, editor, {
-    dom,
-    contentDOM: dom,
-  });
-};
+  addInputRules() {
+    return [
+      ...[1, 2, 3].map((level) => {
+        // Creates a heading of appropriate level when starting with "#", "##", or "###".
+        return new InputRule({
+          find: new RegExp(`^(#{${level}})\\s$`),
+          handler: ({ state, chain, range }) => {
+            const blockInfo = getBlockInfoFromSelection(state);
+            if (
+              !blockInfo.isBlockContainer ||
+              blockInfo.blockContent.node.type.spec.content !== "inline*"
+            ) {
+              return;
+            }
 
-export const headingParse: CustomBlockImplementation<
-  typeof headingConfig,
-  any,
-  any
->["parse"] = (element) => {
-  if (element.tagName === "H1") {
-    return { level: 1 };
-  } else if (element.tagName === "H2") {
-    return { level: 2 };
-  } else if (element.tagName === "H3") {
-    return { level: 3 };
-  }
+            chain()
+              .command(
+                updateBlockCommand(blockInfo.bnBlock.beforePos, {
+                  type: "heading",
+                  props: {
+                    level: level as any,
+                  },
+                }),
+              )
+              // Removes the "#" character(s) used to set the heading.
+              .deleteRange({ from: range.from, to: range.to })
+              .run();
+          },
+        });
+      }),
+    ];
+  },
 
-  return undefined;
-};
+  addKeyboardShortcuts() {
+    return {
+      "Mod-Alt-1": () => {
+        const blockInfo = getBlockInfoFromSelection(this.editor.state);
+        if (
+          !blockInfo.isBlockContainer ||
+          blockInfo.blockContent.node.type.spec.content !== "inline*"
+        ) {
+          return true;
+        }
 
-export const headingImplementation = {
-  render: headingRender,
-  parse: headingParse,
-} satisfies CustomBlockImplementation<typeof headingConfig, any, any>;
+        // call updateBlockCommand
+        return this.editor.commands.command(
+          updateBlockCommand(blockInfo.bnBlock.beforePos, {
+            type: "heading",
+            props: {
+              level: 1 as any,
+            },
+          }),
+        );
+      },
+      "Mod-Alt-2": () => {
+        const blockInfo = getBlockInfoFromSelection(this.editor.state);
+        if (
+          !blockInfo.isBlockContainer ||
+          blockInfo.blockContent.node.type.spec.content !== "inline*"
+        ) {
+          return true;
+        }
 
-export const Heading = createBlockSpec(headingConfig, headingImplementation);
+        return this.editor.commands.command(
+          updateBlockCommand(blockInfo.bnBlock.beforePos, {
+            type: "heading",
+            props: {
+              level: 2 as any,
+            },
+          }),
+        );
+      },
+      "Mod-Alt-3": () => {
+        const blockInfo = getBlockInfoFromSelection(this.editor.state);
+        if (
+          !blockInfo.isBlockContainer ||
+          blockInfo.blockContent.node.type.spec.content !== "inline*"
+        ) {
+          return true;
+        }
 
-// const HeadingBlockContent = createStronglyTypedTiptapNode({
-//   name: "heading",
-//   content: "inline*",
-//   group: "blockContent",
+        return this.editor.commands.command(
+          updateBlockCommand(blockInfo.bnBlock.beforePos, {
+            type: "heading",
+            props: {
+              level: 3 as any,
+            },
+          }),
+        );
+      },
+    };
+  },
+  parseHTML() {
+    return [
+      // Parse from internal HTML.
+      {
+        tag: "div[data-content-type=" + this.name + "]",
+        contentElement: ".bn-inline-content",
+      },
+      // Parse from external HTML.
+      {
+        tag: "h1",
+        attrs: { level: 1 },
+        node: "heading",
+      },
+      {
+        tag: "h2",
+        attrs: { level: 2 },
+        node: "heading",
+      },
+      {
+        tag: "h3",
+        attrs: { level: 3 },
+        node: "heading",
+      },
+    ];
+  },
 
-//   addAttributes() {
-//     return propsToAttributes(headingPropSchema);
-//   },
+  renderHTML({ node, HTMLAttributes }) {
+    return createDefaultBlockDOMOutputSpec(
+      this.name,
+      `h${node.attrs.level}`,
+      {
+        ...(this.options.domAttributes?.blockContent || {}),
+        ...HTMLAttributes,
+      },
+      this.options.domAttributes?.inlineContent || {},
+    );
+  },
 
-//   addInputRules() {
-//     return [
-//       ...[1, 2, 3].map((level) => {
-//         // Creates a heading of appropriate level when starting with "#", "##", or "###".
-//         return new InputRule({
-//           find: new RegExp(`^(#{${level}})\\s$`),
-//           handler: ({ state, chain, range }) => {
-//             const blockInfo = getBlockInfoFromSelection(state);
-//             if (
-//               !blockInfo.isBlockContainer ||
-//               blockInfo.blockContent.node.type.spec.content !== "inline*"
-//             ) {
-//               return;
-//             }
+  addNodeView() {
+    return ({ node, HTMLAttributes, getPos }) => {
+      const renderedElement = createDefaultBlockDOMOutputSpec(
+        this.name,
+        `h${node.attrs.level}`,
+        {
+          ...(this.options.domAttributes?.blockContent || {}),
+          ...HTMLAttributes,
+        },
+        this.options.domAttributes?.inlineContent || {},
+      );
 
-//             chain()
-//               .command(
-//                 updateBlockCommand(blockInfo.bnBlock.beforePos, {
-//                   type: "heading",
-//                   props: {
-//                     level: level as any,
-//                   },
-//                 }),
-//               )
-//               // Removes the "#" character(s) used to set the heading.
-//               .deleteRange({ from: range.from, to: range.to })
-//               .run();
-//           },
-//         });
-//       }),
-//     ];
-//   },
+      const contentDOM = renderedElement.contentDOM;
+      const contentDOMParentElement = contentDOM.parentElement!;
 
-//   addKeyboardShortcuts() {
-//     return {
-//       "Mod-Alt-1": () => {
-//         const blockInfo = getBlockInfoFromSelection(this.editor.state);
-//         if (
-//           !blockInfo.isBlockContainer ||
-//           blockInfo.blockContent.node.type.spec.content !== "inline*"
-//         ) {
-//           return true;
-//         }
+      const editor = this.options.editor;
+      const block = getBlockFromPos(getPos, editor, this.editor, this.name);
 
-//         // call updateBlockCommand
-//         return this.editor.commands.command(
-//           updateBlockCommand(blockInfo.bnBlock.beforePos, {
-//             type: "heading",
-//             props: {
-//               level: 1 as any,
-//             },
-//           }),
-//         );
-//       },
-//       "Mod-Alt-2": () => {
-//         const blockInfo = getBlockInfoFromSelection(this.editor.state);
-//         if (
-//           !blockInfo.isBlockContainer ||
-//           blockInfo.blockContent.node.type.spec.content !== "inline*"
-//         ) {
-//           return true;
-//         }
+      const toggleWrapper = createToggleWrapper(block as any, editor, {
+        dom: contentDOM,
+        contentDOM,
+      });
+      contentDOMParentElement.appendChild(toggleWrapper.dom);
 
-//         return this.editor.commands.command(
-//           updateBlockCommand(blockInfo.bnBlock.beforePos, {
-//             type: "heading",
-//             props: {
-//               level: 2 as any,
-//             },
-//           }),
-//         );
-//       },
-//       "Mod-Alt-3": () => {
-//         const blockInfo = getBlockInfoFromSelection(this.editor.state);
-//         if (
-//           !blockInfo.isBlockContainer ||
-//           blockInfo.blockContent.node.type.spec.content !== "inline*"
-//         ) {
-//           return true;
-//         }
+      return {
+        dom: renderedElement.dom,
+        contentDOM: toggleWrapper.contentDOM,
+        ignoreMutation: toggleWrapper.ignoreMutation,
+        destroy: toggleWrapper.destroy,
+      };
+    };
+  },
+});
 
-//         return this.editor.commands.command(
-//           updateBlockCommand(blockInfo.bnBlock.beforePos, {
-//             type: "heading",
-//             props: {
-//               level: 3 as any,
-//             },
-//           }),
-//         );
-//       },
-//     };
-//   },
-//   parseHTML() {
-//     return [
-//       // Parse from internal HTML.
-//       {
-//         tag: "div[data-content-type=" + this.name + "]",
-//         contentElement: ".bn-inline-content",
-//       },
-//       // Parse from external HTML.
-//       {
-//         tag: "h1",
-//         attrs: { level: 1 },
-//         node: "heading",
-//       },
-//       {
-//         tag: "h2",
-//         attrs: { level: 2 },
-//         node: "heading",
-//       },
-//       {
-//         tag: "h3",
-//         attrs: { level: 3 },
-//         node: "heading",
-//       },
-//     ];
-//   },
-
-//   renderHTML({ node, HTMLAttributes }) {
-//     return createDefaultBlockDOMOutputSpec(
-//       this.name,
-//       `h${node.attrs.level}`,
-//       {
-//         ...(this.options.domAttributes?.blockContent || {}),
-//         ...HTMLAttributes,
-//       },
-//       this.options.domAttributes?.inlineContent || {},
-//     );
-//   },
-// });
-
-// export const Heading = createBlockSpecFromStronglyTypedTiptapNode(
-//   HeadingBlockContent,
-//   headingPropSchema,
-// );
+export const Heading = createBlockSpecFromStronglyTypedTiptapNode(
+  HeadingBlockContent,
+  headingPropSchema,
+);
