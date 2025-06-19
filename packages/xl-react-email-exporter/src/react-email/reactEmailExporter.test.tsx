5d4e12627e1888af74605e0a7d6a2ae782af3524
Jonathan Marbutt <jonathan@marbutt.com>
2025-06-19 10:43:45 -0500

    Trying to get the test for the render

--- packages/xl-react-email-exporter/src/react-email/reactEmailExporter.test.tsx
+++ packages/xl-react-email-exporter/src/react-email/reactEmailExporter.test.tsx
@@ -1,5 +1,5 @@
-import { describe, it } from "vitest";
-
+import { describe, it, expect } from "vitest";
+import { render } from "@react-email/render";
 import { ReactEmailExporter } from "./reactEmailExporter";
 import { reactEmailDefaultSchemaMappings } from "./defaultSchema";
 import { BlockNoteSchema } from "@blocknote/core";
@@ -7,17 +7,14 @@ import { testDocument } from "@shared/testDocument.js";
 
 
 describe("react email exporter", () => {
-  it("should export a document", async () => {
+  it("should export a document (HTML snapshot)", async () => {
     const exporter = new ReactEmailExporter(
       BlockNoteSchema.create(),
       reactEmailDefaultSchemaMappings
     );
 
-    const html = await exporter.toReactEmailDocument(testDocument as any);
-
-    // eslint-disable-next-line no-console
-    console.log(html);
-    // const buffer = await Packer.toBuffer(doc);
-    // fs.writeFileSync(__dirname + "/My Document.docx", buffer);
+    const reactElement = await exporter.toReactEmailDocument(testDocument as any);
+    const html = render(reactElement);
+    expect(html).toMatchSnapshot();
   });
 });
