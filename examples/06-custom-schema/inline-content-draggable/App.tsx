1ae2d78109c97c8fcb662cad97861e84310ed037
Héctor Chong <i@hector.im>
2025-07-04 18:39:48 -0400

    feat: add `draggable` prop to enable inline content dragging

---
+++ examples/06-custom-schema/inline-content-draggable/App.tsx
@@ -0,0 +1,80 @@
+import { BlockNoteSchema, defaultInlineContentSpecs } from "@blocknote/core";
+import "@blocknote/core/fonts/inter.css";
+import {
+  createReactInlineContentSpec,
+  useCreateBlockNote,
+} from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+
+const draggableButton = createReactInlineContentSpec(
+  {
+    type: "draggableButton",
+    propSchema: {
+      title: {
+        default: "",
+      },
+    },
+    draggable: true,
+    content: "none"
+  },
+  {
+    render: (props) => {
+      return (
+        <span
+          style={{
+            border: "none",
+            background: "blue",
+            color: "white",
+            padding: "5px 10px",
+            borderRadius: "4px",
+            cursor: "move",
+          }}
+          data-drag-handle>
+          <span>{props.inlineContent.props.title}</span>
+        </span>
+      );
+    },
+  }
+);
+
+const schema = BlockNoteSchema.create({
+  inlineContentSpecs: {
+    draggableButton,
+    ...defaultInlineContentSpecs,
+  },
+});
+
+export default function ReactInlineContent() {
+  const editor = useCreateBlockNote({
+    schema,
+    initialContent: [
+      {
+        type: "paragraph",
+        content: [
+          "I enjoy working with ",
+          {
+            type: "draggableButton",
+            props: {
+              title: "Hector",
+            },
+          },
+        ],
+      },
+      {
+        type: "paragraph",
+        content: [
+          "I love ",
+          {
+            type: "draggableButton",
+            props: {
+              title: "BlockNote",
+            },
+          },
+        ],
+      },
+    ],
+  });
+
+  return <BlockNoteView editor={editor} />;
+}
---
+++ examples/06-custom-schema/inline-content-draggable/main.tsx
@@ -0,0 +1,11 @@
+// AUTO-GENERATED FILE, DO NOT EDIT DIRECTLY
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
