0c506fd3feed448087cc1d7e91cf427eaaeeff1f
Brad Greenlee <bgreenlee@medium.com>
2025-07-25 17:45:10 -0700

    Use image alt text for image block name This fixes the issue (#1882) where Markdown images that are converted to blocks and back again lose their alt text, having it replaced with the default alt text ("BlockNote image").
    An alternative fix would be to use the alt text as the caption, but given
    that the caption is rendered as a separate element, that seemed less
    appropriate.

--- packages/core/src/blocks/ImageBlockContent/parseImageElement.ts
+++ packages/core/src/blocks/ImageBlockContent/parseImageElement.ts
@@ -1,6 +1,7 @@
 export const parseImageElement = (imageElement: HTMLImageElement) => {
   const url = imageElement.src || undefined;
   const previewWidth = imageElement.width || undefined;
+  const name = imageElement.alt || undefined;
 
-  return { url, previewWidth };
+  return { url, previewWidth, name };
 };
