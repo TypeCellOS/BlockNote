f6cb1f9e018969c45eb0878e0d8548002b8b925f
Matthew Lipski <matthewlipski@gmail.com>
2025-09-01 10:29:12 +0200

    Reverted example changes

--- examples/01-basic/01-minimal/src/App.tsx
+++ examples/01-basic/01-minimal/src/App.tsx
@@ -5,17 +5,7 @@ import { useCreateBlockNote } from "@blocknote/react";
 
 export default function App() {
   // Creates a new editor instance.
-  const editor = useCreateBlockNote({
-    initialContent: [
-      {
-        type: "paragraph",
-        content: "Paragraph 1",
-      },
-      {
-        type: "paragraph",
-      },
-    ],
-  });
+  const editor = useCreateBlockNote();
 
   // Renders the editor instance using a React component.
   return <BlockNoteView editor={editor} />;
