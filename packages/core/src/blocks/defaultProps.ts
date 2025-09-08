f4777cfa14921eba19f77039c068f5fd76b4ac4d
Matthew Lipski <matthewlipski@gmail.com>
2025-09-08 17:58:22 +0200

    Fixed color style parsing

--- packages/core/src/blocks/defaultProps.ts
+++ packages/core/src/blocks/defaultProps.ts
@@ -1,3 +1,4 @@
+import { Attribute } from "@tiptap/core";
 import type { Props, PropSchema } from "../schema/index.js";
 
 // TODO: this system should probably be moved / refactored.
@@ -23,6 +24,8 @@ export type DefaultProps = Props<typeof defaultProps>;
 // a custom block's TipTap node attributes.
 export const inheritedProps = ["backgroundColor", "textColor"];
 
+// TODO: Move text/background color props from `blockContainer` node to
+// `blockContent`.
 export const parseDefaultProps = (element: HTMLElement) => {
   const props: Partial<DefaultProps> = {};
 
@@ -34,3 +37,97 @@ export const parseDefaultProps = (element: HTMLElement) => {
 
   return props;
 };
+
+export const getBackgroundColorAttribute = (
+  attributeName = "backgroundColor",
+): Attribute => ({
+  default: defaultProps.backgroundColor.default,
+  parseHTML: (element) => {
+    if (element.hasAttribute("data-background-color")) {
+      return element.getAttribute("data-background-color");
+    }
+
+    if (element.style.backgroundColor) {
+      // Check if `element.style.backgroundColor` matches the string:
+      // `var(--blocknote-background-<color>)`. If it does, return the color
+      // name only. Otherwise, return `element.style.backgroundColor`.
+      const match = element.style.backgroundColor.match(
+        /var\(--blocknote-background-(.+)\)/,
+      );
+      if (match) {
+        return match[1];
+      }
+
+      return element.style.backgroundColor;
+    }
+
+    return defaultProps.backgroundColor.default;
+  },
+  renderHTML: (attributes) => {
+    if (attributes[attributeName] === defaultProps.backgroundColor.default) {
+      return {};
+    }
+
+    return {
+      "data-background-color": attributes[attributeName],
+    };
+  },
+});
+
+export const getTextColorAttribute = (
+  attributeName = "textColor",
+): Attribute => ({
+  default: defaultProps.textColor.default,
+  parseHTML: (element) => {
+    if (element.hasAttribute("data-text-color")) {
+      return element.getAttribute("data-text-color");
+    }
+
+    if (element.style.color) {
+      // Check if `element.style.color` matches the string:
+      // `var(--blocknote-text-<color>)`. If it does, return the color name
+      // only. Otherwise, return `element.style.color`.
+      const match = element.style.color.match(/var\(--blocknote-text-(.+)\)/);
+      if (match) {
+        return match[1];
+      }
+
+      return element.style.color;
+    }
+
+    return defaultProps.textColor.default;
+  },
+  renderHTML: (attributes) => {
+    if (attributes[attributeName] === defaultProps.textColor.default) {
+      return {};
+    }
+    return {
+      "data-text-color": attributes[attributeName],
+    };
+  },
+});
+
+export const getTextAlignmentAttribute = (
+  attributeName = "textAlignment",
+): Attribute => ({
+  default: defaultProps.textAlignment.default,
+  parseHTML: (element) => {
+    if (element.hasAttribute("data-text-alignment")) {
+      return element.getAttribute("data-text-alignment");
+    }
+
+    if (element.style.textAlign) {
+      return element.style.textAlign;
+    }
+
+    return defaultProps.textAlignment.default;
+  },
+  renderHTML: (attributes) => {
+    if (attributes[attributeName] === defaultProps.textAlignment.default) {
+      return {};
+    }
+    return {
+      "data-text-alignment": attributes[attributeName],
+    };
+  },
+});
