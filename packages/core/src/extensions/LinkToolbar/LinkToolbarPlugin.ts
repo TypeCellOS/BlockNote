6603eea146307065f75aa9c5eee93a5aabc33907
Nick the Sick <nick@blocknotejs.org>
2025-09-15 14:38:07 +0200

    fix: pull from the correct wrapper element

--- packages/core/src/extensions/LinkToolbar/LinkToolbarPlugin.ts
+++ packages/core/src/extensions/LinkToolbar/LinkToolbarPlugin.ts
@@ -119,7 +119,7 @@ class LinkToolbarView implements PluginView {
   };
 
   clickHandler = (event: MouseEvent) => {
-    const editorWrapper = this.pmView.dom.parentElement!;
+    const editorWrapper = this.pmView.dom.parentElement!.parentElement!;
 
     if (
       // Toolbar is open.
