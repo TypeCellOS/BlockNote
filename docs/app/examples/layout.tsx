8de181dda244a07ade7965e057925720d0fba8ce
Nick Perez <nick@blocknotejs.org>
2025-09-22 16:24:43 +0200

    docs: update analytics event tracking & pricing page (#2022)
    Co-authored-by: Nick Perez <nick@blocknotejs.org>
    Co-authored-by: Yousef <yousefdardiry@gmail.com>
    Co-authored-by: Matthew Lipski <50169049+matthewlipski@users.noreply.github.com>

--- docs/app/examples/layout.tsx
+++ docs/app/examples/layout.tsx
@@ -1,11 +1,11 @@
-import type { ReactNode } from "react";
-import { Analytics } from "@vercel/analytics/react";
 import { baseOptions } from "@/app/layout.config";
 import { CustomDocsLayout } from "@/components/CustomDocsLayout";
 import { Footer } from "@/components/Footer";
 import { ProBadge } from "@/components/ProBadge";
 import { source } from "@/lib/source/examples";
 import { getExampleData } from "@/util/getExampleData";
+import type { ReactNode } from "react";
+import * as Sentry from "@sentry/nextjs";
 
 export default function Layout({ children }: { children: ReactNode }) {
   // Add Pro badges to example pages in sidebar.
@@ -22,7 +22,7 @@ export default function Layout({ children }: { children: ReactNode }) {
 
           page.name = (
             <span>
-              {exampleData.title}
+              <span className="mr-1">{exampleData.title}</span>
               {exampleData.isPro && <ProBadge />}
             </span>
           );
@@ -34,10 +34,25 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <CustomDocsLayout tree={source.pageTree} {...baseOptions}>
-        {children}
+        <Sentry.ErrorBoundary
+          fallback={
+            <div>
+              We encountered an error trying to show this page. Please report
+              this to us on GitHub at{" "}
+              <a href="https://github.com/TypeCellOS/BlockNote/issues">
+                https://github.com/TypeCellOS/BlockNote/issues
+              </a>
+            </div>
+          }
+          beforeCapture={(scope) => {
+            scope.setTag("type", "react-render");
+            scope.setTag("page", "examples");
+          }}
+        >
+          {children}
+        </Sentry.ErrorBoundary>
       </CustomDocsLayout>
       <Footer />
-      <Analytics />
     </>
   );
 }
