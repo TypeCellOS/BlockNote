5a91c8ca5e5b50af591555416e5c70f7a94a36c6
Matthew Lipski <50169049+matthewlipski@users.noreply.github.com>
2025-09-24 10:13:19 +0200

    docs:  add ElectricSQL example (#2023)

---
+++ examples/07-collaboration/04-electric-sql/src/App.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export default function App() {
+  return (
+    <iframe
+      src="https://blocknote-electric-example.vercel.app/"
+      height="100%"
+      width="100%"
+    />
+  );
+}
--- playground/src/examples.gen.tsx
+++ playground/src/examples.gen.tsx
@@ -1428,9 +1428,30 @@
         "readme": "In this example, we use Y-Sweet to let multiple users collaborate on a single BlockNote document in real-time.\n\n**Try it out:** Open the [Y-Sweet BlockNote demo](https://demos.y-sweet.dev/blocknote) in multiple browser tabs to see it in action!\n\n**Relevant Docs:**\n\n- [Editor Setup](/docs/getting-started/editor-setup)\n- [Real-time collaboration](/docs/features/collaboration)\n- [Y-Sweet on Jamsocket](https://docs.jamsocket.com/y-sweet/tutorials/blocknote)"
       },
       {
+        "projectSlug": "electric-sql",
+        "fullSlug": "collaboration/electric-sql",
+        "pathFromRoot": "examples/07-collaboration/04-electric-sql",
+        "config": {
+          "playground": true,
+          "docs": true,
+          "author": "matthewlipski",
+          "tags": [
+            "Advanced",
+            "Saving/Loading",
+            "Collaboration"
+          ]
+        },
+        "title": "Collaborative Editing with ElectricSQL",
+        "group": {
+          "pathFromRoot": "examples/07-collaboration",
+          "slug": "collaboration"
+        },
+        "readme": "In this example, we use ElectricSQL to let multiple users collaborate on a single BlockNote document in real-time. The setup for this demo is more involved than the other collaboration examples, as it requires a running server and has a more fully-fledged UI. Therefore, the demo just uses an iframe element to show a hosted instance of the full ElectricSQL + BlockNote setup, which you can find the code for [here](https://github.com/TypeCellOS/blocknote-electric-example).\n\n**Try it out:** Open this page (or the [iframe url](https://blocknote-electric-example.vercel.app/)) in a new browser tab or window to see it in action!\n\n**Relevant Docs:**\n\n- [Editor Setup](/docs/getting-started/editor-setup)\n- [Real-time collaboration](/docs/features/collaboration)\n- [ElectricSQL](https://electric-sql.com/)"
+      },
+      {
         "projectSlug": "comments",
         "fullSlug": "collaboration/comments",
-        "pathFromRoot": "examples/07-collaboration/04-comments",
+        "pathFromRoot": "examples/07-collaboration/05-comments",
         "config": {
           "playground": true,
           "docs": true,
@@ -1455,7 +1476,7 @@
       {
         "projectSlug": "comments-with-sidebar",
         "fullSlug": "collaboration/comments-with-sidebar",
-        "pathFromRoot": "examples/07-collaboration/05-comments-with-sidebar",
+        "pathFromRoot": "examples/07-collaboration/06-comments-with-sidebar",
         "config": {
           "playground": true,
           "docs": true,
@@ -1480,7 +1501,7 @@
       {
         "projectSlug": "ghost-writer",
         "fullSlug": "collaboration/ghost-writer",
-        "pathFromRoot": "examples/07-collaboration/06-ghost-writer",
+        "pathFromRoot": "examples/07-collaboration/07-ghost-writer",
         "config": {
           "playground": true,
           "docs": false,
@@ -1505,7 +1526,7 @@
       {
         "projectSlug": "forking",
         "fullSlug": "collaboration/forking",
-        "pathFromRoot": "examples/07-collaboration/07-forking",
+        "pathFromRoot": "examples/07-collaboration/08-forking",
         "config": {
           "playground": true,
           "docs": false,
