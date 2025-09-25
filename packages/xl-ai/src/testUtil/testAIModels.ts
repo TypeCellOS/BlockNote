935b9581abaefb1f769cbcf6cb6165800d323572
yousefed <yousefdardiry@gmail.com>
2025-09-25 15:26:55 +0200

    fix api keys

--- packages/xl-ai/src/testUtil/testAIModels.ts
+++ packages/xl-ai/src/testUtil/testAIModels.ts
@@ -5,21 +5,21 @@ import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
 import { LanguageModel } from "ai";
 
 const groq = createGroq({
-  apiKey: process.env.GROQ_API_KEY,
+  apiKey: process.env.GROQ_API_KEY || "not-available-in-ci",
 })("llama-3.3-70b-versatile");
 
 const openai = createOpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY || "not-available-in-ci",
 })("gpt-4o-2024-08-06");
 
 const anthropic = createAnthropic({
-  apiKey: process.env.ANTHROPIC_API_KEY,
+  apiKey: process.env.ANTHROPIC_API_KEY || "not-available-in-ci",
 })("claude-3-7-sonnet-latest");
 
 const albert = createOpenAICompatible({
   name: "albert-etalab",
   baseURL: "https://albert.api.etalab.gouv.fr/v1",
-  apiKey: process.env.ALBERT_API_KEY,
+  apiKey: process.env.ALBERT_API_KEY || "not-available-in-ci",
 })("albert-etalab.chat/albert-large");
 
 export const testAIModels: Record<
