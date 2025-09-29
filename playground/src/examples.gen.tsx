1770bb1fc2b127eb4005fc8791166fe4bb31510a
Matthew Lipski <50169049+matthewlipski@users.noreply.github.com>
2025-09-29 16:45:41 +0200

    fix: ShadCN example Tailwind setup (#2042)
    * Added option to `.bnexample.json` to run example in Tailwind project
    
    * Added `type: "module"` to examples `package.json`
    
    * Added option to `.bnexample.json` to hide StackBlitz link

--- playground/src/examples.gen.tsx
+++ playground/src/examples.gen.tsx
@@ -179,14 +179,16 @@
           "author": "matthewlipski",
           "tags": [
             "Basic"
-          ]
+          ],
+          "tailwind": true,
+          "stackBlitz": false
         },
         "title": "Use with ShadCN",
         "group": {
           "pathFromRoot": "examples/01-basic",
           "slug": "basic"
         },
-        "readme": "This example shows how you can use BlockNote with ShadCN (instead of Mantine).\n\n<Callout type={\"warning\"}>\nBecause the ShadCN version of BlockNote requires being in a Tailwind app,\nviewing the demo below on StackBlitz will have missing styles.\n</Callout>\n\n**Relevant Docs:**\n\n- [Getting Started with ShadCN](/docs/getting-started/shadcn)"
+        "readme": "This example shows how you can use BlockNote with ShadCN (instead of Mantine).\n\n**Relevant Docs:**\n\n- [Getting Started with ShadCN](/docs/getting-started/shadcn)"
       },
       {
         "projectSlug": "localization",
