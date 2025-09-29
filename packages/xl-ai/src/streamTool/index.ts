0112171a649cd9fafa2b942be6f148b46b1c2d89
yousefed <yousefdardiry@gmail.com>
2025-09-29 13:13:04 +0200

    fix

--- packages/xl-ai/src/streamTool/index.ts
+++ packages/xl-ai/src/streamTool/index.ts
@@ -1,5 +1,5 @@
-export * from ".Executor.js";
 export * from "./jsonSchema.js";
+export * from "./StreamToolExecutor.js";
 export * from "./toolDefinitionsToToolSet.js";
 export * from "./vercelAiSdk/clientside/ClientSideTransport.js";
 export * from "./vercelAiSdk/util/partialObjectStreamUtil.js";
