664aedcfaa16d17f0693072462a925efc05bfcbc
Matthew Lipski <matthewlipski@gmail.com>
2025-10-02 22:25:39 +0200

    Minor type fix

--- packages/core/src/api/exporters/markdown/util/convertVideoToMarkdownRehypePlugin.ts
+++ packages/core/src/api/exporters/markdown/util/convertVideoToMarkdownRehypePlugin.ts
@@ -1,10 +1,11 @@
+import { Parent as HASTParent } from "hast";
 import { visit } from "unist-util-visit";
 
 // Originally, rehypeParse parses videos as links, which is incorrect.
 export function convertVideoToMarkdown() {
-  return (tree: any) => {
+  return (tree: HASTParent) => {
     visit(tree, "element", (node, index, parent) => {
-      if (node.tagName === "video") {
+      if (parent && node.tagName === "video") {
         const src = node.properties?.src || node.properties?.["data-url"] || "";
         const name =
           node.properties?.title || node.properties?.["data-name"] || "";
