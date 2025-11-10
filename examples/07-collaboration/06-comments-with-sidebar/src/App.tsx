TypeCellOS/BlockNote

commit 1b2e86b5c5c89d7d5c3faab61431f6395f30a24c

Open feat: Update Mantine setup (#2033)

packages/mantine/src/blocknoteStyles.css
@@ -1,0 +1,794 @@
+@import url("@blocknote/react/style.css");
+
+/* Mantine base styles*/
+
+/* Removes Mantine active styles */
+.bn-mantine .mantine-active:active {
+  transform: none;
+}
+
+/* Mantine Badge component base styles */
+.bn-mantine .mantine-Badge-root {
+  background-color: var(--bn-colors-tooltip-background);
+  color: var(--bn-colors-tooltip-text);
+}
+
+/* Mantine FileInput component base styles */
+.bn-mantine .mantine-FileInput-input {
+  align-items: center;
+  background-color: var(--bn-colors-menu-background);
+  border: none;
+  border-radius: 4px;
+  color: var(--bn-colors-menu-text);
+  display: flex;
+  flex-direction: row;
+  font-family: var(--bn-font-family);
+  justify-content: center;
+}
+
+.bn-mantine .mantine-FileInput-input:hover {
+  background-color: var(--bn-colors-hovered-background);
+}
+
+.bn-mantine .mantine-FileInput-wrapper {
+  border: solid var(--bn-colors-border) 1px;
+  border-radius: 4px;
+}
+
+.bn-mantine .mantine-InputPlaceholder-placeholder {
+  color: var(--bn-colors-menu-text);
+  font-family: var(--bn-font-family);
+  font-weight: 600;
+}
+
+/* Mantine Menu component base styles */
+.bn-mantine .mantine-Menu-dropdown,
+.bn-mantine .bn-menu-dropdown {
+  background-color: var(--bn-colors-menu-background);
+  border: var(--bn-border);
+  border-radius: var(--bn-border-radius-medium);
+  box-shadow: var(--bn-shadow-medium);
+  box-sizing: border-box;
+  color: var(--bn-colors-menu-text);
+  padding: 2px;
+  overflow: auto;
+}
+
+.bn-mantine .mantine-Menu-label {
+  background-color: var(--bn-colors-menu-background);
+  color: var(--bn-colors-menu-text);
+}
+
+.bn-mantine .mantine-Menu-item {
+  background-color: var(--bn-colors-menu-background);
+  border: none;
+  border-radius: var(--bn-border-radius-small);
+  color: var(--bn-colors-menu-text);
+}
+
+.bn-mantine .mantine-Menu-item[aria-selected="true"],
+.bn-mantine .mantine-Menu-item:hover {
+  background-color: var(--bn-colors-hovered-background);
+  border: none;
+  color: var(--bn-colors-hovered-text);
+}
+
+/* Mantine Popover component base styles */
+.bn-mantine .mantine-Popover-dropdown:not(.bn-menu-dropdown) {
+  background-color: transparent;
+  border: none;
+  border-radius: 0;
+  box-shadow: none;
+  padding: 0;
+}
+
+/* Mantine Tabs component base styles */
+.bn-mantine .mantine-Tabs-root {
+  width: 100%;
+  background-color: var(--bn-colors-menu-background);
+}
+
+.bn-mantine .mantine-Tabs-list:before {
+  border-color: var(--bn-colors-hovered-background);
+}
+
+.bn-mantine .mantine-Tabs-tab {
+  color: var(--bn-colors-menu-text);
+  border-color: var(--bn-colors-hovered-background);
+}
+
+.bn-mantine .mantine-Tabs-tab:hover {
+  background-color: var(--bn-colors-hovered-background);
+  border-color: var(--bn-colors-hovered-background);
+  color: var(--bn-colors-hovered-text);
+}
+
+.bn-mantine .mantine-Tabs-tab[data-active],
+.bn-mantine .mantine-Tabs-tab[data-active]:hover {
+  border-color: var(--bn-colors-menu-text);
+  color: var(--bn-colors-menu-text);
+}
+
+.bn-mantine .mantine-Tabs-panel {
+  padding: 8px;
+}
+
+/* Mantine TextInput component base styles */
+.bn-mantine .mantine-TextInput-input {
+  background-color: var(--bn-colors-menu-background);
+  border: solid var(--bn-colors-border) 1px;
+  border-radius: 4px;
+  color: var(--bn-colors-menu-text);
+  font-family: var(--bn-font-family);
+  height: 32px;
+}
+
+.bn-mantine .bn-mt-input-large .mantine-TextInput-input {
+  border: none;
+  font-size: 14px;
+  height: 52px;
+}
+
+/* Mantine Tooltip component base styles */
+.bn-mantine .mantine-Tooltip-tooltip {
+  background-color: transparent;
+  border: none;
+  border-radius: 0;
+  box-shadow: none;
+  padding: 0;
+}
+
+.bn-mantine .mantine-Tooltip-tooltip p:last-child {
+  white-space: pre-wrap;
+}
+
+.bn-mantine .mantine-Tooltip-tooltip p:first-child {
+  white-space: unset;
+}
+
+/* UI element styling */
+
+/* Select styling */
+.bn-mantine .bn-select {
+  overflow: auto;
+}
+
+.bn-mantine .mantine-Button-root[aria-controls*="dropdown"] {
+  min-width: fit-content;
+}
+
+/* Toolbar styling */
+.bn-mantine .bn-toolbar {
+  background-color: var(--bn-colors-menu-background);
+  border: var(--bn-border);
+  border-radius: var(--bn-border-radius-medium);
+  box-shadow: var(--bn-shadow-medium);
+  flex-wrap: nowrap;
+  gap: 2px;
+  padding: 2px;
+  width: fit-content;
+  overflow-x: auto;
+  max-width: 100vw;
+}
+
+.bn-mantine .bn-toolbar:empty {
+  display: none;
+}
+
+.bn-toolbar .mantine-Button-root,
+.bn-toolbar .mantine-ActionIcon-root {
+  background-color: var(--bn-colors-menu-background);
+  border: none;
+  border-radius: var(--bn-border-radius-small);
+  color: var(--bn-colors-menu-text);
+}
+
+.bn-toolbar .mantine-Button-root:hover,
+.bn-toolbar .mantine-ActionIcon-root:hover {
+  background-color: var(--bn-colors-hovered-background);
+  border: none;
+  color: var(--bn-colors-hovered-text);
+}
+
+.bn-toolbar .mantine-Button-root[data-selected],
+.bn-toolbar .mantine-ActionIcon-root[data-selected] {
+  background-color: var(--bn-colors-selected-background);
+  border: none;
+  color: var(--bn-colors-selected-text);
+}
+
+.bn-toolbar .mantine-Button-root[data-disabled],
+.bn-toolbar .mantine-ActionIcon-root[data-disabled] {
+  background-color: var(--bn-colors-disabled-background);
+  border: none;
+  color: var(--bn-colors-disabled-text);
+}
+
+.bn-toolbar .mantine-Menu-item {
+  font-size: 12px;
+  height: 30px;
+}
+
+.bn-toolbar .mantine-Menu-item:hover {
+  background-color: var(--bn-colors-hovered-background);
+}
+
+.bn-mantine .bn-form-popover {
+  background-color: var(--bn-colors-menu-background);
+  border: var(--bn-border);
+  border-radius: var(--bn-border-radius-medium);
+  box-shadow: var(--bn-shadow-medium);
+  color: var(--bn-colors-menu-text);
+  gap: 4px;
+  min-width: 145px;
+  padding: 2px;
+}
+
+.bn-form-popover .mantine-TextInput-root,
+.bn-form-popover .mantine-FileInput-root {
+  width: 300px;
+}
+
+.bn-form-popover .mantine-TextInput-wrapper,
+.bn-form-popover .mantine-FileInput-wrapper {
+  padding: 0;
+  border-radius: 4px;
+}
+
+.bn-form-popover .mantine-TextInput-wrapper:hover {
+  background-color: var(--bn-colors-hovered-background);
+}
+
+.bn-form-popover .mantine-TextInput-input,
+.bn-form-popover .mantine-FileInput-input {
+  border: none;
+  font-size: 12px;
+}
+
+.bn-form-popover .mantine-FileInput-input:hover {
+  background-color: var(--bn-colors-hovered-background);
+}
+
+.bn-form-popover .mantine-FileInput-section[data-position="left"] {
+  color: var(--bn-colors-menu-text);
+}
+
+.bn-form-popover .mantine-FileInput-placeholder {
+  color: var(--bn-colors-menu-text);
+}
+
+/* Suggestion Menu styling*/
+
+/* Base styles for Suggestion Menus, copied from the Mantine Menu component. */
+/* Unfortunately necessary, as we can't use a Menu.Dropdown component on its
+ own. */
+/* https://github.com/mantinedev/mantine/blob/e3e3bb834de1f2f75a27dbc757dc0a2fc6a6cba8/packages/%40mantine/core/src/components/Menu/Menu.module.css */
+.bn-mantine .bn-suggestion-menu {
+  max-height: 100%;
+  position: relative;
+  box-shadow: var(--mantine-shadow-md);
+  border: calc(0.0625rem * var(--mantine-scale)) solid
+    var(--mantine-color-gray-2);
+  border-radius: var(--mantine-radius-default);
+  padding: 4px;
+}
+
+.bn-mantine .bn-suggestion-menu-label {
+  color: var(--mantine-color-dimmed);
+  font-weight: 500;
+  font-size: var(--mantine-font-size-xs);
+  padding: calc(var(--mantine-spacing-xs) / 2) var(--mantine-spacing-sm);
+  cursor: default;
+}
+
+.bn-mantine .bn-suggestion-menu-item {
+  font-size: var(--mantine-font-size-sm);
+  width: 100%;
+  padding: calc(var(--mantine-spacing-xs) / 1.5) var(--mantine-spacing-sm);
+  border-radius: var(--popover-radius, var(--mantine-radius-default));
+  color: var(--menu-item-color, var(--mantine-color-text));
+  display: flex;
+  align-items: center;
+  user-select: none;
+
+  &:where([data-disabled], :disabled) {
+    color: var(--mantine-color-dimmed);
+    opacity: 0.6;
+    pointer-events: none;
+  }
+}
+
+/* Additional Suggestion Menu styling*/
+.bn-mt-suggestion-menu-item-body {
+  flex: 1;
+}
+
+.bn-mt-suggestion-menu-item-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:where([data-position="left"]) {
+    margin-inline-end: var(--mantine-spacing-xs);
+  }
+
+  &:where([data-position="right"]) {
+    margin-inline-start: var(--mantine-spacing-xs);
+  }
+}
+
+.bn-mantine .bn-suggestion-menu {
+  background-color: var(--bn-colors-menu-background);
+  border: var(--bn-border);
+  border-radius: var(--bn-border-radius-medium);
+  box-shadow: var(--bn-shadow-medium);
+  box-sizing: border-box;
+  color: var(--bn-colors-menu-text);
+  height: fit-content;
+  max-height: inherit;
+  overflow-y: auto;
+  padding: 2px;
+}
+
+.bn-mantine .bn-suggestion-menu-item {
+  cursor: pointer;
+  height: 52px;
+}
+
+.bn-mantine .bn-suggestion-menu-item-small {
+  height: fit-content;
+  /* Made to match with labels */
+  padding: calc(var(--mantine-spacing-xs) / 2) var(--mantine-spacing-sm);
+}
+
+.bn-mantine .bn-suggestion-menu-item[aria-selected="true"],
+.bn-mantine .bn-suggestion-menu-item:hover {
+  background-color: var(--bn-colors-hovered-background);
+}
+
+.bn-mt-suggestion-menu-item-section {
+  color: var(--bn-colors-tooltip-text);
+}
+
+.bn-mt-suggestion-menu-item-section[data-position="left"] {
+  background-color: var(--bn-colors-tooltip-background);
+  border-radius: var(--bn-border-radius-small);
+  padding: 8px;
+}
+
+.bn-suggestion-menu-item-small
+  .bn-mt-suggestion-menu-item-section[data-position="left"] {
+  background-color: transparent;
+  padding: 0;
+}
+
+.bn-suggestion-menu-item-small
+  .bn-mt-suggestion-menu-item-section[data-position="left"]
+  svg {
+  height: 14px;
+  width: 14px;
+}
+
+.bn-mt-suggestion-menu-item-body {
+  align-items: stretch;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding-right: 16px;
+}
+
+.bn-mt-suggestion-menu-item-title {
+  color: var(--bn-colors-menu-text);
+  line-height: 20px;
+  font-weight: 500;
+  font-size: 14px;
+  margin: 0;
+  padding: 0;
+}
+
+.bn-suggestion-menu-item-small .bn-mt-suggestion-menu-item-title {
+  font-size: 12px;
+}
+
+.bn-mt-suggestion-menu-item-subtitle {
+  color: var(--bn-colors-menu-text);
+  line-height: 16px;
+  font-size: 10px;
+  margin: 0;
+  padding: 0;
+}
+
+.bn-suggestion-menu-item-small .bn-mt-suggestion-menu-item-subtitle {
+  display: none;
+}
+
+.bn-mantine .bn-suggestion-menu-label {
+  color: var(--bn-colors-hovered-text);
+}
+
+.bn-mantine .bn-suggestion-menu-loader {
+  height: 20px;
+  width: 100%;
+}
+
+.bn-mantine .bn-suggestion-menu-loader span {
+  background-color: var(--bn-colors-side-menu);
+}
+
+.bn-mantine .bn-grid-suggestion-menu {
+  background: var(--bn-colors-menu-background);
+  border-radius: var(--bn-border-radius-large);
+  box-shadow: var(--bn-shadow-medium);
+  display: grid;
+  gap: 7px;
+  height: fit-content;
+  justify-items: center;
+  max-height: inherit;
+  overflow-y: auto;
+  padding: 20px;
+}
+
+.bn-mantine .bn-grid-suggestion-menu-item {
+  align-items: center;
+  border-radius: var(--bn-border-radius-large);
+  cursor: pointer;
+  display: flex;
+  font-size: 24px;
+  height: 32px;
+  justify-content: center;
+  margin: 2px;
+  padding: 4px;
+  width: 32px;
+}
+
+.bn-mantine .bn-grid-suggestion-menu-item[aria-selected="true"],
+.bn-mantine .bn-grid-suggestion-menu-item:hover {
+  background-color: var(--bn-colors-hovered-background);
+}
+
+.bn-mantine .bn-grid-suggestion-menu-empty-item,
+.bn-mantine .bn-grid-suggestion-menu-loader {
+  align-items: center;
+  color: var(--bn-colors-menu-text);
+  display: flex;
+  font-size: 14px;
+  font-weight: 500;
+  height: 32px;
+  justify-content: center;
+}
+
+.bn-mantine .bn-grid-suggestion-menu-loader span {
+  background-color: var(--bn-colors-side-menu);
+}
+
+/* Side Menu styling */
+.bn-mantine .bn-side-menu {
+  background-color: transparent;
+  overflow: visible;
+}
+
+.bn-side-menu .mantine-Menu-item,
+.bn-table-handle-menu .mantine-Menu-item {
+  font-size: 12px;
+  height: 30px;
+}
+
+.bn-side-menu .mantine-UnstyledButton-root:not(.mantine-Menu-item) {
+  background-color: transparent;
+}
+
+.bn-side-menu .mantine-UnstyledButton-root:hover {
+  background-color: var(--bn-colors-hovered-background);
+}
+
+.bn-side-menu .mantine-UnstyledButton-root:not(.mantine-Menu-item) svg {
+  background-color: transparent;
+  color: var(--bn-colors-side-menu);
+  height: 22px;
+  width: 22px;
+}
+
+.bn-mantine .bn-side-menu > [draggable="true"] {
+  display: flex;
+}
+
+.bn-side-menu .mantine-Menu-dropdown {
+  min-width: 100px;
+  padding: 2px;
+  position: absolute;
+}
+
+/* Image Panel styling*/
+.bn-mantine .bn-panel {
+  background-color: var(--bn-colors-menu-background);
+  border: var(--bn-border);
+  border-radius: var(--bn-border-radius-medium);
+  box-shadow: var(--bn-shadow-medium);
+  padding: 2px;
+  width: 500px;
+}
+
+.bn-mantine .bn-panel .bn-tab-panel {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+}
+
+.bn-panel .mantine-TextInput-root,
+.bn-panel .mantine-FileInput-root {
+  width: 100%;
+}
+
+.bn-panel .mantine-Button-root {
+  background-color: var(--bn-colors-menu-background);
+  border: solid var(--bn-colors-border) 1px;
+  border-radius: var(--bn-border-radius-small);
+  color: var(--bn-colors-menu-text);
+  height: 32px;
+  width: 60%;
+}
+
+.bn-panel .mantine-Button-root:hover {
+  background-color: var(--bn-colors-hovered-background);
+}
+
+.bn-panel .mantine-Text-root {
+  text-align: center;
+}
+
+/* Table Handle styling */
+.bn-mantine .bn-table-handle,
+.bn-mantine .bn-extend-button,
+.bn-mantine .bn-table-cell-handle {
+  align-items: center;
+  background-color: var(--bn-colors-menu-background);
+  border: var(--bn-border);
+  border-radius: var(--bn-border-radius-small);
+  box-shadow: var(--bn-shadow-light);
+  color: var(--bn-colors-side-menu);
+  cursor: grab;
+  display: flex;
+  height: fit-content;
+  justify-content: center;
+  overflow: visible;
+  padding: 0;
+}
+
+.bn-mantine .bn-table-cell-handle {
+  padding: 0 4px;
+}
+
+.bn-mantine .bn-table-handle svg {
+  margin-inline: -4px;
+}
+
+.bn-mantine .bn-table-handle-not-draggable {
+  cursor: pointer;
+}
+
+.bn-mantine .bn-table-handle:hover,
+.bn-mantine .bn-table-handle-dragging,
+.bn-mantine .bn-extend-button:hover,
+.bn-mantine .bn-extend-button-editing,
+.bn-mantine .bn-table-cell-handle:hover {
+  background-color: var(--bn-colors-hovered-background);
+}
+
+.bn-mantine .bn-extend-button-add-remove-columns {
+  height: 100%;
+  width: 18px;
+  margin-left: 4px;
+  cursor: col-resize;
+}
+
+.bn-mantine .bn-extend-button-add-remove-rows {
+  height: 18px;
+  width: 100%;
+  margin-top: 4px;
+  cursor: row-resize;
+}
+
+/* Drag Handle & Table Handle Menu styling */
+.bn-mantine .bn-drag-handle-menu,
+.bn-mantine .bn-table-handle-menu {
+  overflow: visible;
+}
+
+/* Tooltip styling */
+.bn-mantine .bn-tooltip {
+  background-color: var(--bn-colors-tooltip-background);
+  border: var(--bn-border);
+  border-radius: var(--bn-border-radius-medium);
+  box-shadow: var(--bn-shadow-medium);
+  color: var(--bn-colors-tooltip-text);
+  padding: 4px 10px;
+  text-align: center;
+}
+
+/* Additional menu styles */
+.bn-mantine .bn-tick-space {
+  padding: 0;
+  width: 20px;
+}
+
+/* Comment styling */
+.bn-mantine .bn-thread {
+  background-color: var(--bn-colors-menu-background);
+  border: var(--bn-border);
+  border-radius: var(--bn-border-radius-medium);
+  box-shadow: var(--bn-shadow-medium);
+  color: var(--bn-colors-menu-text);
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  min-width: 350px;
+  overflow: visible;
+}
+
+.bn-mantine .bn-threads-sidebar .bn-thread {
+  box-shadow: none;
+}
+
+.bn-mantine .bn-thread:not(.selected) {
+  cursor: pointer;
+}
+
+.bn-mantine .bn-thread-comments,
+.bn-mantine .bn-thread-composer {
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+}
+
+.bn-mantine .bn-thread-comments {
+  gap: 1rem;
+}
+
+.bn-mantine .bn-thread-comment {
+  align-items: flex-start;
+  flex-direction: column;
+  gap: 0.25rem;
+}
+
+.bn-mantine .bn-thread-comment > .mantine-Group-root {
+  gap: 12px;
+}
+
+.bn-mantine .bn-thread-comment p {
+  color: var(--bn-colors-menu-text);
+}
+
+.bn-mantine .bn-thread .bn-header-text,
+.bn-mantine .bn-thread-comment .bn-resolved-text {
+  font-size: 0.8rem;
+  font-style: italic;
+}
+
+.bn-mantine .bn-comment-actions-wrapper {
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+}
+
+/* Comment action toolbar styling */
+.bn-mantine .bn-action-toolbar {
+  align-self: flex-end;
+  background-color: var(--bn-colors-menu-background);
+  border: var(--bn-border);
+  border-radius: var(--bn-border-radius-medium);
+  gap: 0;
+  padding: 2px;
+}
+
+.bn-action-toolbar .mantine-Button-root,
+.bn-action-toolbar .mantine-ActionIcon-root {
+  background-color: var(--bn-colors-menu-background);
+  border: none;
+  border-radius: var(--bn-border-radius-small);
+  color: var(--bn-colors-menu-text);
+}
+
+.bn-action-toolbar .mantine-Button-root:hover,
+.bn-action-toolbar .mantine-ActionIcon-root:hover {
+  background-color: var(--bn-colors-hovered-background);
+  border: none;
+  color: var(--bn-colors-hovered-text);
+}
+
+.bn-action-toolbar .mantine-Button-root[data-selected],
+.bn-action-toolbar .mantine-ActionIcon-root[data-selected] {
+  background-color: var(--bn-colors-selected-background);
+  border: none;
+  color: var(--bn-colors-selected-text);
+}
+
+.bn-action-toolbar .mantine-Button-root[data-disabled],
+.bn-action-toolbar .mantine-ActionIcon-root[data-disabled] {
+  background-color: var(--bn-colors-disabled-background);
+  border: none;
+  color: var(--bn-colors-disabled-text);
+}
+
+.bn-mantine .bn-action-toolbar .mantine-Menu-itemLabel {
+  font-size: 12px;
+}
+
+/* Badge styling */
+.bn-mantine .bn-badge-group {
+  display: flex;
+  gap: 4px;
+  justify-content: flex-start;
+  width: 100%;
+}
+
+.bn-mantine .bn-badge {
+  flex-grow: 0;
+}
+
+.bn-mantine .bn-badge .mantine-Chip-label {
+  padding: 0 8px;
+}
+
+.bn-mantine .bn-badge .mantine-Chip-label:not([data-checked="true"]) {
+  background-color: var(--bn-colors-menu-background);
+  border: var(--bn-border);
+  color: var(--bn-colors-menu-text);
+}
+
+.bn-mantine .bn-badge .mantine-Chip-label:hover {
+  border: 1px solid var(--mantine-primary-color-filled-hover);
+  color: var(--mantine-primary-color-filled-hover);
+}
+
+.bn-mantine
+  .bn-badge
+  .mantine-Chip-label
+  > span:not(.mantine-Chip-iconWrapper) {
+  display: inline-flex;
+  gap: 4px;
+}
+
+.bn-mantine
+  .bn-badge
+  .mantine-Chip-label
+  > span:not(.mantine-Chip-iconWrapper)
+  > span {
+  align-items: center;
+  display: inline-flex;
+  justify-content: center;
+}
+
+/* Combobox styling */
+.bn-mantine .bn-combobox-input,
+.bn-mantine .bn-combobox-items:not(:empty) {
+  background-color: var(--bn-colors-menu-background);
+  border: var(--bn-border);
+  border-radius: var(--bn-border-radius-medium);
+  box-shadow: var(--bn-shadow-medium);
+  color: var(--bn-colors-menu-text);
+  gap: 4px;
+  min-width: 145px;
+  padding: 2px;
+}
+
+.bn-mantine .bn-combobox-input .bn-combobox-icon,
+.bn-mantine .bn-combobox-input .bn-combobox-right-section {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+
+.bn-mantine .bn-combobox-input .bn-combobox-error {
+  color: var(--bn-colors-highlights-red-background);
+}
+
+/* We need to get rid of the checked icon - you can set the icon prop to an
+empty element (<></>), but even so Mantine leaves extra space for the icon, so
+we just don't display it in CSS instead. */
+.bn-mantine .bn-badge .mantine-Chip-iconWrapper {
+  display: none;
+}

pnpm-lock.yaml
@@ -147,6 +147,12 @@ importers:
       '@mantine/core':
         specifier: ^8.3.2
         version: 8.3.2(@mantine/hooks@8.3.2(react@19.1.0))(@types/react@19.1.8)(react-dom@19.1.0(react@19.1.0))(react@19.1.0)
+      '@mantine/hooks':
+        specifier: ^8.3.2
+        version: 8.3.2(react@19.1.0)
+      '@mantine/utils':
+        specifier: ^6.0.22
+        version: 6.0.22(react@19.1.0)
       '@mui/icons-material':
         specifier: ^5.16.1
         version: 5.17.1(@mui/material@5.17.1(@emotion/react@11.14.0(@types/react@19.1.8)(react@19.1.0))(@emotion/styled@11.14.0(@emotion/react@11.14.0(@types/react@19.1.8)(react@19.1.0))(@types/react@19.1.8)(react@19.1.0))(@types/react@19.1.8)(react-dom@19.1.0(react@19.1.0))(react@19.1.0))(@types/react@19.1.8)(react@19.1.0)
@@ -4670,6 +4676,12 @@ importers:
       '@mantine/core':
         specifier: ^8.3.2
         version: 8.3.2(@mantine/hooks@8.3.2(react@19.1.0))(@types/react@19.1.8)(react-dom@19.1.0(react@19.1.0))(react@19.1.0)
+      '@mantine/hooks':
+        specifier: ^8.3.2
+        version: 8.3.2(react@19.1.0)
+      '@mantine/utils':
+        specifier: ^6.0.22
+        version: 6.0.22(react@19.1.0)
       '@mui/icons-material':
         specifier: ^5.16.1
         version: 5.17.1(@mui/material@5.17.1(@emotion/react@11.14.0(@types/react@19.1.8)(react@19.1.0))(@emotion/styled@11.14.0(@emotion/react@11.14.0(@types/react@19.1.8)(react@19.1.0))(@types/react@19.1.8)(react@19.1.0))(@types/react@19.1.8)(react-dom@19.1.0(react@19.1.0))(react@19.1.0))(@types/react@19.1.8)(react@19.1.0)

docs/content/docs/getting-started/mantine.mdx
@@ -9,17 +9,27 @@ imageTitle: Mantine rich text editor using BlockNote
 [Mantine](https://mantine.dev/) is an open-source collection of React components.
 
 ```console tab="npm"
+npm install @mantine/core @mantine/hooks @mantine/util
 npm install @blocknote/core @blocknote/react @blocknote/mantine
 ```
 
 ```console tab="pnpm"
+pnpm add @mantine/core @mantine/hooks @mantine/util
 pnpm add @blocknote/core @blocknote/react @blocknote/mantine
 ```
 
 ```console tab="bun"
+bun add @mantine/core @mantine/hooks @mantine/util
 bun add @blocknote/core @blocknote/react @blocknote/mantine
 ```
 
 To use BlockNote with Mantine, you can import `BlockNoteView` from `@blocknote/mantine` and the stylesheet from `@blocknote/mantine/style.css`.
+
+<Callout type={"info"}>
+  If your application already uses Mantine UI, you should use the
+  `@blocknote/mantine/blocknoteStyles.css` stylesheet instead. It only contains
+  the styles added by BlockNote on top of the Mantine core styles, whereas
+  `@blocknote/mantine/style.css` includes both.
+</Callout>
 
 <Example name="basic/minimal" />

packages/mantine/src/BlockNoteView.tsx
@@ -18,7 +18,6 @@ import {
   Theme,
 } from "./BlockNoteTheme.js";
 import { components } from "./components.js";
-import "./style.css";
 
 export const BlockNoteView = <
   BSchema extends BlockSchema,
@@ -98,9 +97,10 @@ export const BlockNoteView = <
 
   return (
     <MantineProvider
-      // Scopes Mantine CSS variables to only the editor, as proposed here:
-      // https://github.com/orgs/mantinedev/discussions/5685
-      cssVariablesSelector=".bn-mantine"
+      // By default, Mantine adds its CSS variables to the root. This disables
+      // that, as we instead set the variables on `.bn-mantine` in
+      // `mantineStyles.css`.
+      withCssVariables={false}
       // This gets the element to set `data-mantine-color-scheme` on. This
       // element needs to already be rendered, so we can't set it to the
       // editor container element. Instead, we set it to `undefined` and set it

packages/mantine/src/components.tsx
@@ -21,7 +21,6 @@ import { PanelTextInput } from "./panel/PanelTextInput.js";
 import { Popover, PopoverContent, PopoverTrigger } from "./popover/Popover.js";
 import { SideMenu } from "./sideMenu/SideMenu.js";
 import { SideMenuButton } from "./sideMenu/SideMenuButton.js";
-import "./style.css";
 import { SuggestionMenu } from "./suggestionMenu/SuggestionMenu.js";
 import { SuggestionMenuEmptyItem } from "./suggestionMenu/SuggestionMenuEmptyItem.js";
 import { SuggestionMenuItem } from "./suggestionMenu/SuggestionMenuItem.js";

commit eedd5518284b94ec197bf3c4b8c4efc786493785

fix(comments): update the styles for the cursor to be the default cursor
Addresses: https://linear.app/blocknote/issue/BLO-333/comments-cursor
Introduced by: https://github.com/TypeCellOS/BlockNote/pull/2033

examples/07-collaboration/06-comments-with-sidebar/src/App.tsx
@@ -12,8 +12,9 @@ import {
   ThreadsSidebar,
   useCreateBlockNote,
 } from "@blocknote/react";
-import { YDocProvider, useYDoc, useYjsProvider } from "@y-sweet/react";
 import { useMemo, useState } from "react";
+import YPartyKitProvider from "y-partykit/provider";
+import * as Y from "yjs";
 
 import { SettingsSelect } from "./SettingsSelect";
 import { HARDCODED_USERS, MyUserType, getRandomColor } from "./userdata";
@@ -31,23 +32,19 @@ async function resolveUsers(userIds: string[]) {
   return HARDCODED_USERS.filter((user) => userIds.includes(user.id));
 }
 
+// Sets up Yjs document and PartyKit Yjs provider.
+const doc = new Y.Doc();
+const provider = new YPartyKitProvider(
+  "blocknote-dev.yousefed.partykit.dev",
+  // Use a unique name as a "room" for your application.
+  "comments-with-sidebar",
+  doc,
+);
+
 // This follows the Y-Sweet example to setup a collabotive editor
 // (but of course, you also use other collaboration providers
 // see the docs for more information)
 export default function App() {
-  const docId = "my-blocknote-document-with-comments-2";
-
-  return (
-    <YDocProvider
-      docId={docId}
-      authEndpoint="https://demos.y-sweet.dev/api/auth"
-    >
-      <Document />
-    </YDocProvider>
-  );
-}
-
-function Document() {
   const [activeUser, setActiveUser] = useState<MyUserType>(HARDCODED_USERS[0]);
   const [commentFilter, setCommentFilter] = useState<
     "open" | "resolved" | "all"
@@ -55,11 +52,6 @@ function Document() {
   const [commentSort, setCommentSort] = useState<
     "position" | "recent-activity" | "oldest"
   >("position");
-
-  const provider = useYjsProvider();
-
-  // take the Y.Doc collaborative document from Y-Sweet
-  const doc = useYDoc();
 
   // setup the thread store which stores / and syncs thread / comment data
   const threadStore = useMemo(() => {

packages/core/src/comments/threadstore/yjs/YjsThreadStoreBase.ts
@@ -29,7 +29,9 @@ export abstract class YjsThreadStoreBase extends ThreadStore {
   public getThreads(): Map<string, ThreadData> {
     const threadMap = new Map<string, ThreadData>();
     this.threadsYMap.forEach((yThread, id) => {
-      threadMap.set(id, yMapToThread(yThread));
+      if (yThread instanceof Y.Map) {
+        threadMap.set(id, yMapToThread(yThread));
+      }
     });
     return threadMap;
   }

packages/mantine/src/blocknoteStyles.css
@@ -635,10 +635,6 @@
   box-shadow: none;
 }
 
-.bn-mantine .bn-thread:not(.selected) {
-  cursor: pointer;
-}
-
 .bn-mantine .bn-thread-comments,
 .bn-mantine .bn-thread-composer {
   display: flex;

pnpm-lock.yaml
@@ -3635,15 +3635,18 @@ importers:
       '@mantine/utils':
         specifier: ^6.0.22
         version: 6.0.22(react@19.2.0)
-      '@y-sweet/react':
-        specifier: ^0.6.3
-        version: 0.6.4(react@19.2.0)(yjs@13.6.27)
       react:
         specifier: ^19.2.0
         version: 19.2.0
       react-dom:
         specifier: ^19.2.0
         version: 19.2.0(react@19.2.0)
+      y-partykit:
+        specifier: ^0.0.25
+        version: 0.0.25
+      yjs:
+        specifier: ^13.6.27
+        version: 13.6.27
     devDependencies:
       '@types/react':
         specifier: ^19.2.2