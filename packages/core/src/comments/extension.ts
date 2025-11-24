38a5fc20846e9d73042cb9fbfe2b377d8fd72fa4
Matthew Lipski <matthewlipski@gmail.com>
2025-11-24 15:06:37 +0100

    Removed logs

--- packages/core/src/comments/extension.ts
+++ packages/core/src/comments/extension.ts
@@ -179,8 +179,6 @@ export const CommentsExtension = createExtension(
                 return state;
               }
 
-              console.log("fuck");
-
               // only update threadPositions if the doc changed
               const newThreadPositions = tr.docChanged
                 ? getUpdatedThreadPositions(tr.doc, markType)
@@ -196,7 +194,6 @@ export const CommentsExtension = createExtension(
               const decorations = [] as any[];
 
               if (selectedThreadId) {
-                console.log(selectedThreadId);
                 const selectedThreadPosition =
                   newThreadPositions.get(selectedThreadId);
 
