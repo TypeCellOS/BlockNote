a760fa3977e6d46c7e2a39479bb7e6acd3225758
yousefed <yousefdardiry@gmail.com>
2026-01-13 11:27:40 +0100

    npm create fumadocs

---
+++ docs2/app/api/search/route.ts
@@ -0,0 +1,9 @@
+import { source } from '@/lib/source';
+import { createFromSource } from 'fumadocs-core/search/server';
+
+export const revalidate = false;
+
+export const { staticGET: GET } = createFromSource(source, {
+  // https://docs.orama.com/docs/orama-js/supported-languages
+  language: 'english',
+});
---
+++ docs2/next.config.mjs
@@ -0,0 +1,11 @@
+import { createMDX } from 'fumadocs-mdx/next';
+
+const withMDX = createMDX();
+
+/** @type {import('next').NextConfig} */
+const config = {
+  output: 'export',
+  reactStrictMode: true,
+};
+
+export default withMDX(config);
