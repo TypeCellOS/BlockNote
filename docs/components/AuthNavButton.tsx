dadb970865264d5e480d477b8a531d2bf2070edb
yousefed <yousefdardiry@gmail.com>
2026-01-21 05:04:21 +0100

    fix nav button dark

--- docs/components/AuthNavButton.tsx
+++ docs/components/AuthNavButton.tsx
@@ -49,16 +49,16 @@ export const UserImage = ({ user }: UserImageProps) => {
 };
 
 export function AuthNavButton() {
-  const session = authClient.useSession();
-  // const session = {
-  //   data: {
-  //     planType: "free",
-  //     user: {
-  //       name: "You",
-  //       image: undefined,
-  //     },
-  //   },
-  // };
+  // const session = authClient.useSession();
+  const session = {
+    data: {
+      planType: "free",
+      user: {
+        name: "You",
+        image: undefined,
+      },
+    },
+  };
   const pathname = usePathname();
 
   if (!session.data) {
@@ -78,7 +78,7 @@ export function AuthNavButton() {
         <Menu.Item
           key={"subscription"}
           className={cn(
-            "flex cursor-default select-none py-2 pl-4 pr-8 text-sm leading-4 outline-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900",
+            "flex cursor-default select-none py-2 pl-4 pr-8 text-sm leading-4 outline-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900 dark:data-[highlighted]:text-gray-900 dark:data-[highlighted]:before:bg-gray-100",
           )}
         >
           <a
@@ -98,7 +98,7 @@ export function AuthNavButton() {
         <Menu.Item
           key={"signout"}
           className={cn(
-            "flex cursor-default select-none py-2 pl-4 pr-8 text-sm leading-4 outline-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900",
+            "flex cursor-default select-none py-2 pl-4 pr-8 text-sm leading-4 outline-none data-[highlighted]:relative data-[highlighted]:z-0 data-[highlighted]:text-gray-50 data-[highlighted]:before:absolute data-[highlighted]:before:inset-x-1 data-[highlighted]:before:inset-y-0 data-[highlighted]:before:z-[-1] data-[highlighted]:before:rounded-sm data-[highlighted]:before:bg-gray-900 dark:data-[highlighted]:text-gray-900 dark:data-[highlighted]:before:bg-gray-100",
           )}
         >
           <button
@@ -139,7 +139,7 @@ function NavbarMenu({
         </Menu.Button> */}
       <Menu.Portal>
         <Menu.Positioner className="z-50000 outline-none" sideOffset={10}>
-          <Menu.Popup className="origin-[var(--transform-origin)] rounded-md bg-[canvas] py-1 text-gray-900 shadow-lg shadow-gray-200 outline outline-1 outline-gray-200 transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[starting-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:shadow-none dark:-outline-offset-1 dark:outline-gray-300">
+          <Menu.Popup className="origin-[var(--transform-origin)] rounded-md bg-[canvas] py-1 text-gray-900 shadow-lg shadow-gray-200 outline outline-1 outline-gray-200 transition-[transform,scale,opacity] data-[ending-style]:scale-90 data-[starting-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:bg-neutral-900 dark:text-gray-100 dark:shadow-none dark:-outline-offset-1 dark:outline-gray-700">
             <Menu.Arrow className="data-[side=bottom]:top-[-8px] data-[side=left]:right-[-13px] data-[side=right]:left-[-13px] data-[side=top]:bottom-[-8px] data-[side=left]:rotate-90 data-[side=right]:-rotate-90 data-[side=top]:rotate-180">
               <ArrowSvg />
             </Menu.Arrow>
@@ -165,7 +165,7 @@ function ArrowSvg(props: ComponentProps<"svg">) {
     <svg width="20" height="10" viewBox="0 0 20 10" fill="none" {...props}>
       <path
         d="M9.66437 2.60207L4.80758 6.97318C4.07308 7.63423 3.11989 8 2.13172 8H0V10H20V8H18.5349C17.5468 8 16.5936 7.63423 15.8591 6.97318L11.0023 2.60207C10.622 2.2598 10.0447 2.25979 9.66437 2.60207Z"
-        className="fill-[canvas]"
+        className="fill-[canvas] dark:fill-neutral-900"
       />
       <path
         d="M8.99542 1.85876C9.75604 1.17425 10.9106 1.17422 11.6713 1.85878L16.5281 6.22989C17.0789 6.72568 17.7938 7.00001 18.5349 7.00001L15.89 7L11.0023 2.60207C10.622 2.2598 10.0447 2.2598 9.66436 2.60207L4.77734 7L2.13171 7.00001C2.87284 7.00001 3.58774 6.72568 4.13861 6.22989L8.99542 1.85876Z"
@@ -173,7 +173,7 @@ function ArrowSvg(props: ComponentProps<"svg">) {
       />
       <path
         d="M10.3333 3.34539L5.47654 7.71648C4.55842 8.54279 3.36693 9 2.13172 9H0V8H2.13172C3.11989 8 4.07308 7.63423 4.80758 6.97318L9.66437 2.60207C10.0447 2.25979 10.622 2.2598 11.0023 2.60207L15.8591 6.97318C16.5936 7.63423 17.5468 8 18.5349 8H20V9H18.5349C17.2998 9 16.1083 8.54278 15.1901 7.71648L10.3333 3.34539Z"
-        className="dark:fill-gray-300"
+        className="dark:fill-gray-700"
       />
     </svg>
   );
