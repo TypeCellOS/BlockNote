87292389d676f831932b2a54aebb683073177ba7
yousefed <yousefdardiry@gmail.com>
2026-01-26 09:41:07 +0100

    fix

--- docs/app/demo/_components/utils.ts
+++ docs/app/demo/_components/utils.ts
@@ -3,41 +3,49 @@ export const HARDCODED_USERS = [
     id: "user-1",
     username: "Kev",
     color: "#9810fa",
+    avatarUrl: "/avatars/avater2-m.jpg",
   },
   {
     id: "user-2",
     username: "Matt",
     color: "#00ff00",
+    avatarUrl: "/avatars/avatar3-m.jpg",
   },
   {
     id: "user-3",
     username: "Sef",
     color: "#ababffff",
+    avatarUrl: "/avatars/avater5-m.jpg",
   },
   {
     id: "user-4",
     username: "Nicky",
     color: "#ff00ff",
+    avatarUrl: "/avatars/avatar1-f.jpg",
   },
   {
     id: "user-5",
     username: "Sam",
     color: "#00ffff",
+    avatarUrl: "/avatars/avatar7-m.jpg",
   },
   {
     id: "user-6",
     username: "Walter",
     color: "#ffff00",
+    avatarUrl: "/avatars/avatar10-m.jpg",
   },
   {
     id: "user-7",
     username: "Amanda",
     color: "#ffa500",
+    avatarUrl: "/avatars/avatar4-f.jpg",
   },
   {
     id: "user-8",
     username: "Sara",
     color: "#800080",
+    avatarUrl: "/avatars/avatar6-f.jpg",
   },
 ];
 
