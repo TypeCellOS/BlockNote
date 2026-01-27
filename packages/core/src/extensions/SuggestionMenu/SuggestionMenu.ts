a61282b8b07bb5bac5cc5437f57d82b71ea7d4ce
Matthew Lipski <matthewlipski@gmail.com>
2026-01-27 02:12:51 +0100

    Moved page up/down handling to keyboard navigation hook

--- packages/core/src/extensions/SuggestionMenu/SuggestionMenu.ts
+++ packages/core/src/extensions/SuggestionMenu/SuggestionMenu.ts
@@ -347,20 +347,6 @@ export const SuggestionMenu = createExtension(({ editor }) => {
             }
             return false;
           },
-          handleKeyDown(view, event) {
-            const pluginState = suggestionMenuPluginKey.getState(view.state);
-
-            if (!pluginState) {
-              return false;
-            }
-
-            if (event.key === "PageDown" || event.key === "PageUp") {
-              event.preventDefault();
-              return true;
-            }
-
-            return false;
-          },
 
           // Setup decorator on the currently active suggestion.
           decorations(state) {
