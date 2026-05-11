d82ad67a93e37f593fbd92535eeee711d62f954b
Matthew Lipski <matthewlipski@gmail.com>
2026-05-11 15:21:07 +0200

    Updated e2e test snapshots

--- tests/src/end-to-end/multicolumn/multicolumn.test.ts
+++ tests/src/end-to-end/multicolumn/multicolumn.test.ts
@@ -43,8 +43,7 @@ test.describe("Check Multi-Column Behaviour", () => {
   test("Check Delete end of column list", async ({ page }) => {
     await focusOnEditor(page);
 
-    await page.locator(BULLET_LIST_SELECTOR).last().click();
-    await page.keyboard.press("ArrowDown");
+    await page.locator(".bn-trailing-block").click();
     await page.keyboard.type("Paragraph");
     await page.keyboard.press("ControlOrMeta+ArrowLeft");
     await page.keyboard.press("ArrowLeft");
