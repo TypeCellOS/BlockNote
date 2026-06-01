a88625d3b4eb9d91749657488749cfb9370bc391
Luc H <wereld03@gmail.com>
2026-06-01 15:56:16 +0200

    cleanup scrollintoview call
    Co-authored-by: Nick Perez <computers@nickthesick.com>

--- packages/core/src/blocks/utils/listItemEnterHandler.ts
+++ packages/core/src/blocks/utils/listItemEnterHandler.ts
@@ -34,10 +34,9 @@ export const handleEnter = (
   } else if (blockContent.node.childCount > 0) {
     return editor.transact((tr) => {
       tr.deleteSelection();
-      const result = splitBlockTr(tr, tr.selection.from, true);
       tr.scrollIntoView();
-      return result;
-    });
+      return splitBlockTr(tr, tr.selection.from, true);
+});
   }
 
   return false;
