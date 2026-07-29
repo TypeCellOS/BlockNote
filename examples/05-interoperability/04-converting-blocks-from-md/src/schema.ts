8d7774948a08f95817aa1ed01aff96b11c5a4052
hieuph <hieuph@authenticeducationhub.com>
2026-07-29 17:50:22 +0700

    feat(example): add formulaBlock node

--- examples/05-interoperability/04-converting-blocks-from-md/src/schema.ts
+++ examples/05-interoperability/04-converting-blocks-from-md/src/schema.ts
@@ -3,8 +3,9 @@ import {
   defaultBlockSpecs,
   defaultInlineContentSpecs,
 } from "@blocknote/core";
-import { createReactInlineContentSpec } from "@blocknote/react";
+import { createReactInlineContentSpec, createReactBlockSpec } from "@blocknote/react";
 import { FormulaInlineView } from "./formula/FormulaInline";
+import { FormulaBlockView } from "./formula/FormulaBlock";
 import { useFormulaEditor } from "./formula/formulaContext";
 
 export const formulaInline = createReactInlineContentSpec(
@@ -30,6 +31,33 @@ export const formulaInline = createReactInlineContentSpec(
   },
 );
 
+export const formulaBlock = createReactBlockSpec(
+  {
+    type: "formulaBlock",
+    propSchema: {
+      latex: { default: "" },
+    },
+    content: "none",
+  },
+  {
+    render: (props) => {
+      const editor = useFormulaEditor();
+      return (
+        <FormulaBlockView
+          latex={props.block.props.latex}
+          onOpenEditor={() =>
+            editor.openEdit({
+              kind: "block",
+              latex: props.block.props.latex,
+              blockId: props.block.id,
+            })
+          }
+        />
+      );
+    },
+  },
+);
+
 export const schema = BlockNoteSchema.create({
   inlineContentSpecs: {
     ...defaultInlineContentSpecs,
@@ -37,7 +65,7 @@ export const schema = BlockNoteSchema.create({
   },
   blockSpecs: {
     ...defaultBlockSpecs,
-    // formulaBlock added in Task 4
+    formulaBlock,
   },
 });
 
