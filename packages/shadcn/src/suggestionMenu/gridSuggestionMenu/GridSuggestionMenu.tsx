3d0806433238af064e74428aa2f42b57426d2fdb
Matthew Lipski <50169049+matthewlipski@users.noreply.github.com>
2026-08-04 09:29:44 +0200

    feat(shadcn): use base-ui instead of radix (BLO-1279) (#2913)
    ShadCN has moved from Radix as the default to Base UI as the default.
    
    BREAKING CHANGE: This upgrades the blocknote-shadcn integration to move from the Radix UI version of ShadCN to the Base UI version of ShadCN

--- packages/shadcn/src/suggestionMenu/gridSuggestionMenu/GridSuggestionMenu.tsx
+++ packages/shadcn/src/suggestionMenu/gridSuggestionMenu/GridSuggestionMenu.tsx
@@ -16,7 +16,7 @@ export const GridSuggestionMenu = forwardRef<
     <div
       // Styles from ShadCN DropdownMenuContent component
       className={cn(
-        "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 max-h-(--radix-dropdown-menu-content-available-height) origin-(--radix-dropdown-menu-content-transform-origin) z-50 min-w-[8rem] overflow-y-auto overflow-x-hidden rounded-md border p-1 shadow-md",
+        "bg-popover text-popover-foreground z-50 min-w-32 overflow-x-hidden overflow-y-auto rounded-md p-1 shadow-md ring-1 ring-foreground/10",
         "grid",
         className,
       )}
