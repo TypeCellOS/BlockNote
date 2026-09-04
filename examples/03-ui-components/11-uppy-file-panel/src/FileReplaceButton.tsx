3c9c0b68870a8805582c6b6fdd36b54529db6cf9
Matthew Lipski <matthewlipski@gmail.com>
2026-09-04 10:51:13 +0200

    Fixed build

--- examples/03-ui-components/11-uppy-file-panel/src/FileReplaceButton.tsx
+++ examples/03-ui-components/11-uppy-file-panel/src/FileReplaceButton.tsx
@@ -8,7 +8,7 @@ import {
   useBlockNoteEditor,
   useComponentsContext,
   useDictionary,
-  useMobileToolbarPortal,
+  usePortalContext,
   useSelectedBlocks,
 } from "@blocknote/react";
 import { useCallback, useEffect, useState } from "react";
@@ -24,7 +24,7 @@ export const FileReplaceButton = () => {
   const dict = useDictionary();
   const Components = useComponentsContext()!;
   // Portal necessary to properly show popover on mobile.
-  const mobileToolbarPortal = useMobileToolbarPortal();
+  const mobileToolbarPortal = usePortalContext();
 
   const editor = useBlockNoteEditor<
     BlockSchema,
@@ -68,7 +68,7 @@ export const FileReplaceButton = () => {
       open={isOpen}
       onOpenChange={setIsOpen}
       position={"bottom"}
-      portalRoot={mobileToolbarPortal ?? undefined}
+      portalRoot={mobileToolbarPortal}
     >
       <Components.Generic.Popover.Trigger>
         <Components.FormattingToolbar.Button
